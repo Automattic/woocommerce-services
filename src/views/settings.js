a762e8480b774f62aba70b39d9eca230d2f7d435
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-02-09 17:22:21 -0700

    Render Form component using a custom SchemaField that decorates fields with a specific uiSchema property.

--- src/views/settings.js
+++ src/views/settings.js
@@ -1,12 +1,47 @@
-var React = require( 'react' );
+import React from "react";
+import SchemaField from "react-jsonschema-form/lib/components/fields/SchemaField";
+import Form from "react-jsonschema-form";
+
+const schema = {
+  title: "Todo Tasks",
+  type: "object",
+  required: ["title"],
+  properties: {
+    title: {type: "string", title: "Title", default: "A new task"},
+    done: {type: "boolean", title: "Done?", default: false}
+  }
+};
+
+const uiSchema = {
+	done: {
+		"ui:widget": "select",
+		"arbitrary": "value"
+	}
+};
+
+const CustomSchemaField = function( props ) {
+
+	if ( props.uiSchema && props.uiSchema.hasOwnProperty( "arbitrary" ) && "value" === props.uiSchema.arbitrary ) {
+
+		return (
+			<div className="custom">
+				<p>Arbitrary Value found</p>
+				<SchemaField {...props} />
+			</div>
+		);
+
+	}
+
+	return (
+		<SchemaField {...props} />
+	);
+};
 
 module.exports = React.createClass( {
 
     render: function() {
         return (
-            <p>
-                How doodly do - I'm a react component
-            </p>
+            <Form schema={schema} uiSchema={uiSchema} SchemaField={CustomSchemaField} />
         );
     }
 
