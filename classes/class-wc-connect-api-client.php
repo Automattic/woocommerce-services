6cd56bc9aca1d5f4fc21e695372161994f9acbfb
Allen Snook <allen@allendav.com>
2016-02-17 13:43:08 -0800

    Code review fixes

--- classes/class-wc-connect-api-client.php
+++ classes/class-wc-connect-api-client.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'WOOCOMMERCE_CONNECT_SERVER_URL' ) ) {
-    define( 'WOOCOMMERCE_CONNECT_SERVER_URL', 'https://wooconnect.woocommerce.com/' );
+    define( 'WOOCOMMERCE_CONNECT_SERVER_URL', 'https://api.woocommerce.com/' );
 }
 
 if ( ! class_exists( 'WC_Connect_API_Client' ) ) {
@@ -14,57 +14,12 @@ if ( ! class_exists( 'WC_Connect_API_Client' ) ) {
     class WC_Connect_API_Client {
 
         /**
-         * @var static The reference the *Singleton* instance of this class
-         */
-        private static $instance;
-
-        /**
-         * Returns the *Singleton* instance of this class.
-         *
-         * @return static The *Singleton* instance.
-         */
-        public static function getInstance() {
-            if ( null === self::$instance ) {
-                self::$instance = new self();
-            }
-
-            return self::$instance;
-        }
-
-        /**
-         * Private clone method to prevent cloning of the instance of the
-         * *Singleton* instance.
-         *
-         * @return void
-         */
-        private function __clone() {
-        }
-
-        /**
-         * Private un-serialize method to prevent un-serializing of the *Singleton*
-         * instance.
-         *
-         * @return void
-         */
-        private function __wakeup() {
-        }
-
-        /**
-         * Protected constructor to prevent creating a new instance of the
-         * *Singleton* via the `new` operator from outside of this class.
-         */
-        protected function __construct() {
-
-        }
-
-
-        /**
          * Requests the available services for this site from the WooCommerce Connect Server
          *
          * @return array|WP_Error
          */
-        public function get_services() {
-            return do_request( 'GET', '/services' );
+        public static function get_services() {
+            return self::request( 'GET', '/services' );
         }
 
 
@@ -76,12 +31,12 @@ if ( ! class_exists( 'WC_Connect_API_Client' ) ) {
          *
          * @return bool|WP_Error
          */
-        public function validate_service_settings( $service_slug, $service_settings ) {
+        public static function validate_service_settings( $service_slug, $service_settings ) {
             if ( 1 === preg_match( "/[^a-z_-]*/gi", $service_slug ) ) {
-                return new WP_Error( 'error', __( 'Invalid WooCommerce Connect service slug provided' ) );
+                return new WP_Error( 'invalid_service_slug', 'Invalid WooCommerce Connect service slug provided' );
             }
 
-            return do_request( 'POST', "/services/{$service_slug}/settings", $service_settings );
+            return self::request( 'POST', "/services/{$service_slug}/settings", $service_settings );
         }
 
 
@@ -92,7 +47,7 @@ if ( ! class_exists( 'WC_Connect_API_Client' ) ) {
          * @param $package Package provided to WC_Shipping_Method::calculate_shipping()
          * @return object|WP_Error
          */
-        public function get_shipping_rates( $settings, $package ) {
+        public static function get_shipping_rates( $settings, $package ) {
             $body = array(
                 'fields' => $settings,
                 'destination' => $package->destination,
@@ -101,7 +56,7 @@ if ( ! class_exists( 'WC_Connect_API_Client' ) ) {
                 )
             );
 
-            return do_request( 'GET', '/shipping/rates', $body );
+            return self::request( 'GET', '/shipping/rates', $body );
         }
 
 
@@ -110,8 +65,8 @@ if ( ! class_exists( 'WC_Connect_API_Client' ) ) {
          *
          * @return true|WP_Error
          */
-        public function test_connection() {
-            return do_request( 'GET', '/connection-test' );
+        public static function auth_test() {
+            return self::request( 'GET', '/auth-test' );
         }
 
 
@@ -123,14 +78,14 @@ if ( ! class_exists( 'WC_Connect_API_Client' ) ) {
          * @param $body
          * @return mixed|WP_Error
          */
-        public function do_request( $method, $path, $body = array() ) {
+        protected static function request( $method, $path, $body = array() ) {
 
             if ( ! class_exists( 'Jetpack_client' ) ) {
-                return new WP_Error( 'error', __( 'Unable to send request to WooCommerce Connect server. Jetpack client was not found.', '' ) );
+                return new WP_Error( 'jetpack_client_not_found', 'Unable to send request to WooCommerce Connect server. Jetpack client was not found.' );
             }
 
             if ( ! is_array( $body ) ) {
-                return new WP_Error( 'error', __( 'Unable to send request to WooCommerce Connect server. Body must be an array.', '' ) );
+                return new WP_Error( 'request_body_should_be_array', 'Unable to send request to WooCommerce Connect server. Body must be an array.' );
             }
 
             $url = trailingslashit( WOOCOMMERCE_CONNECT_SERVER_URL ) . $path;
@@ -144,6 +99,7 @@ if ( ! class_exists( 'WC_Connect_API_Client' ) ) {
             if ( ! array_key_exists( 'settings', $body ) ) {
                 $body['settings'] = array();
             }
+
             $body['settings'] = wp_parse_args( $body['settings'], array(
                 'currency' => get_woocommerce_currency(),
                 'weight' => strtolower( get_option('woocommerce_weight_unit') ),
@@ -151,12 +107,22 @@ if ( ! class_exists( 'WC_Connect_API_Client' ) ) {
                 'wp_version' => get_bloginfo( 'version' ),
                 'wc_version' => WC()->version,
                 'jetpack_version' => JETPACK__VERSION
-            );
+            ) );
 
-            add_filter( 'http_request_args', array( $this, 'filter_http_request_args' ), 10, 2 );
-            Jetpack_client::remote_request( $args, $body );
-            remove_filter( 'http_request_args', array( $this, 'filter_http_request_args' ) );
+            $body = apply_filters( 'wc_connect_api_client_body', $body );
 
+            add_filter( 'http_request_args', array( 'WC_Connect_API_Client', 'filter_http_request_args' ), 10, 2 );
+            $body = wp_json_encode( $body );
+            if ( $body ) {
+                $result = Jetpack_client::remote_request( $args, $body );
+            }
+            remove_filter( 'http_request_args', array( 'WC_Connect_API_Client', 'filter_http_request_args' ) );
+
+            if ( ! $body ) {
+                return new WP_Error( 'unable_to_json_encode_body', 'Unable to encode body for request to WooCommerce Connect server.' );
+            }
+
+            return $result;
         }
 
 
@@ -167,9 +133,15 @@ if ( ! class_exists( 'WC_Connect_API_Client' ) ) {
          * @param $url string
          * @return array
          */
-        public function filter_http_request_args( $request_args, $url ) {
-            // TODO add Accept-Language: language
-            // TODO add Accept: application/vnd.woocommerce-connect.v1
+        public static function filter_http_request_args( $request_args, $url ) {
+
+            if ( ! array_key_exists( 'headers', $request_args ) ) {
+                $request_args['headers'] = array();
+            }
+
+            $request_args['headers']['Accept-Language'] = substr( get_locale(), 0, 2 );
+            $request_args['headers']['Accept'] = 'application/vnd.woocommerce-connect.v1';
+
             return $request_args;
         }
     }
