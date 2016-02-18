1a1e34ab803be6a1c5f5426c118b7d3a996119e6
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-02-18 11:38:06 -0700

    Attempt to get dops-components (on React 0.14.x) to work.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,80 @@
+var webpack = require("webpack"),
+	fs = require('fs'),
+	path = require('path'),
+	ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var styleDefaults = 'css?sourceMap!autoprefixer!sass?sourceMap!custom-colors';
+
+var cssLoader = ExtractTextPlugin.extract('css?sourceMap!autoprefixer!');
+
+var scssLoader = ExtractTextPlugin.extract(styleDefaults);
+
+// build the plugin list, optionally excluding hot update plugins if we're building for production
+var plugins = [
+	new ExtractTextPlugin("[name].css")
+].filter( function(plugin) { return plugin !== false; } );
+
 module.exports = {
+	progress: true,
 	entry: [
-		'babel-polyfill',
 		'./src/main.js'
 	],
 	output: {
-		filename: './build/bundle.js'
+		publicPath: '/assets/',
+		path: path.resolve(__dirname, "build"),
+		filename: 'bundle.js',
+		chunkFilename: "[id].js"
 	},
 	resolve: {
-		extensions: [ '', '.js', '.jsx' ]
+		extensions: ["", ".js", ".jsx"],
+		alias: {
+			"react": path.join(__dirname, "/node_modules/react")
+		},
+		root: [ path.resolve( __dirname, 'src' ), fs.realpathSync( path.join(__dirname, 'node_modules/@automattic/dops-components/client') ) ]
 	},
+	resolveLoader: {
+		root: path.join( __dirname, "node_modules" ),
+		alias: {
+			'custom-colors': '@automattic/custom-colors-loader'
+		}
+	},
+	node: {
+		fs: "empty"
+	},
+	stats: { colors: true, reasons: true },
 	module: {
-		loaders: [{
-			loader: "babel",
-			query: {
-				presets: ['es2015', 'react']
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				loader: require.resolve('babel-loader'),
+				query: {
+					presets: [ 'es2015', 'react', 'stage-1' ]
+				},
+
+				// include both typical npm-linked locations and default module locations to handle both cases
+				include: [
+					path.join(__dirname, 'src'),
+					fs.realpathSync( path.join(__dirname, 'node_modules/@automattic/dops-components/client') ),
+					path.join(__dirname, 'node_modules/@automattic/dops-components/client')
+				]
 			},
-			include: [
-				__dirname + '/src',
-				__dirname + '/node_modules/@automattic/dops-components/client'
-			],
-			test: /\.jsx?$/
-		}]
-	}
+			{
+				test: /\.json$/,
+				loader: require.resolve('json-loader')
+			},
+			{
+				test: /\.css$/,
+				loader: cssLoader
+			},
+			{
+				test: /\.scss$/,
+				loader: scssLoader
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: require.resolve('url-loader')+"?limit=20000&mimetype=image/svg+xml"
+			}
+		]
+	},
+	plugins: plugins
 };
