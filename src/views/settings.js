e66829350849e3ea0effa421ca6b1c2a76b04fc7
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-02-18 15:48:27 -0700

    Render a form based on the transformed JSON Schema.

--- src/views/settings.js
+++ src/views/settings.js
@@ -1,12 +1,25 @@
 var React = require( 'react' );
+var t = require( 'tcomb-form' );
+var transform = require( 'tcomb-json-schema' );
 
 module.exports = React.createClass( {
 
+    onSubmit: function ( evt ) {
+        evt.preventDefault();
+        console.log( this.refs.form.getValue() );
+    },
+
     render: function() {
+
+        var FormSchema = transform( this.props.schema );
+
         return (
-            <p>
-                How doodly do - I'm a react component
-            </p>
+            <form onSubmit={this.onSubmit}>
+                <t.form.Form ref="form" type={FormSchema} />
+                <div className="form-group">
+                    <button type="submit" className="btn btn-primary">Save</button>
+                </div>
+            </form>
         );
     }
 
