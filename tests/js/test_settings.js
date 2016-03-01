1ccdf19260dd362387eeaf16cc4c647ed7e12b86
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-02-29 21:53:57 -0700

    Add initial Settings component test.

---
+++ tests/js/test_settings.js
@@ -0,0 +1,30 @@
+/* eslint-disable vars-on-top */
+require( '../../lib/react-test-env-setup' )( null, {
+	localStorage: false
+} );
+
+/**
+ * External dependencies
+ */
+var expect = require( 'chai' ).expect,
+	ReactDom = require( 'react-dom' ),
+	React = require( 'react' ),
+	TestUtils = require( 'react-addons-test-utils' );
+
+/**
+ * Internal dependencies
+ */
+var Settings = require( '../../src/views/settings' );
+
+describe( 'Settings', function() {
+	afterEach( function () {
+		ReactDom.unmountComponentAtNode( document.body );
+	} );
+
+	it( 'should contain an input', function() {
+		var tree = TestUtils.renderIntoDocument( <Settings schema={{ "type": "string" }} /> ),
+			input = TestUtils.findRenderedDOMComponentWithTag( tree, 'input' );
+
+		expect( input ).to.be.ok;
+	} );
+} );
