4c2760c6392b83baab12326134d83d7bdab26167
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-03-03 18:11:40 -0700

    Add test for shipping script enqueuing.

--- tests/php/test_woocommerce-connect-client.php
+++ tests/php/test_woocommerce-connect-client.php
@@ -2,6 +2,14 @@
 
 class WP_Test_WC_Connect_Loader extends WC_Unit_Test_Case {
 
+	const SHIPPING_SCRIPT_HANDLE = 'wc_connect_shipping_admin';
+
+	public function tearDown() {
+
+		wp_deregister_script( self::SHIPPING_SCRIPT_HANDLE );
+
+	}
+
 	public function test_class_exists() {
 
 		$this->assertTrue( class_exists( 'WC_Connect_Loader' ) );
@@ -14,6 +22,30 @@ class WP_Test_WC_Connect_Loader extends WC_Unit_Test_Case {
 		$attached = has_action( 'woocommerce_init', array( $loader, 'init' ) );
 
 		$this->assertNotFalse( $attached, 'WC_Connect_Loader::init() not attached to `woocommerce_init`.' );
+
+	}
+
+	public function enqueue_scripts_provider() {
+
+		return array(
+			'good values'     => array( 'woocommerce_page_wc-settings', 'shipping', 1, true ),
+			'bad page hook'   => array( 'woocommerce_page_wc-status', 'shipping', 1, false ),
+			'bad tab value'   => array( 'woocommerce_page_wc-settings', 'checkout', 1, false ),
+			'bad instance id' => array( 'woocommerce_page_wc-settings', 'shipping', null, false ),
+		);
+
+	}
+
+	/**
+	 * @dataProvider enqueue_scripts_provider
+	 */
+	public function test_enqueue_shipping_script( $hook, $tab, $instance_id, $expected ) {
+
+		$loader = new WC_Connect_Loader();
+
+		$loader->enqueue_shipping_script( $hook, $tab, $instance_id );
+
+		$this->assertEquals( $expected, wp_script_is( self::SHIPPING_SCRIPT_HANDLE, 'registered' ) );
 
 	}
 
