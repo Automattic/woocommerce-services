b27ec2d151cc530a8a320b8daf04cc0dd5f8e732
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-03-07 10:58:41 -0700

    Add test coverage for package destination validation method.

---
+++ tests/php/test_class-wc-connect-shipping-method.php
@@ -0,0 +1,69 @@
+<?php
+
+class WP_Test_WC_Connect_Shipping_Method extends WP_UnitTestCase {
+
+	public function is_valid_package_destination_provider() {
+
+		return array(
+			'empty country' => array( array(
+				'destination' => array(
+					'country'  => '',
+					'state'    => 'UT',
+					'postcode' => '84068'
+				)
+			), false ),
+			'empty state' => array( array(
+				'destination' => array(
+					'country'  => 'US',
+					'state'    => '',
+					'postcode' => '84068'
+				)
+			), false ),
+			'empty postcode' => array( array(
+				'destination' => array(
+					'country'  => 'US',
+					'state'    => 'UT',
+					'postcode' => ''
+				)
+			), false ),
+			'invalid postcode' => array( array(
+				'destination' => array(
+					'country'  => 'US',
+					'state'    => 'UT',
+					'postcode' => 'EC4V 6JA'
+				)
+			), false ),
+			'invalid state' => array( array(
+				'destination' => array(
+					'country'  => 'US',
+					'state'    => 'XX',
+					'postcode' => '84068'
+				)
+			), false ),
+			'valid destination' => array( array(
+				'destination' => array(
+					'country'  => 'US',
+					'state'    => 'UT',
+					'postcode' => '84068'
+				)
+			), true ),
+		);
+
+	}
+
+	/**
+	 * @dataProvider is_valid_package_destination_provider
+	 * @covers WC_Connect_Shipping_Method::is_valid_package_destination
+	 */
+	public function test_is_valid_package_destination( $package, $expected ) {
+
+		$shipping_method = $this->getMockBuilder( 'WC_Connect_Shipping_Method' )
+			->disableOriginalConstructor()
+			->setMethods( null )
+			->getMock();
+
+		$this->assertEquals( $expected, $shipping_method->is_valid_package_destination( $package ) );
+
+	}
+
+}
