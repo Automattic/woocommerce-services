989591371d8d3e9dc568e5087d6b726054ea51f6
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-03-08 13:54:49 -0700

    Add tests for POSTed form setting whitelist method.

--- tests/php/test_class-wc-connect-shipping-method.php
+++ tests/php/test_class-wc-connect-shipping-method.php
@@ -83,4 +83,64 @@ class WP_Test_WC_Connect_Shipping_Method extends WP_UnitTestCase {
 
 	}
 
+	public function get_posted_form_settings_provider() {
+
+		$posted_data = array(
+			'field_1'          => 'test',
+			'field_2'          => 'on',
+			'_wp_http_referer' => '/wp-admin/admin.php?page=wc-settings&tab=shipping&instance_id=1',
+			'_wpnonce'         => '1q2w3e4r5t',
+		);
+
+		$bad_schema = (object) array();
+		$expected_bad_schema = array();
+
+		$good_schema = (object) array(
+			'properties' => (object) array(
+				'field_1' => (object) array(
+					'type'  => 'string',
+					'title' => 'Field 1',
+				),
+				'field_2' => (object) array(
+					'type'  => 'boolean',
+					'title' => 'Field 2',
+				),
+			)
+		);
+
+		$expected_good_schema = array(
+			'field_1' => 'test',
+			'field_2' => true,
+		);
+
+		return array(
+			'bad schema' => array( array(), $posted_data, $bad_schema, $expected_bad_schema ),
+			'good schema, passed in data' => array( array(), $posted_data, $good_schema, $expected_good_schema ),
+			'good schema, data from POST' => array( $posted_data, null, $good_schema, $expected_good_schema ),
+		);
+
+	}
+
+	/**
+	 * @dataProvider get_posted_form_settings_provider
+	 * @covers WC_Connect_Shipping_Method::get_posted_form_settings
+	 */
+	public function test_get_posted_form_settings( $post_data, $passed_in, $schema, $expected ) {
+
+		$loader = new WC_Connect_Loader();
+		$loader->load_dependencies();
+
+		$shipping_method = $this->getMockBuilder( 'WC_Connect_Shipping_Method' )
+			->disableOriginalConstructor()
+			->setMethods( array( 'get_form_schema' ) )
+			->getMock();
+
+		$shipping_method->method( 'get_form_schema' )->willReturn( $schema );
+
+		$_POST = $post_data;
+
+		$this->assertEquals( $expected, $shipping_method->get_posted_form_settings( $passed_in ) );
+
+	}
+
 }
