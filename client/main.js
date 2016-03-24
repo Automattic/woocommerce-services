29edde40eefd2e97851aa84fea7dc0e933954418
Joey Kudish <joachim.kudish@gmail.com>
2016-03-23 18:35:04 -0700

    refactor main file for redux and hmr

--- client/main.js
+++ client/main.js
@@ -1,16 +1,49 @@
 /*global wcConnectData */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Settings from './views/usps';
+import { Provider } from 'react-redux'
+import configureStore from './store';
 import '../assets/stylesheets/style.scss';
 
-document.addEventListener( 'DOMContentLoaded', () => {
+const store = configureStore()
+const rootEl = document.getElementById( 'wc-connect-admin-container' )
+
+let render = () => {
+	const Settings = require( './views/usps' );
 	ReactDOM.render(
-		React.createElement( Settings, {
-			schema: wcConnectData.formSchema,
-			layout: wcConnectData.formLayout,
-			initialValue: wcConnectData.formData
-		} ),
-		document.getElementById( 'wc-connect-admin-container' )
-	);
-} );
+		<Provider store={ store }>
+			<Settings
+				schema={ wcConnectData.formSchema }
+				layout={ wcConnectData.formLayout }
+				initialValue={ wcConnectData.formData }
+			/>
+		</Provider>,
+		rootEl
+	)
+}
+
+if (module.hot) {
+	// Support hot reloading of components
+	// and display an overlay for runtime errors
+	const renderApp = render
+	const renderError = ( error ) => {
+		const RedBox = require( 'redbox-react' );
+		ReactDOM.render(
+			<RedBox error={error} />,
+			rootEl
+		);
+	};
+
+	render = () => {
+		try {
+			renderApp();
+		} catch ( error ) {
+			renderError( error );
+		}
+	}
+	module.hot.accept( './views/usps', () => {
+		setTimeout( render );
+	})
+}
+
+render();
