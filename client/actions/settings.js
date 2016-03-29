834928157e1493971cb39cdb8050254bb3281aa0
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-03-29 17:52:00 -0600

    Add an action creator for form field changes in the USPS settings component.

--- client/actions/settings.js
+++ client/actions/settings.js
@@ -1,1 +1,9 @@
 export const UPDATE_TEXT_FIELD = 'UPDATE_TEXT_FIELD';
+
+export function onFieldChange( event ) {
+	return {
+		type: UPDATE_TEXT_FIELD,
+		key: event.target.name,
+		value: ( 'checkbox' === event.target.type ) ? event.target.checked : event.target.value
+	};
+};
