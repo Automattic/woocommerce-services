d670d51d7438dadb0edd1a6705a95ac7d1c0b1d9
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-03-31 11:59:24 -0600

    Move “add a package” form to it’s own component.

--- client/views/usps-settings/index.js
+++ client/views/usps-settings/index.js
@@ -5,23 +5,20 @@ import FormSectionHeading from 'components/forms/form-section-heading';
 import FormFieldset from 'components/forms/form-fieldset';
 import FormLabel from 'components/forms/form-label';
 import FormLegend from 'components/forms/form-legend';
-import FormTextInput from 'components/forms/form-text-input';
 import FormSettingExplanation from 'components/forms/form-setting-explanation';
 import FormSelect from 'components/forms/form-select';
 import FormButton from 'components/forms/form-button';
 import FormRadio from 'components/forms/form-radio';
-import FormCheckbox from 'components/forms/form-checkbox';
 import FormButtonsBar from 'components/forms/form-buttons-bar';
-import FormInputValidation from 'components/forms/form-input-validation';
 import CompactCard from 'components/card/compact';
 import Gridicon from 'components/gridicon';
-import SelectOptGroups from 'components/forms/select-opt-groups';
 import ShippingServiceGroups from 'components/shipping-service-groups';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as SettingsActions from 'state/actions/settings';
 import * as FormActions from 'state/actions/form';
 import SettingsGroup from './render-group';
+import AddPackageDialog from 'components/shipping/packages/add-package';
 
 const handleSaveForm = ( event, props ) => {
 	event.preventDefault();
@@ -37,6 +34,7 @@ const Settings = ( props ) => {
 	const { updateSettingsField, updateSettingsObjectSubField } = settingsActions;
 	return (
 		<div>
+			<AddPackageDialog />
 			<SectionHeader label="USPS Shipping">
 				<FormToggle id="enabled" name="enabled" checked={ true } readOnly={ true }>
 					<FormLabel htmlFor="enabled" style={ { float: 'left' } }>Enable</FormLabel>
@@ -108,72 +106,6 @@ const Settings = ( props ) => {
 					<FormButton onClick={ ( event ) => handleSaveForm( event, props ) }>
 						{ form.isSaving ? 'Saving...' : 'Save changes' }
 					</FormButton>
-				</FormButtonsBar>
-			</CompactCard>
-			<br />{ /* Add a package modal */ }
-			<CompactCard>
-				<FormSectionHeading>Add a package</FormSectionHeading>
-				<FormFieldset>
-					<FormLabel htmlFor="package_type">Type of package</FormLabel>
-					<SelectOptGroups id="package_type" value="box" optGroups={ [
-						{
-							options: [
-								{ value: 'box', label: 'Box' },
-								{ value: 'envelope', label: 'Envelope' },
-							],
-						},
-						{
-							label: 'Saved packages',
-							options: [
-								{ value: 'bike-box', label: 'Bike box' },
-								{ value: 'small-padded-envelope', label: 'Small padded envelope' },
-							],
-						},
-						{
-							label: 'USPS Flat Rate Boxes and Envelopes',
-							options: [
-								{ value: 'small-box', label: 'Priority Mail Small Flat Rate Box' },
-								{ value: 'medium-box', label: 'Priority Mail Medium Flat Rate Box' },
-								{ value: 'large-box', label: 'Priority Mail Large Flat Rate Box' },
-								{ value: 'legal-envelope', label: 'Priority Mail Legal Flat Rate Envelope' },
-							],
-						},
-					] } readOnly={ true }/>
-				</FormFieldset>
-				<FormFieldset>
-					<FormLabel htmlFor="package_name">Package name</FormLabel>
-					<FormTextInput
-						id="package_name"
-						name="package_name"
-						className="is-error"
-						placeholder="The customer will see this during checkout"/>
-					<FormInputValidation isError text="A package name is needed"/>
-				</FormFieldset>
-				<FormFieldset>
-					<FormLabel>Inner Dimensions (L x W x H)</FormLabel>
-					<FormTextInput placeholder="100 x 25 x 5.5"/>
-					<a href="#">Define exterior dimensions</a>
-				</FormFieldset>
-				<FormFieldset>
-					<div style={ { width: '200px', float: 'left' } }>
-						<FormLabel htmlFor="package_weight">Package weight</FormLabel>
-						<FormTextInput style={ { width: '88%' } } id="package_weight" name="package_weight" placeholder="Weight of box"/>
-					</div>
-					<div style={ { width: '200px', float: 'left' } }>
-						<FormLabel htmlFor="max_weight">Max weight</FormLabel>
-						<FormTextInput style={ { width: '88%' } } id="max_weight" name="max_weight" placeholder="Max weight"/> lbs
-					</div>
-					<FormSettingExplanation style={ { display: 'inline-block' } }>Define both the weight of the empty
-						box and the max weight it can hold</FormSettingExplanation>
-				</FormFieldset>
-			</CompactCard>
-			<CompactCard>
-				<FormLabel>
-					<FormCheckbox checked={ true } readOnly={ true }/>
-					<span>Save package to use in other shipping methods</span>
-				</FormLabel>
-				<FormButtonsBar>
-					<FormButton>Add package</FormButton>
 				</FormButtonsBar>
 			</CompactCard>
 		</div>
