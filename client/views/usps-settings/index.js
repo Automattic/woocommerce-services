b487f281df2ea183d86cfee50d03d84a45b54b68
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-04-11 18:52:14 -0500

    Define package “types” to pass as props to the Packages component.
    These will be used by the Add/Edit Package dialog.

--- client/views/usps-settings/index.js
+++ client/views/usps-settings/index.js
@@ -41,6 +41,55 @@ const getPackages = () => ( [
 	},
 ] );
 
+const getPackageTypes = () => ( {
+	user: {
+		label: 'Saved packages',
+		packages: [
+			{
+				id: 'padded-envelope',
+				type: 'envelope',
+				name: 'Large padded envelope',
+				dimensions: '14 x 7 x .25 in',
+			},
+			{
+				id: 'bike-box',
+				type: 'box',
+				name: 'Bike box',
+				dimensions: '34 x 12 x 9.75 in',
+			},
+		],
+	},
+	service: {
+		label: 'USPS Flat Rate Boxes and Envelopes',
+		packages: [
+			{
+				id: 'small-box',
+				type: 'box',
+				name: 'Priority Mail Small Flat Rate Box',
+				dimensions: '14 x 7 x .25 in',
+			},
+			{
+				id: 'medium-box',
+				type: 'box',
+				name: 'Priority Mail Medium Flat Rate Box',
+				dimensions: '14 x 7 x .25 in',
+			},
+			{
+				id: 'large-box',
+				type: 'box',
+				name: 'Priority Mail Large Flat Rate Box',
+				dimensions: '14 x 7 x .25 in',
+			},
+			{
+				id: 'legal-envelope',
+				type: 'box',
+				name: 'Priority Mail Legal Flat Rate Envelope',
+				dimensions: '14 x 7 x .25 in',
+			},
+		],
+	},
+} );
+
 const Settings = ( props ) => {
 	const { settings, form, wooCommerceSettings, settingsActions, schema, layout } = props;
 	const { updateSettingsField, updateSettingsObjectSubField } = settingsActions;
@@ -85,7 +134,7 @@ const Settings = ( props ) => {
 			</CompactCard>
 			<CompactCard>
 				<FormSectionHeading>Packages</FormSectionHeading>
-				<Packages packages={ getPackages() } />
+				<Packages packages={ getPackages() } packageTypes={ getPackageTypes() } />
 			</CompactCard>
 			<CompactCard>
 				<FormButtonsBar>
