610d3e2f1ef3859b8a3fd366ef729d64dfc9c4c5
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-04-13 09:51:33 -0600

    Update Settings actions to use ES6 syntax.

--- client/state/actions/settings.js
+++ client/state/actions/settings.js
@@ -1,49 +1,52 @@
-export const UPDATE_SETTINGS_FIELD = 'UPDATE_SETTINGS_FIELD';
-export const ADD_SETTINGS_OBJECT_FIELD = 'ADD_SETTINGS_OBJECT_FIELD';
-export const REMOVE_SETTINGS_OBJECT_FIELD = 'REMOVE_SETTINGS_OBJECT_FIELD';
-export const UPDATE_SETTINGS_OBJECT_SUB_FIELD = 'UPDATE_SETTINGS_OBJECT_SUB_FIELD';
-export const REMOVE_SETTINGS_OBJECT_SUB_FIELD = 'REMOVE_SETTINGS_OBJECT_SUB_FIELD';
+const UPDATE_SETTINGS_FIELD = 'UPDATE_SETTINGS_FIELD';
+const ADD_SETTINGS_OBJECT_FIELD = 'ADD_SETTINGS_OBJECT_FIELD';
+const REMOVE_SETTINGS_OBJECT_FIELD = 'REMOVE_SETTINGS_OBJECT_FIELD';
+const UPDATE_SETTINGS_OBJECT_SUB_FIELD = 'UPDATE_SETTINGS_OBJECT_SUB_FIELD';
+const REMOVE_SETTINGS_OBJECT_SUB_FIELD = 'REMOVE_SETTINGS_OBJECT_SUB_FIELD';
 
-export function updateSettingsField( key, value ) {
-	return {
-		type: UPDATE_SETTINGS_FIELD,
-		key: key,
-		value: value,
-	};
-}
+const updateSettingsField = ( key, value ) => ( {
+	type: UPDATE_SETTINGS_FIELD,
+	key,
+	value,
+} );
 
-export function addSettingsObjectField( settings_key, key, object ) {
-	return {
-		type: ADD_SETTINGS_OBJECT_FIELD,
-		settings_key,
-		key,
-		object,
-	};
-}
+const addSettingsObjectField = ( settings_key, key, object ) => ( {
+	type: ADD_SETTINGS_OBJECT_FIELD,
+	settings_key,
+	key,
+	object,
+} );
 
-export function removeSettingsObjectField( settings_key, key ) {
-	return {
-		type: REMOVE_SETTINGS_OBJECT_FIELD,
-		settings_key,
-		key,
-	};
-}
+const removeSettingsObjectField = ( settings_key, key ) => ( {
+	type: REMOVE_SETTINGS_OBJECT_FIELD,
+	settings_key,
+	key,
+} );
 
-export function updateSettingsObjectSubField( settings_key, key, subfield_key, value ) {
-	return {
-		type: UPDATE_SETTINGS_OBJECT_SUB_FIELD,
-		settings_key,
-		key,
-		subfield_key,
-		value,
-	};
-}
+const updateSettingsObjectSubField = ( settings_key, key, subfield_key, value ) => ( {
+	type: UPDATE_SETTINGS_OBJECT_SUB_FIELD,
+	settings_key,
+	key,
+	subfield_key,
+	value,
+} );
 
-export function removeSettingsObjectSubField( settings_key, key, subfield_key ) {
-	return {
-		type: REMOVE_SETTINGS_OBJECT_SUB_FIELD,
-		settings_key,
-		key,
-		subfield_key,
-	};
-}
+const removeSettingsObjectSubField = ( settings_key, key, subfield_key ) => ( {
+	type: REMOVE_SETTINGS_OBJECT_SUB_FIELD,
+	settings_key,
+	key,
+	subfield_key,
+} );
+
+export default {
+	UPDATE_SETTINGS_FIELD,
+	ADD_SETTINGS_OBJECT_FIELD,
+	REMOVE_SETTINGS_OBJECT_FIELD,
+	UPDATE_SETTINGS_OBJECT_SUB_FIELD,
+	REMOVE_SETTINGS_OBJECT_SUB_FIELD,
+	updateSettingsField,
+	addSettingsObjectField,
+	removeSettingsObjectField,
+	updateSettingsObjectSubField,
+	removeSettingsObjectSubField,
+};
