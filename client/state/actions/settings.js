985b876d10f2abe46104d9de019e42a4bee59bbb
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-04-13 12:38:36 -0600

    Immediately export Settings actions for clarity’s sake.

--- client/state/actions/settings.js
+++ client/state/actions/settings.js
@@ -1,29 +1,29 @@
-const UPDATE_SETTINGS_FIELD = 'UPDATE_SETTINGS_FIELD';
-const ADD_SETTINGS_OBJECT_FIELD = 'ADD_SETTINGS_OBJECT_FIELD';
-const REMOVE_SETTINGS_OBJECT_FIELD = 'REMOVE_SETTINGS_OBJECT_FIELD';
-const UPDATE_SETTINGS_OBJECT_SUB_FIELD = 'UPDATE_SETTINGS_OBJECT_SUB_FIELD';
-const REMOVE_SETTINGS_OBJECT_SUB_FIELD = 'REMOVE_SETTINGS_OBJECT_SUB_FIELD';
+export const UPDATE_SETTINGS_FIELD = 'UPDATE_SETTINGS_FIELD';
+export const ADD_SETTINGS_OBJECT_FIELD = 'ADD_SETTINGS_OBJECT_FIELD';
+export const REMOVE_SETTINGS_OBJECT_FIELD = 'REMOVE_SETTINGS_OBJECT_FIELD';
+export const UPDATE_SETTINGS_OBJECT_SUB_FIELD = 'UPDATE_SETTINGS_OBJECT_SUB_FIELD';
+export const REMOVE_SETTINGS_OBJECT_SUB_FIELD = 'REMOVE_SETTINGS_OBJECT_SUB_FIELD';
 
-const updateSettingsField = ( key, value ) => ( {
+export const updateSettingsField = ( key, value ) => ( {
 	type: UPDATE_SETTINGS_FIELD,
 	key,
 	value,
 } );
 
-const addSettingsObjectField = ( settings_key, key, object ) => ( {
+export const addSettingsObjectField = ( settings_key, key, object ) => ( {
 	type: ADD_SETTINGS_OBJECT_FIELD,
 	settings_key,
 	key,
 	object,
 } );
 
-const removeSettingsObjectField = ( settings_key, key ) => ( {
+export const removeSettingsObjectField = ( settings_key, key ) => ( {
 	type: REMOVE_SETTINGS_OBJECT_FIELD,
 	settings_key,
 	key,
 } );
 
-const updateSettingsObjectSubField = ( settings_key, key, subfield_key, value ) => ( {
+export const updateSettingsObjectSubField = ( settings_key, key, subfield_key, value ) => ( {
 	type: UPDATE_SETTINGS_OBJECT_SUB_FIELD,
 	settings_key,
 	key,
@@ -31,22 +31,9 @@ const updateSettingsObjectSubField = ( settings_key, key, subfield_key, value ) 
 	value,
 } );
 
-const removeSettingsObjectSubField = ( settings_key, key, subfield_key ) => ( {
+export const removeSettingsObjectSubField = ( settings_key, key, subfield_key ) => ( {
 	type: REMOVE_SETTINGS_OBJECT_SUB_FIELD,
 	settings_key,
 	key,
 	subfield_key,
 } );
-
-export default {
-	UPDATE_SETTINGS_FIELD,
-	ADD_SETTINGS_OBJECT_FIELD,
-	REMOVE_SETTINGS_OBJECT_FIELD,
-	UPDATE_SETTINGS_OBJECT_SUB_FIELD,
-	REMOVE_SETTINGS_OBJECT_SUB_FIELD,
-	updateSettingsField,
-	addSettingsObjectField,
-	removeSettingsObjectField,
-	updateSettingsObjectSubField,
-	removeSettingsObjectSubField,
-};
