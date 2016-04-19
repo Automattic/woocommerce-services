96e67aa570c92ecc56e57b8c662fe8b46cc168d7
Nabeel Sulieman <nabeel@automattic.com>
2016-04-19 14:10:27 -0700

    Factor radio button list into a separate component

---
+++ client/components/radio-buttons/index.js
@@ -0,0 +1,42 @@
+import React, { PropTypes } from 'react';
+import FormFieldset from 'components/forms/form-fieldset';
+import FormLabel from 'components/forms/form-label';
+import FormLegend from 'components/forms/form-legend';
+import FormRadio from 'components/forms/form-radio';
+
+const RadioButton = ( { value, currentValue, setValue, description } ) => {
+	return (
+		<FormLabel>
+			<FormRadio value={ value } checked={ value === currentValue } onChange={ () => setValue( value ) } />
+			<span>{ description }</span>
+		</FormLabel>
+	);
+};
+
+const RadioButtons = ( { layout, schema, value, setValue } ) => {
+	return (
+		<FormFieldset>
+			<FormLegend>{ schema.title }</FormLegend>
+			{ Object.keys( layout.titleMap ).map( key => {
+				return (
+					<RadioButton
+						key={ key }
+						value={ key }
+						currentValue={ value }
+						setValue={ setValue }
+						description={ layout.titleMap[key] }
+					/>
+				);
+			} ) }
+		</FormFieldset>
+	);
+};
+
+RadioButtons.propTypes = {
+	layout: PropTypes.object.isRequired,
+	schema: PropTypes.object.isRequired,
+	value: PropTypes.string.isRequired,
+	setValue: PropTypes.func.isRequired,
+};
+
+export default RadioButtons;
--- client/views/usps-settings/index.js
+++ client/views/usps-settings/index.js
@@ -1,10 +1,8 @@
 import React, { PropTypes } from 'react';
 import FormSectionHeading from 'components/forms/form-section-heading';
 import FormFieldset from 'components/forms/form-fieldset';
-import FormLabel from 'components/forms/form-label';
 import FormLegend from 'components/forms/form-legend';
 import FormButton from 'components/forms/form-button';
-import FormRadio from 'components/forms/form-radio';
 import FormButtonsBar from 'components/forms/form-buttons-bar';
 import CompactCard from 'components/card/compact';
 import ShippingServiceGroups from 'components/shipping-service-groups';
@@ -15,6 +13,7 @@ import * as FormActions from 'state/form/actions';
 import SettingsGroup from './render-group';
 import Notice from 'components/notice';
 import { translate as __ } from 'lib/mixins/i18n';
+import RadioButtons from 'components/radio-buttons';
 
 const handleSaveForm = ( event, props ) => {
 	event.preventDefault();
@@ -29,6 +28,18 @@ const handleSaveForm = ( event, props ) => {
 			props.formActions.setField( 'error', result.data.message );
 		}
 	} );
+};
+
+const findLayoutInfo = ( layout, target ) => {
+	for ( let groupIdx = 0; groupIdx < layout.length; groupIdx++ ) {
+		for ( let fieldIdx = 0; fieldIdx < layout[groupIdx].items.length; fieldIdx++ ) {
+			if ( target === layout[groupIdx].items[fieldIdx].key ) {
+				return layout[groupIdx].items[fieldIdx];
+			}
+		}
+	}
+
+	return null;
 };
 
 const Settings = ( props ) => {
@@ -75,17 +86,12 @@ const Settings = ( props ) => {
 						settingsKey="services"
 					/>
 				</FormFieldset>
-				<FormFieldset>
-					<FormLegend>{ schema.properties.rate_filter.title }</FormLegend>
-					<FormLabel>
-						<FormRadio value="all" checked={ 'all' === settings.rate_filter } onChange={ () => updateSettingsField( 'rate_filter', 'all' ) } />
-						<span>{ __( 'All available rates that apply and let them choose' ) }</span>
-					</FormLabel>
-					<FormLabel>
-						<FormRadio value="cheapest" checked={ 'cheapest' === settings.rate_filter } onChange={ () => updateSettingsField( 'rate_filter', 'cheapest' ) } />
-						<span>{ __( 'Only give them the one, cheapest rate' ) }</span>
-					</FormLabel>
-				</FormFieldset>
+				<RadioButtons
+					layout={ findLayoutInfo( layout, 'rate_filter' ) }
+					schema={ schema.properties.rate_filter }
+					value={ settings.rate_filter }
+					setValue={ ( value ) => updateSettingsField( 'rate_filter', value ) }
+				/>
 			</CompactCard>
 			<CompactCard className="save-button-bar">
 				<FormButtonsBar>
