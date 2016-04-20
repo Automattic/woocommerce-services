8ccb5131ecbe9e25ae9547466966e1a099ee02de
Nabeel Sulieman <nabeel@automattic.com>
2016-04-19 17:51:51 -0700

    Refactor USPS form to be completely data driven Sub components now get state from connect() instead of passing it all down

--- client/views/usps-settings/render-group.js
+++ client/views/usps-settings/render-group.js
@@ -2,34 +2,75 @@ import React, { PropTypes } from 'react';
 import CompactCard from 'components/card/compact';
 import FormSectionHeading from 'components/forms/form-section-heading';
 import RenderItem from './render-item';
+import { connect } from 'react-redux';
+import * as FormActions from 'state/form/actions';
+import { bindActionCreators } from 'redux';
+import SaveForm from 'components/save-form';
 
-const Group = ( { group, schema, settings, updateValue, updateSubValue, updateSubSubValue } ) => (
-	<CompactCard>
-		<FormSectionHeading>{ group.title }</FormSectionHeading>
-		{ group.items.map( item => (
-			<RenderItem
-				key={ item.key ? item.key : item }
-				item={ item }
-				schema={ schema }
-				settings={ settings }
-				updateValue={ updateValue }
-				updateSubValue={ updateSubValue }
-				updateSubSubValue={ updateSubSubValue }
-			/>
-		) ) }
-	</CompactCard>
-);
+const Group = ( { group, schema, wooCommerceSettings, settings, form, formActions, saveFormData } ) => {
+	switch ( group.type ) {
+		case 'fieldset':
+			return (
+				<CompactCard>
+					<FormSectionHeading>{ group.title }</FormSectionHeading>
+					{ group.items.map( item => (
+						<RenderItem
+							key={ item.key ? item.key : item }
+							layout={ item }
+							schema={ schema }
+							wooCommerceSettings={ wooCommerceSettings }
+						/>
+					) ) }
+				</CompactCard>
+			);
+
+		case 'actions':
+			const setIsSaving = ( value ) => formActions.setField( 'isSaving', value );
+			const setSuccess = ( value ) => formActions.setField( 'success', value );
+			const setError = ( value ) => formActions.setField( 'error', value );
+			const dismissSuccess = () => formActions.setField( 'success', false );
+			const saveForm = () => saveFormData( setIsSaving, setSuccess, setError, settings );
+			return (
+				<CompactCard className="save-button-bar">
+					<SaveForm
+						saveForm={ saveForm }
+						isSaving={ form.isSaving }
+						success={ form.success }
+						error={ form.error }
+						dismissSuccess={ dismissSuccess }
+					/>
+				</CompactCard>
+			);
+	}
+};
 
 Group.propTypes = {
 	group: PropTypes.shape( {
-		title: PropTypes.string.isRequired,
+		title: PropTypes.string,
 		items: PropTypes.array.isRequired,
 	} ),
 	schema: PropTypes.object.isRequired,
+	wooCommerceSettings: PropTypes.object.isRequired,
+	saveFormData: PropTypes.func.isRequired,
 	settings: PropTypes.object.isRequired,
-	updateValue: PropTypes.func.isRequired,
-	updateSubValue: PropTypes.func.isRequired,
-	updateSubSubValue: PropTypes.func.isRequired,
+	form: PropTypes.object.isRequired,
+	formActions: PropTypes.object.isRequired,
 };
 
-export default Group;
+function mapStateToProps( state ) {
+	return {
+		settings: state.settings,
+		form: state.form,
+	};
+}
+
+function mapDispatchToProps( dispatch ) {
+	return {
+		formActions: bindActionCreators( FormActions, dispatch ),
+	};
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)( Group );
--- client/views/usps-settings/render-item.js
+++ client/views/usps-settings/render-item.js
@@ -1,35 +1,77 @@
 import React, { PropTypes } from 'react';
 import TextField from 'components/text-field';
+import RadioButtons from 'components/radio-buttons';
+import ShippingServiceGroups from 'components/shipping-service-groups';
+import { connect } from 'react-redux';
+import * as SettingsActions from 'state/settings/actions';
+import * as FormActions from 'state/form/actions';
+import { bindActionCreators } from 'redux';
 
-const RenderField = ( { item, schema, settings, updateValue } ) => {
-	const id = item.key ? item.key : item;
-	if ( 'string' === schema.properties[id].type ) {
-		return (
-			<TextField
-				id={ id }
-				schema={ schema.properties[id] }
-				value={ settings[id] }
-				placeholder={ item.placeholder }
-				updateValue={ value => updateValue( id, value ) }
-			/>
-		)
+const RenderItem = ( { layout, schema, settings, settingsActions, wooCommerceSettings } ) => {
+	const id = layout.key ? layout.key : layout;
+	const updateValue = value => settingsActions.updateSettingsField( id, value );
+	const updateSubSubValue = ( key, subKey, val ) => settingsActions.updateSettingsObjectSubField( id, key, subKey, val );
+
+	switch ( layout.type ) {
+		case 'radios':
+			return (
+				<RadioButtons
+					layout={ layout }
+					schema={ schema.properties[id] }
+					value={ settings[id] }
+					setValue={ updateValue }
+				/>
+			);
+
+		case 'shipping_services':
+			return (
+				<ShippingServiceGroups
+					services={ schema.definitions.services }
+					settings={ settings[id] }
+					currencySymbol={ wooCommerceSettings.currency_symbol }
+					updateValue={ updateSubSubValue }
+					settingsKey={ id }
+				/>
+			);
+
+		default:
+			return (
+				<TextField
+					id={ id }
+					schema={ schema.properties[id] }
+					value={ settings[id] }
+					placeholder={ layout.placeholder }
+					updateValue={ updateValue }
+				/>
+			);
 	}
 };
 
-RenderField.propTypes = {
-	item: PropTypes.oneOfType( [
+RenderItem.propTypes = {
+	layout: PropTypes.oneOfType( [
 		PropTypes.string.isRequired,
-		PropTypes.shape( {
-			key: PropTypes.string.isRequired,
-		} ).isRequired,
+		PropTypes.object.isRequired,
 	] ).isRequired,
-	schema: PropTypes.shape( {
-		properties: PropTypes.object.isRequired,
-	} ).isRequired,
+	schema: PropTypes.object.isRequired,
 	settings: PropTypes.object.isRequired,
-	updateValue: PropTypes.func.isRequired,
-	updateSubValue: PropTypes.func.isRequired,
-	updateSubSubValue: PropTypes.func.isRequired,
+	wooCommerceSettings: PropTypes.object.isRequired,
 };
 
-export default RenderField;
+function mapStateToProps( state ) {
+	return {
+		settings: state.settings,
+		form: state.form,
+	};
+}
+
+function mapDispatchToProps( dispatch ) {
+	return {
+		settingsActions: bindActionCreators( SettingsActions, dispatch ),
+		formActions: bindActionCreators( FormActions, dispatch ),
+	};
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)( RenderItem );
