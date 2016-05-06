21767c4cafeb10af92581483cc4073a2168b1109
Nabeel Sulieman <nabsul@outlook.com>
2016-05-06 12:24:51 -0500

    Add modal read only state and handle it when using presets

--- client/state/form/packages/actions.js
+++ client/state/form/packages/actions.js
@@ -1,6 +1,7 @@
 export const ADD_PACKAGE = 'ADD_PACKAGE';
 export const EDIT_PACKAGE = 'EDIT_PACKAGE';
 export const DISMISS_MODAL = 'DISMISS_MODAL';
+export const SET_MODAL_READONLY = 'SET_MODAL_READONLY';
 export const SAVE_PACKAGE = 'SAVE_PACKAGE';
 export const UPDATE_PACKAGES_FIELD = 'UPDATE_PACKAGES_FIELD';
 export const TOGGLE_OUTER_DIMENSIONS = 'TOGGLE_OUTER_DIMENSIONS';
@@ -16,6 +17,11 @@ export const editPackage = ( packageToEdit ) => ( {
 
 export const dismissModal = () => ( {
 	type: DISMISS_MODAL,
+} );
+
+export const setModalReadOnly = ( value ) => ( {
+	type: SET_MODAL_READONLY,
+	value,
 } );
 
 export const savePackage = ( settings_key, packageData ) => ( {
