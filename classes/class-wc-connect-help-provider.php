0f908f3dc93bdd3bac744966da974c125dd21444
Allen Snook <allen@allendav.com>
2016-05-10 09:25:01 -0700

    Injected dependency for service settings into help provider in order to gain store options during script enqueuing (store options are a required property AND may prove useful for the help page too)

--- classes/class-wc-connect-help-provider.php
+++ classes/class-wc-connect-help-provider.php
@@ -4,65 +4,107 @@ if ( ! class_exists( 'WC_Connect_Help_Provider' ) ) {
 
 	class WC_Connect_Help_Provider {
 
-		public function __construct() {
+		/**
+		 * @var WC_Connect_Service_Schemas_Store
+		 */
+		protected $service_settings_store;
+
+		public function __construct( WC_Connect_Service_Settings_Store $service_settings_store ) {
+
+			$this->service_settings_store = $service_settings_store;
+
 			add_filter( 'woocommerce_admin_status_tabs', array( $this, 'status_tabs' ) );
 			add_action( 'woocommerce_admin_status_content_connect', array( $this, 'page' ) );
+
 		}
 
+		/**
+		 * Filters the WooCommerce System Status Tabs to add connect
+		 *
+		 * @param array $tabs
+		 * @return array
+		 */
 		public function status_tabs( $tabs ) {
+
 			if ( ! is_array( $tabs ) ) {
 				$tabs = array();
 			}
 			$tabs[ 'connect' ] = _x( 'Connect', 'The WooCommerce Connect brandname', 'woocommerce' );
 			return $tabs;
+
 		}
 
+		/**
+		 * Returns the form schema object for the entire help page
+		 *
+		 * @return object
+		 */
 		protected function get_form_schema() {
+
 			$form_schema = new stdClass();
 			$form_schema->properties = new stdClass();
 			return $form_schema;
+
 		}
 
+		/**
+		 * Returns the form layout array for the entire help page
+		 *
+		 * @return array
+		 */
 		protected function get_form_layout() {
+
 			return array(
 				(object) array(
 					'title' => __( 'Health', 'woocommerce' ),
 					'type' => 'fieldset',
-					'items' => array()
+					'items' => array() // TODO
 				),
 				(object) array(
 					'title' => __( 'Services', 'woocommerce' ),
 					'type' => 'fieldset',
-					'items' => array()
+					'items' => array() // TODO
 				),
 				(object) array(
 					'title' => __( 'Debug', 'woocommerce' ),
 					'type' => 'fieldset',
-					'items' => array()
+					'items' => array() // TODO
 				),
 				(object) array(
 					'title' => __( 'Support', 'woocommerce' ),
 					'type' => 'fieldset',
-					'items' => array()
+					'items' => array() // TODO
 				)
 			);
+
 		}
 
+		/**
+		 * Returns the data bootstrap for the help page
+		 *
+		 * @return array
+		 */
 		protected function get_form_data() {
+
 			return array(); // TODO
+
 		}
 
+		/**
+		 * Localizes the bootstrap, enqueues the script and styles for the help page
+		 */
 		public function page() {
+
 			$form_schema = new stdClass();
 			$form_schema->properties = new stdClass();
 
 			$admin_array = array(
-				'storeOptions' => new stdClass(),
+				'storeOptions' => $this->service_settings_store->get_shared_settings(),
 				'formSchema'   => $this->get_form_schema(),
 				'formLayout'   => $this->get_form_layout(),
 				'formData'     => $this->get_form_data(),
-				'callbackURL'  => '',
-				'nonce'        => '',
+				'callbackURL'  => '', // TODO
+				'nonce'        => '', // TODO
 			);
 
 			wp_localize_script( 'wc_connect_admin', 'wcConnectData', $admin_array );
@@ -76,6 +118,7 @@ if ( ! class_exists( 'WC_Connect_Help_Provider' ) ) {
 				<div id="wc-connect-admin-container"></div>
 			<?php
 		}
+
 	}
 
 }
