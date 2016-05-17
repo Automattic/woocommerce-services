4312a2cefeef837b15f722379bbebb365e2efc95
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-05-17 15:29:10 -0400

    Decorate package list items that have errors.

--- client/components/shipping/packages/packages-list-item.js
+++ client/components/shipping/packages/packages-list-item.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import Gridicon from 'components/gridicon';
 import Button from 'components/button';
+import classNames from 'classnames';
 
 const PackagesListItem = ( {
 	index,
@@ -8,29 +9,35 @@ const PackagesListItem = ( {
 	dimensionUnit,
 	onRemove,
 	editPackage,
-} ) => (
-	<div className="wcc-shipping-packages-list-item">
-		<div className="package-type">
-			<Gridicon icon={ data.is_letter ? 'mail' : 'flip-horizontal' } size={ 18 } />
-		</div>
-		<div className="package-name">
-			<a href="#" onClick={ ( event ) => {
+	errors,
+} ) => {
+	const classes = classNames( 'wcc-shipping-packages-list-item', {
+		error: !! errors.length,
+	} );
+	return (
+		<div className={ classes }>
+			<div className="package-type">
+				<Gridicon icon={ data.is_letter ? 'mail' : 'flip-horizontal' } size={ 18 } />
+			</div>
+			<div className="package-name">
+				<a href="#" onClick={ ( event ) => {
 				event.preventDefault();
 				editPackage( Object.assign( {}, data, { index } ) );
 			} }>
-				{ data.name }
-			</a>
+					{ data.name }
+				</a>
+			</div>
+			<div className="package-dimensions">
+				<span>{ data.inner_dimensions } { dimensionUnit }</span>
+			</div>
+			<div className="package-actions">
+				<Button compact borderless className="remove-package" onClick={ onRemove }>
+					<Gridicon icon="cross-small" size={ 18 } />
+				</Button>
+			</div>
 		</div>
-		<div className="package-dimensions">
-			<span>{ data.inner_dimensions } { dimensionUnit }</span>
-		</div>
-		<div className="package-actions">
-			<Button compact borderless className="remove-package" onClick={ onRemove }>
-				<Gridicon icon="cross-small" size={ 18 } />
-			</Button>
-		</div>
-	</div>
-);
+	);
+}
 
 PackagesListItem.propTypes = {
 	index: PropTypes.number.isRequired,
