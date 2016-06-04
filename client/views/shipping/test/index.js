2a7f0f21f04176e1e2ef1e68ba7cca4cee4fba21
Joey Kudish <joachim.kudish@gmail.com>
2016-06-04 13:02:37 -0700

    import the component tree in tests
    in order for test coverage to pick up the component files, we import the
    component tree in this test
    
    of course, the next step will be adding actual tests for this

--- client/views/shipping/test/index.js
+++ client/views/shipping/test/index.js
@@ -1,6 +1,9 @@
-import { expect } from 'chai';
+// this is a bit of a hack
+// in order to load the entire component tree
+// and get test coverage to work
+// TODO: add actual tests for the component tree
+import '../';
 
-// TODO
 describe( 'Shipping Settings', () => {
 	it( 'test suite should work', () => {
 		expect( true ).to.be.ok;
