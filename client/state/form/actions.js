bd58dcea2c6fec4d273983c7abd77ea0aa1cd27e
Daniel Rey <Daniel Rey>
2016-07-05 11:40:22 +0100

    Refactor: cleaned-up the massive nextStep action, hope to improve readability

--- client/state/form/actions.js
+++ client/state/form/actions.js
@@ -1,9 +1,8 @@
 import * as FormValueActions from 'state/form/values/actions';
 import * as NoticeActions from 'state/notices/actions';
 import { getStepFormErrors, getStepFormSuggestions } from 'state/selectors/errors';
-import saveForm from 'lib/save-form';
-import isObject from 'lodash/isObject';
-import isEmpty from 'lodash/isEmpty';
+import { bindActionCreators } from 'redux';
+import { submitForm, autoAdvanceForm } from 'state/form/auto-advance';
 
 export const SET_FORM_PROPERTY = 'SET_FORM_PROPERTY';
 export const GO_TO_STEP = 'GO_TO_STEP';
@@ -31,78 +30,22 @@ export const goToStep = ( stepIndex ) => ( dispatch, getState, { formLayout } ) 
 };
 
 export const nextStep = () => ( dispatch, getState, { callbackURL, nonce, submitMethod, formSchema, formLayout } ) => {
-	const stepLayout = formLayout[ getState().form.currentStep ] || {};
-	const suggestions = getStepFormSuggestions( getState(), formSchema, formLayout );
-	if ( ! isEmpty( suggestions ) ) {
-		if ( getState().form.acceptSuggestion ) {
-			Object.keys( suggestions ).forEach( ( fieldName ) => {
-				dispatch( FormValueActions.updateField( fieldName, suggestions[ fieldName ] ) );
-			} );
-		} else {
-			dispatch( FormValueActions.updateField( stepLayout.bypass_suggestion_flag, true ) );
-		}
-	}
+	const getCurrentStepLayout = () => formLayout[ getState().form.currentStep ] || {};
+	const getFormState = () => getState().form;
+	const updateField = bindActionCreators( FormValueActions.updateField, dispatch );
 
-	const submitForm = ( callback ) => {
-		const setIsSaving = ( value ) => {
-			dispatch( setFormProperty( 'isSaving', value ) );
-			if ( false === value ) {
-				callback();
-			}
-		};
-		const setSuccess = ( value ) => dispatch( setFormProperty( 'success', value ) );
-		const setError = ( value ) => dispatch( setFormProperty( 'errors', value ) );
-
-		dispatch( setFormProperty( 'pristine', true ) );
-		saveForm( setIsSaving, setSuccess, setError, callbackURL, nonce, submitMethod, getState().form.values );
-	};
-
-	const tryAutoAdvance = ( isManualAction ) => {
-		const currentStep = getState().form.currentStep;
-		const currentStepLayout = formLayout[ currentStep ] || {};
-
-		if ( currentStep >= formLayout.length - 1 ) {
-			return;
-		}
-
-		if ( ! isEmpty( getStepFormErrors( getState(), formSchema, formLayout ) ) ) {
-			if ( ! isEmpty( getStepFormSuggestions( getState(), formSchema, formLayout ) ) ) {
-				dispatch( setFormProperty( 'acceptSuggestion', true ) );
-			}
-			return;
-		}
-
-		if ( isManualAction && currentStepLayout.confirmation_flag ) {
-			dispatch( FormValueActions.updateField( currentStepLayout.confirmation_flag, true ) );
-		}
-
-		if ( getState().form.pristine ) {
-			dispatch( goToStep( currentStep + 1 ) );
-			tryAutoAdvance( false );
-			return;
-		}
-
-		submitForm( () => {
-			dispatch( setFormProperty( 'acceptSuggestion', undefined ) );
-			if ( getState().form.errors && ! isObject( getState().form.errors ) ) {
-				dispatch( NoticeActions.errorNotice( getState().form.errors, {
-					duration: 7000,
-				} ) );
-				return;
-			}
-
-			if ( ! isEmpty( getStepFormErrors( getState(), formSchema, formLayout ) ) ) {
-				if ( ! isEmpty( getStepFormSuggestions( getState(), formSchema, formLayout ) ) ) {
-					dispatch( setFormProperty( 'acceptSuggestion', true ) );
-				}
-				return;
-			}
-
-			dispatch( goToStep( currentStep + 1 ) );
-
-			tryAutoAdvance( false );
-		} );
-	};
-
-	tryAutoAdvance( true );
+	autoAdvanceForm( {
+		getStepFormErrors: () => getStepFormErrors( getState(), formSchema, formLayout ),
+		getStepFormSuggestions: () => getStepFormSuggestions( getState(), formSchema, formLayout ),
+		checkAcceptSuggestion: () => dispatch( setFormProperty( 'acceptSuggestion', true ) ),
+		uncheckAcceptSuggestion: () => dispatch( setFormProperty( 'acceptSuggestion', undefined ) ),
+		goToNextStep: () => dispatch( goToStep( getFormState().currentStep + 1 ) ),
+		updateField: updateField,
+		showErrorNotice: ( errors ) => dispatch( NoticeActions.errorNotice( errors, { duration: 7000 } ) ),
+		getFormState: getFormState,
+		submit: () => submitForm( callbackURL, nonce, submitMethod, getFormState().values, bindActionCreators( setFormProperty, dispatch ) ),
+		setConfirmationFlag: () => getCurrentStepLayout().confirmation_flag && updateField( getCurrentStepLayout().confirmation_flag, true ),
+		setBypassSuggestionFlag: () => updateField( getCurrentStepLayout().bypass_suggestion_flag ),
+		isLastStep: () => getFormState().currentStep >= formLayout.length - 1,
+	} );
 };
