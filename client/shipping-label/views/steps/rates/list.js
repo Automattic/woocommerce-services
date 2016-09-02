3769a4841b41b35de8a1fb49aa7c760d8c23109e
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-09-02 14:37:01 -0600

    Render “choose rate” titles above dropdowns.

--- client/shipping-label/views/steps/rates/list.js
+++ client/shipping-label/views/steps/rates/list.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import Dropdown from 'components/dropdown';
 import { translate as __ } from 'lib/mixins/i18n';
+import { sprintf } from 'sprintf-js';
 
 const ShippingRates = ( {
 		id,
@@ -8,13 +9,14 @@ const ShippingRates = ( {
 		availableRates,
 		packages,
 		updateRate,
-		dimensionUnit,
-		weightUnit,
 		currencySymbol,
 		errors,
 	} ) => {
-	const renderTitle = ( pckg ) => {
-		return `${pckg.length} ${dimensionUnit} x ${pckg.width} ${dimensionUnit} x ${pckg.height} ${dimensionUnit} (${pckg.weight} ${weightUnit})`;
+	const renderTitle = ( pckg, idx ) => {
+		if ( 1 === packages.length ) {
+			return __( 'Choose rate' );
+		}
+		return sprintf( __( 'Choose rate: Package %(index)d' ), { index: idx + 1 } );
 	};
 
 	const renderSinglePackage = ( pckg, index ) => {
@@ -27,22 +29,22 @@ const ShippingRates = ( {
 		} );
 
 		return (
-			<li key={ index }>
+			<div key={ index }>
 				<Dropdown
 					id={ id + '_' + index }
 					valuesMap={ valuesMap }
-					title={ renderTitle( pckg ) }
+					title={ renderTitle( pckg, index ) }
 					value={ selectedRate }
 					updateValue={ ( value ) => updateRate( index, value ) }
 					error={ errors[ index ] } />
-			</li>
+			</div>
 		);
 	};
 
 	return (
-		<ul>
+		<div>
 			{ packages.map( renderSinglePackage ) }
-		</ul>
+		</div>
 	);
 };
 
