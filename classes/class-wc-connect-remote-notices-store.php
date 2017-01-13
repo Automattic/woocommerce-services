55c140351b805eea33ab2914401bb4ace578f257
Valerie <valerie@automattic.com>
2017-01-12 19:22:55 -0500

    Show the saved remote notices in the dashboard

--- classes/class-wc-connect-remote-notices-store.php
+++ classes/class-wc-connect-remote-notices-store.php
@@ -21,10 +21,15 @@ if ( ! class_exists( 'WC_Connect_Remote_Notices_Store' ) ) {
 
 		public function __construct( WC_Connect_API_Client $api_client ) {
 			$this->api_client = $api_client;
+			add_action( 'admin_notices', array( $this, 'display_notices' ) );
 		}
 
 		public function set_notices( $notices ) {
 			update_option( 'wc_connect_remote_notices', $notices );
+		}
+
+		public function get_notices() {
+			return get_option( 'wc_connect_remote_notices', null );
 		}
 
 		public function fetch_notices_from_connect_server() {
@@ -36,6 +41,26 @@ if ( ! class_exists( 'WC_Connect_Remote_Notices_Store' ) ) {
 				'button' => wp_strip_all_tags( $response->button ),
 			);
 			$this->set_notices( $notices );
+		}
+
+		public function display_notices() {
+			// TODO: show more than one notice
+			$notices = $this->get_notices();
+			$this->display_notice( $notices->title , $notices->body, $notices->button );
+		}
+
+		public function display_notice( $title, $body, $button ) {
+			?>
+			<div class="notice notice-info">
+				<h1><?php echo esc_html( $title ); ?></h1>
+				<p><?php echo wp_kses( $body, $this->allowed_html ); ?></p>
+				<p>
+					<a class="button-primary">
+						<?php echo esc_html( $button ); ?>
+					</a>
+				</p>
+			</div>
+			<?php
 		}
 	}
 }
