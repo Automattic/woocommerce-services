fc09c2e41efb8a9db0a3c7ff9a9db844194d09d1
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-01-23 11:51:46 -0400

    Cleaned-up styles on the info tooltip

--- client/components/info-tooltip/index.js
+++ client/components/info-tooltip/index.js
@@ -1,7 +1,7 @@
 import React, { PropTypes, Component } from 'react';
-
 import Tooltip from 'components/tooltip';
 import Gridicon from 'components/gridicon';
+import classNames from 'classnames';
 
 export default class InfoTooltip extends Component {
 	static propTypes = {
@@ -39,8 +39,7 @@ export default class InfoTooltip extends Component {
 			<span
 				onMouseEnter={ this.openTooltip }
 				onMouseLeave={ this.closeTooltip }
-				className={ this.props.className }
-				style={ { cursor: 'help', color: 'black' } } >
+				className={ classNames( 'info-tooltip', this.props.className ) } >
 				<Gridicon ref="icon" icon="info-outline" size={ 18 } />
 				{ this.state.showTooltip &&
 					<Tooltip
