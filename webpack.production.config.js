d6bf66bdc6c3042ca1ef08bef2b4a8fc6cafda34
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-02-18 22:59:42 +0000

    Added more aggresive UglifyJS compression options
    Shaves about 4KB from the JS bundle

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -12,8 +12,16 @@ config.plugins.push( new webpack.optimize.OccurrenceOrderPlugin( false ) );
 
 config.plugins.push( new webpack.optimize.UglifyJsPlugin( {
 	compress: {
+		screw_ie8: true,
 		warnings: false,
 		unsafe: true,
+	},
+	mangle: {
+		screw_ie8: true,
+	},
+	output: {
+		comments: false,
+		screw_ie8: true,
 	},
 } ) );
 
