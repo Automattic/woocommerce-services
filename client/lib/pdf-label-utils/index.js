c224461151150ed30ac092db3f0e8888aec7a6ca
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-02-25 22:38:58 +0000

    Don't show preview PDF when the browser doesn't support PDFs at all

--- client/lib/pdf-label-utils/index.js
+++ client/lib/pdf-label-utils/index.js
@@ -1,6 +1,7 @@
 import { translate as __ } from 'lib/mixins/i18n';
 import querystring from 'querystring';
 import _ from 'lodash';
+import pdfSupport from 'lib/utils/pdf-support';
 
 const PAPER_SIZES = {
 	a4: {
@@ -45,5 +46,5 @@ export const getPrintURL = ( paperSize, labels, { labelsPrintURL, nonce } ) => {
 };
 
 export const getPreviewURL = ( paperSize, labels, { labelsPreviewURL, nonce } ) => {
-	return _getPDFURL( paperSize, labels, labelsPreviewURL, nonce );
+	return pdfSupport() ? _getPDFURL( paperSize, labels, labelsPreviewURL, nonce ) : null;
 };
