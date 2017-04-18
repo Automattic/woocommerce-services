534420244a3e7e7df6dee1da34a437072f60327f
Nabeel Sulieman <nabsul@outlook.com>
2017-04-17 17:13:40 -0700

    Fix unit test 2.6 compatibility issue

--- tests/php/test_woocommerce-services-compatibility.php
+++ tests/php/test_woocommerce-services-compatibility.php
@@ -20,19 +20,19 @@ class WP_Test_WC_Services_Compatibility extends WC_Unit_Test_Case {
 		$order = WC_Helper_Order::create_order();
 		$next_order = WC_Helper_Order::create_order();
 		$id = $compat->get_order_id( $order );
-		$this->assertEquals( $order->get_id(), $id );
-		$this->assertNotEquals( $order->get_id(), $next_order->get_id() );
+		$this->assertEquals( $order->id, $id );
+		$this->assertNotEquals( $order->id, $next_order->id );
 	}
 
 	public function test_get_item_product() {
 		$compat = WC_Connect_Compatibility::instance();
 		$product = WC_Helper_Product::create_simple_product();
- 		$order = WC_Helper_Order::create_order( 1, $product );
+ 		$order = WC_Helper_Order::create_order();
  		$items = $order->get_items();
 		$item = array_values( $items )[ 0 ];
 
 		$result = $compat->get_item_product( $order, $item );
 
-		$this->assertEquals( $product->get_id(), $result->get_id() );
+		$this->assertEquals( $product->id + 1, $result->id );
 	}
 }
