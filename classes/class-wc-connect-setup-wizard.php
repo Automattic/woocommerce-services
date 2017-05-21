d4e3a89cf5cc1dd802605f8d2d1f635e36732809
Valerie <valerie@automattic.com>
2017-05-20 20:47:46 -0400

    Add button to connect to Jetpack
    Before displaying the button, make sure that:
    - the store locale is in the US
    - shipping is enabled for the store
    
    There are no checks yet to make sure JP
    is installed and activated

--- classes/class-wc-connect-setup-wizard.php
+++ classes/class-wc-connect-setup-wizard.php
@@ -39,15 +39,38 @@ if ( ! class_exists( 'WC_Connect_Setup_Wizard' ) ) {
 				$shipping_enabled = 'disabled' === get_option( 'woocommerce_ship_to_countries' )
 					? 'Disabled'
 					: 'Enabled';
+				$is_shipping_enabled = 'disabled' !== get_option( 'woocommerce_ship_to_countries' );
+				$default_country = get_option( 'woocommerce_default_country' );
 			?>
 			<h1><?php _e( 'Add a WooCommerce shipping service to a Zone' ,'woocommerce-services' ) ?></h1>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula fermentum mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
 			<h2>Things we know</h2>
 			<p><strong>Shipping:</strong> <?php echo esc_html( $shipping_enabled ) ?></p>
-			<p><strong>Default country:</strong> <?php echo esc_html( get_option( 'woocommerce_default_country' ) ) ?></p>
+			<p><strong>Default country:</strong> <?php echo esc_html( $default_country ) ?></p>
 			<p><strong>Currency:</strong> <?php echo esc_html( get_option( 'woocommerce_currency' ) ) ?></p>
 			<p><strong>Weight unit:</strong> <?php echo esc_html( get_option( 'woocommerce_weight_unit' ) ) ?></p>
 			<p><strong>Dimension unit:</strong> <?php echo esc_html( get_option( 'woocommerce_dimension_unit' ) ) ?></p>
+			<?php
+			if (
+				'us' === strtolower( substr( $default_country, 0, 2 ) ) &&
+				true === $is_shipping_enabled
+			) {
+				// For now, connect to Jetpack
+				// TODO: make sure Jetpack is installed & activated
+				// The wizard setup URL is hardcoded in WC
+				$redirect_url = admin_url( 'index.php?page=wc-setup&step=wcs_shipping' );
+			?>
+				<p class="wc-setup-actions">
+					<a
+						class="button-primary button button-large button-next"
+						href="<?php echo esc_url( Jetpack::init()->build_connect_url( true, $redirect_url, 'wcs' ) ); ?>"
+					>
+						<?php esc_html_e( 'Connect to Jetpack', 'woocommerce-services' ); ?>
+					</a>
+				<p>
+			<?php
+			}
+			?>
 			<p class="wc-setup-actions step">
 				<a class="button-primary button button-large button-next" href="<?php echo esc_url( $setup_wizard->get_next_step_link() ); ?>">Continue</a>
 			</p>
