b53b68ac6d26267bb7ffc1ae20f57bcc939d7bba
Valerie <valerie@automattic.com>
2017-05-22 11:38:13 -0400

    Check if Jetpack is connected and activated first

--- classes/class-wc-connect-setup-wizard.php
+++ classes/class-wc-connect-setup-wizard.php
@@ -45,29 +45,69 @@ if ( ! class_exists( 'WC_Connect_Setup_Wizard' ) ) {
 			<h1><?php _e( 'Add a WooCommerce shipping service to a Zone' ,'woocommerce-services' ) ?></h1>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula fermentum mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
 			<h2>Things we know</h2>
-			<p><strong>Shipping:</strong> <?php echo esc_html( $shipping_enabled ) ?></p>
-			<p><strong>Default country:</strong> <?php echo esc_html( $default_country ) ?></p>
-			<p><strong>Currency:</strong> <?php echo esc_html( get_option( 'woocommerce_currency' ) ) ?></p>
-			<p><strong>Weight unit:</strong> <?php echo esc_html( get_option( 'woocommerce_weight_unit' ) ) ?></p>
-			<p><strong>Dimension unit:</strong> <?php echo esc_html( get_option( 'woocommerce_dimension_unit' ) ) ?></p>
+			<p><strong>Shipping: </strong>
+				<?php echo esc_html( $shipping_enabled ) ?>
+			</p>
+			<p><strong>Default country: </strong>
+				<?php echo esc_html( $default_country ) ?>
+			</p>
+			<p><strong>Currency: </strong>
+				<?php echo esc_html( get_option( 'woocommerce_currency' ) ); ?>
+			</p>
+			<p><strong>Weight unit: </strong>
+				<?php echo esc_html( get_option( 'woocommerce_weight_unit' ) ); ?>
+			</p>
+			<p><strong>Dimension unit: </strong>
+				<?php echo esc_html( get_option( 'woocommerce_dimension_unit' ) ); ?>
+			</p>
 			<?php
 			if (
 				'us' === strtolower( substr( $default_country, 0, 2 ) ) &&
 				true === $is_shipping_enabled
 			) {
-				// For now, connect to Jetpack
-				// TODO: make sure Jetpack is installed & activated
+				$is_jetpack_installed = false;
+				$is_jetpack_activated = false;
+				$is_jetpack_connected = false;
+
+				if ( class_exists( 'Jetpack_Data' ) ) {
+					$is_jetpack_installed = true;
+					$is_jetpack_activated = true;
+
+					$user_token = Jetpack_Data::get_access_token( JETPACK_MASTER_USER );
+
+					$is_jetpack_connected = $user_token && is_object( $user_token ) && isset( $user_token->external_user_id );
+				} else {
+					if ( 0 === validate_plugin( 'jetpack/jetpack.php' ) ) {
+						$is_jetpack_installed = true;
+					}
+				}
+
 				// The wizard setup URL is hardcoded in WC
 				$redirect_url = admin_url( 'index.php?page=wc-setup&step=wcs_shipping' );
-			?>
-				<p class="wc-setup-actions">
-					<a
-						class="button-primary button button-large button-next"
-						href="<?php echo esc_url( Jetpack::init()->build_connect_url( true, $redirect_url, 'wcs' ) ); ?>"
-					>
-						<?php esc_html_e( 'Connect to Jetpack', 'woocommerce-services' ); ?>
-					</a>
-				<p>
+				?>
+				<p><strong>Jetpack is: </strong>
+					<?php $jetpack_statuses = array(
+						$is_jetpack_installed ? 'installed' : 'not installed',
+						$is_jetpack_activated ? 'activated' : 'not activated',
+						$is_jetpack_connected ? 'connected' : 'not connected'
+					);
+					echo esc_html( implode( ', ', $jetpack_statuses ) );
+					?>
+				</p>
+				<?php
+				if ( $is_jetpack_installed && $is_jetpack_activated && ! $is_jetpack_connected ) {
+				?>
+					<p class="wc-setup-actions">
+						<a
+							class="button-primary button button-large button-next"
+							href="<?php echo esc_url( Jetpack::init()->build_connect_url( true, $redirect_url, 'wcs' ) ); ?>"
+						>
+							<?php esc_html_e( 'Connect to Jetpack', 'woocommerce-services' ); ?>
+						</a>
+					<p>
+				<?php
+				}
+				?>
 			<?php
 			}
 			?>
