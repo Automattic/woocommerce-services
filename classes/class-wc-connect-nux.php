41ae1172397d86a160bdb45f7310ab9697d3f9d2
Nabeel Sulieman <nabeel.sulieman@live.com>
2017-06-16 15:22:02 -0700

    remove extra tabs

--- classes/class-wc-connect-nux.php
+++ classes/class-wc-connect-nux.php
@@ -351,19 +351,19 @@ if ( ! class_exists( 'WC_Connect_Nux' ) ) {
 					</p>
 					<?php if ( $content['should_show_jp'] ) : ?>
 						<p><?php
-							/* translators: %1$s example values include "Install Jetpack and CONNECT >", "Activate Jetpack and CONNECT >", "CONNECT >" */
-							printf(
-								wp_kses( __( 'By clicking "%1$s", you agree to the <a href="%2$s">Terms of Service</a> and understand that some of your data will be passed to external servers. You can find more information about how your data is handled <a href="%3$s">here</a>', 'woocommerce-services' ),
-									array(
-										'a' => array(
-											'href' => array(),
-										),
-									) ),
-								esc_html( $content['button_text'] ),
-								'https://woocommerce.com/terms-conditions/',
-								'https://woocommerce.com/terms-conditions/services-privacy/'
-							);
-							?></p>
+						/* translators: %1$s example values include "Install Jetpack and CONNECT >", "Activate Jetpack and CONNECT >", "CONNECT >" */
+						printf(
+							wp_kses( __( 'By clicking "%1$s", you agree to the <a href="%2$s">Terms of Service</a> and understand that some of your data will be passed to external servers. You can find more information about how your data is handled <a href="%3$s">here</a>', 'woocommerce-services' ),
+								array(
+									'a' => array(
+										'href' => array(),
+									),
+								) ),
+							esc_html( $content['button_text'] ),
+							'https://woocommerce.com/terms-conditions/',
+							'https://woocommerce.com/terms-conditions/services-privacy/'
+						);
+						?></p>
 					<?php endif; ?>
 					<?php if ( isset( $content['button_link'] ) ) : ?>
 						<a
