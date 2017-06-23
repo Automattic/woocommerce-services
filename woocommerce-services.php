61e7f07fc32b93883b2bc9edb29924f87c393dbf
Nabeel Sulieman <nabeel.sulieman@live.com>
2017-06-23 07:45:59 -0700

    Add check for Jetpack connection status in plugin init

--- woocommerce-services.php
+++ woocommerce-services.php
@@ -363,8 +363,10 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 			add_action( 'admin_init', array( $this, 'admin_enqueue_scripts' ) );
 			add_action( 'admin_init', array( $this->nux, 'set_up_nux_notices' ) );
 
-			// if the TOS has not bee accepted, do nothing else.
-			if ( true !== WC_Connect_Options::get_option( 'tos_accepted' ) ) {
+			// if the TOS has not bee accepted or Jetpack is not Connected do nothing else.
+			$jetpack_status = $this->nux->get_jetpack_install_status();
+			$is_jetpack_connected = WC_Connect_Nux::JETPACK_CONNECTED === $jetpack_status || WC_Connect_Nux::JETPACK_DEV === $jetpack_status;
+			if ( true !== WC_Connect_Options::get_option( 'tos_accepted' ) && $is_jetpack_connected ) {
 				return;
 			}
 
