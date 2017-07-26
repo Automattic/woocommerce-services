e7ece04e87b6724f123aec53ae8796ff7789ce7a
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-07-26 13:07:07 +0100

    Refactored the label purchase code so it uses the new request lib instead of saveForm.

--- client/api/url.js
+++ client/api/url.js
@@ -3,3 +3,5 @@ const namespace = 'wc/v1/connect/';
 export const accountSettings = () => namespace + 'account/settings';
 
 export const packages = () => namespace + 'packages';
+
+export const purchaseLabel = ( orderId ) => namespace + 'label/' + orderId;
