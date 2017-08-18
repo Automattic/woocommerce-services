3421a7a5b6a2790f2dec6809c10f7cd1e5f04994
Jeff Stieler <jeff.m.stieler@gmail.com>
2017-08-17 19:19:35 -0600

    Handle comma separated values for test label captions.

--- classes/class-wc-rest-connect-shipping-label-preview-controller.php
+++ classes/class-wc-rest-connect-shipping-label-preview-controller.php
@@ -19,8 +19,10 @@ class WC_REST_Connect_Shipping_Label_Preview_Controller extends WC_REST_Connect_
 		$this->settings_store->set_preferred_paper_size( $params[ 'paper_size' ] );
 		$params[ 'carrier' ] = 'usps';
 		$params[ 'labels' ] = array();
-		foreach ( $raw_params[ 'captions' ] as $caption ) {
-			$params[ 'labels' ][] = array( 'caption' => $caption );
+		$captions = empty( $raw_params[ 'caption_csv' ] ) ? array() : explode( ',', $raw_params[ 'caption_csv' ] );
+
+		foreach ( $captions as $caption ) {
+			$params[ 'labels' ][] = array( 'caption' => urldecode( $caption ) );
 		}
 
 		$raw_response = $this->api_client->get_labels_preview_pdf( $params );
