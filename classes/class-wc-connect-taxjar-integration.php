a2772b88b590eb7ca9d596f888c82b47eab71de2
Jeff Stieler <jeff.m.stieler@gmail.com>
2017-08-29 16:26:50 -0600

    Export/backup and clear existing tax rates.
    Direct port from Taxjar’s plugin activation routine.

--- classes/class-wc-connect-taxjar-integration.php
+++ classes/class-wc-connect-taxjar-integration.php
@@ -15,6 +15,131 @@ class WC_Connect_TaxJar_Integration {
 
 	public function init() {
 		// TODO: check if TaxJar plugin is enabled, abort if so
-		// TODO: backup existing tax rates if enabled
+
+		// TODO: check if WCS Taxes are enabled before backup existing tax rates
+		$this->backup_existing_tax_rates();
+	}
+
+	/**
+	 * Exports existing tax rates to a CSV and clears the table.
+	 *
+	 * Ported from TaxJar's plugin.
+	 * See: https://github.com/taxjar/taxjar-woocommerce-plugin/blob/42cd4cd0/taxjar-woocommerce.php#L75
+	 */
+	public function backup_existing_tax_rates() {
+		global $wpdb;
+
+		// Export Tax Rates
+		$rates = $wpdb->get_results( $wpdb->prepare(
+			"SELECT * FROM {$wpdb->prefix}woocommerce_tax_rates
+			ORDER BY tax_rate_order
+			LIMIT %d, %d
+			",
+			0,
+			10000
+		) );
+
+		ob_start();
+		$header =
+			__( 'Country Code', 'woocommerce' ) . ',' .
+			__( 'State Code', 'woocommerce' ) . ',' .
+			__( 'ZIP/Postcode', 'woocommerce' ) . ',' .
+			__( 'City', 'woocommerce' ) . ',' .
+			__( 'Rate %', 'woocommerce' ) . ',' .
+			__( 'Tax Name', 'woocommerce' ) . ',' .
+			__( 'Priority', 'woocommerce' ) . ',' .
+			__( 'Compound', 'woocommerce' ) . ',' .
+			__( 'Shipping', 'woocommerce' ) . ',' .
+			__( 'Tax Class', 'woocommerce' ) . "\n";
+
+		echo $header;
+
+		foreach ( $rates as $rate ) {
+			if ( $rate->tax_rate_country ) {
+				echo esc_attr( $rate->tax_rate_country );
+			} else {
+				echo '*';
+			}
+
+			echo ',';
+
+			if ( $rate->tax_rate_country ) {
+				echo esc_attr( $rate->tax_rate_state );
+			} else {
+				echo '*';
+			}
+
+			echo ',';
+
+			$locations = $wpdb->get_col( $wpdb->prepare( "SELECT location_code FROM {$wpdb->prefix}woocommerce_tax_rate_locations WHERE location_type='postcode' AND tax_rate_id = %d ORDER BY location_code", $rate->tax_rate_id ) );
+
+			if ( $locations ) {
+				echo esc_attr( implode( '; ', $locations ) );
+			} else {
+				echo '*';
+			}
+
+			echo ',';
+
+			$locations = $wpdb->get_col( $wpdb->prepare( "SELECT location_code FROM {$wpdb->prefix}woocommerce_tax_rate_locations WHERE location_type='city' AND tax_rate_id = %d ORDER BY location_code", $rate->tax_rate_id ) );
+			if ( $locations ) {
+				echo esc_attr( implode( '; ', $locations ) );
+			} else {
+				echo '*';
+			}
+
+			echo ',';
+
+			if ( $rate->tax_rate ) {
+				echo esc_attr( $rate->tax_rate );
+			} else {
+				echo '0';
+			}
+
+			echo ',';
+
+			if ( $rate->tax_rate_name ) {
+				echo esc_attr( $rate->tax_rate_name );
+			} else {
+				echo '*';
+			}
+
+			echo ',';
+
+			if ( $rate->tax_rate_priority ) {
+				echo esc_attr( $rate->tax_rate_priority );
+			} else {
+				echo '1';
+			}
+
+			echo ',';
+
+			if ( $rate->tax_rate_compound ) {
+				echo esc_attr( $rate->tax_rate_compound );
+			} else {
+				echo '0';
+			}
+
+			echo ',';
+
+			if ( $rate->tax_rate_shipping ) {
+				echo esc_attr( $rate->tax_rate_shipping );
+			} else {
+				echo '0';
+			}
+
+			echo ',';
+
+			echo "\n";
+		} // End foreach().
+
+		$csv = ob_get_contents();
+		ob_end_clean();
+		$upload_dir = wp_upload_dir();
+		file_put_contents( $upload_dir['basedir'] . '/taxjar-wc_tax_rates-' . date( 'm-d-Y' ) . '-' . time() . '.csv', $csv );
+
+		// Delete all tax rates
+		$wpdb->query( 'TRUNCATE ' . $wpdb->prefix . 'woocommerce_tax_rates' );
+		$wpdb->query( 'TRUNCATE ' . $wpdb->prefix . 'woocommerce_tax_rate_locations' );
 	}
 }
