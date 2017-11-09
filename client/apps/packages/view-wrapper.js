94eacbdcfb9de4bad5512fe20ac66c71d6a83c2a
Jeff Stieler <jeff.m.stieler@gmail.com>
2017-11-09 12:42:32 -0500

    Hook package manager save action up to GlobalNotices.

--- client/apps/packages/view-wrapper.js
+++ client/apps/packages/view-wrapper.js
@@ -13,21 +13,30 @@ import { localize } from 'i18n-calypso';
 import Button from 'components/button';
 import Packages from 'woocommerce/woocommerce-services/views/packages';
 import { submit } from 'woocommerce/woocommerce-services/state/packages/actions';
+import * as NoticeActions from 'state/notices/actions';
+import GlobalNotices from 'components/global-notices';
+import notices from 'notices';
 
 const PackagesWrapper = ( props ) => {
-	const { translate } = props;
+	const { translate, noticeActions } = props;
 
-	const onSave = () => props.submit( 1, () => console.log( 'ok' ), () => console.log( 'not ok' ) );
+	const onSaveSuccess = () => noticeActions.successNotice( translate( 'Your packages have been saved.' ), { duration: 5000 } );
+	const onSaveFailure = () => noticeActions.errorNotice( translate( 'Unable to save your packages. Please try again.' ) );
+	const onSaveChanges = () => props.submit( 1, onSaveSuccess, onSaveFailure );
 
 	return (
 		<div>
+			<GlobalNotices notices={ notices.list } />
 			<Packages />
-			<Button primary onClick={ onSave }>{ translate( 'Save' ) }</Button>
+			<Button primary onClick={ onSaveChanges }>{ translate( 'Save' ) }</Button>
 		</div>
 	);
 };
 
 export default connect(
 	( state ) => state,
-	( dispatch ) => bindActionCreators( { submit }, dispatch )
+	( dispatch ) => ( {
+		...bindActionCreators( { submit }, dispatch ),
+		noticeActions: bindActionCreators( NoticeActions, dispatch ),
+	} ),
 )( localize( PackagesWrapper ) );
