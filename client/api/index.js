Automattic/woocommerce-services

commit 653853a1ba548627eb23314156ada418964212ed

Tweaked "request.js" to make it 100% compatible with what Calypso expects.

client/api/request.js
@@ -15,7 +15,7 @@ export const setNonce = ( _nonce ) => nonce = _nonce;
 let baseURL;
 export const setBaseURL = ( _baseURL ) => baseURL = _baseURL;
 
-const _request = ( url, data, method ) => {
+const _request = ( url, data, method, namespace = '' ) => {
 	const request = {
 		method,
 		credentials: 'same-origin',
@@ -28,7 +28,10 @@ const _request = ( url, data, method ) => {
 		request.body = JSON.stringify( data );
 	}
 
-	return fetch( baseURL + url, request ).then( ( response ) => {
+	if ( namespace && ! namespace.endsWith( '/' ) ) {
+		namespace += '/';
+	}
+	return fetch( baseURL + namespace + url, request ).then( ( response ) => {
 		return parseJson( response ).then( ( json ) => {
 			if ( json.success ) {
 				return json;
@@ -46,9 +49,10 @@ const _request = ( url, data, method ) => {
 	} );
 };
 
-export const post = ( url, data ) => _request( url, data, 'POST' );
-
-export const get = ( url ) => _request( url, null, 'GET' );
+export default () => ( {
+	post: ( url, data, namespace ) => _request( url, data, 'POST', namespace ),
+	get: ( url, namespace ) => _request( url, null, 'GET', namespace ),
+} );
 
 export const createGetUrlWithNonce = ( url, queryString ) => {
 	if ( queryString ) {

commit f33936a4c07129e1f331085e83b984ad96e449ea

Fix non-Calypso-sourced component usage of request.js.
Error introduced in 653853a1ba548627eb23314156ada418964212ed.

client/api/index.js
@@ -1,7 +1,7 @@
 /**
  * Internal dependencies
  */
-import * as request from './request';
+import request from './request';
 
 export * as url from './url';
 
@@ -13,8 +13,8 @@ const handleError = ( jsonError ) => {
 	throw JSON.stringify( jsonError );
 };
 
-export const post = ( url, data ) => request.post( url, data ).catch( handleError );
+export const post = ( url, data ) => request().post( url, data ).catch( handleError );
 
-export const get = ( url ) => request.get( url ).catch( handleError );
+export const get = ( url ) => request().get( url ).catch( handleError );
 
-export const createGetUrlWithNonce = ( url, queryString ) => request.createGetUrlWithNonce( url, queryString );
+export const createGetUrlWithNonce = ( url, queryString ) => request().createGetUrlWithNonce( url, queryString );

client/api/request.js
@@ -49,12 +49,7 @@ const _request = ( url, data, method, namespace = '' ) => {
 	} );
 };
 
-export default () => ( {
-	post: ( url, data, namespace ) => _request( url, data, 'POST', namespace ),
-	get: ( url, namespace ) => _request( url, null, 'GET', namespace ),
-} );
-
-export const createGetUrlWithNonce = ( url, queryString ) => {
+const createGetUrlWithNonce = ( url, queryString ) => {
 	if ( queryString ) {
 		if ( startsWith( queryString, '?' ) ) {
 			queryString = queryString.substring( 1 );
@@ -69,3 +64,9 @@ export const createGetUrlWithNonce = ( url, queryString ) => {
 
 	return `${ baseURL }${ url }${ queryStart }_wpnonce=${ nonce }${ queryString }`;
 };
+
+export default () => ( {
+	post: ( url, data, namespace ) => _request( url, data, 'POST', namespace ),
+	get: ( url, namespace ) => _request( url, null, 'GET', namespace ),
+	createGetUrlWithNonce,
+} );