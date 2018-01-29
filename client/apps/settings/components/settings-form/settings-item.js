ee69abfab62b71ac0bf516ac16ae72cbb09844f2
Jeff Stieler <jeff.m.stieler@gmail.com>
2018-01-29 16:24:58 -0700

    Fix react/jsx-no-bind lint error in SettingsItem.

--- client/apps/settings/components/settings-form/settings-item.js
+++ client/apps/settings/components/settings-form/settings-item.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { translate as __ } from 'i18n-calypso';
 
@@ -19,167 +19,187 @@ import Dropdown from 'components/dropdown';
 import ShippingServiceGroups from '../shipping-services';
 import FormLegend from 'components/forms/form-legend';
 
-const SettingsItem = ( {
-	form,
-	layout,
-	schema,
-	formValueActions,
-	storeOptions,
-	errors,
-	saveForm,
-} ) => {
-	const id = layout.key ? layout.key : layout;
-	const updateValue = ( value ) => formValueActions.updateField( id, value );
-	const updateSubValue = ( key, val ) => formValueActions.updateField( [ id ].concat( key ), val );
-	const fieldValue = form.values[ id ];
-	const fieldSchema = schema.properties[ id ];
-	const fieldType = layout.type || fieldSchema.type || '';
-	const fieldError = errors[ '' ] ? ( errors[ '' ].value || layout.validation_hint || '' ) : false;
+class SettingsItem extends Component {
+	static propTypes = {
+		layout: PropTypes.oneOfType( [
+			PropTypes.string.isRequired,
+			PropTypes.object.isRequired,
+		] ).isRequired,
+		schema: PropTypes.object.isRequired,
+		storeOptions: PropTypes.object.isRequired,
+		form: PropTypes.object.isRequired,
+		formValueActions: PropTypes.object.isRequired,
+		errors: PropTypes.object,
+		saveForm: PropTypes.func,
+	};
 
-	switch ( fieldType ) {
-		case 'radios':
-			return (
-				<RadioButtons
-					valuesMap={ layout.titleMap }
-					title={ fieldSchema.title }
-					description={ fieldSchema.description }
-					value={ fieldValue }
-					setValue={ updateValue }
-					error={ fieldError }
-				/>
-			);
+	updateValue = ( value ) => {
+		const {
+			layout,
+			formValueActions,
+		} = this.props;
+		const id = layout.key ? layout.key : layout;
 
-		case 'dropdown':
-			return (
-				<Dropdown
-					id={ id }
-					valuesMap={ layout.titleMap }
-					title={ fieldSchema.title }
-					description={ fieldSchema.description }
-					value={ fieldValue }
-					updateValue={ updateValue }
-					error={ fieldError }
-				/>
-			);
-
-		case 'shipping_services':
-			return (
-				<ShippingServiceGroups
-					services={ schema.definitions.services }
-					title={ fieldSchema.title }
-					description={ fieldSchema.description }
-					settings={ fieldValue }
-					currencySymbol={ storeOptions.currency_symbol }
-					updateValue={ updateSubValue }
-					settingsKey={ id }
-					errors={ errors }
-					generalError={ fieldError }
-				/>
-			);
-
-		case 'packages':
-			return (
-				<div>
-					<FormLegend>{ __( 'Saved Packages' ) }</FormLegend>
-					{ __(
-						'Add and edit saved packages using the {{a}}Packaging Manager{{/a}}.',
-						{
-							components: {
-								a: <a href="admin.php?page=wc-settings&tab=shipping&section=package-settings" />,
-							},
-						}
-					) }
-				</div>
-			);
-
-		case 'indicators':
-			return (
-				<Indicators
-					title={ fieldSchema.title }
-					subtitle={ fieldSchema.subtitle }
-					indicators={ [ fieldValue ] }
-				/>
-			);
-
-		case 'text':
-			return (
-				<Text
-					id={ id }
-					title={ layout.title }
-					className={ layout.class }
-					value={ fieldValue || layout.description }
-				/>
-			);
-
-		case 'textarea':
-			return (
-				<TextArea
-					error={ fieldError }
-					id={ id }
-					readonly={ Boolean( layout.readonly ) }
-					placeholder={ layout.placeholder }
-					title={ fieldSchema.title }
-					description={ fieldSchema.description }
-					updateValue={ updateValue }
-					value={ fieldValue }
-				/>
-			);
-
-		case 'boolean':
-			const onToggle = () => saveForm( schema );
-			return (
-				<Toggle
-					checked={ Boolean( fieldValue ) }
-					id={ id }
-					title={ fieldSchema.title }
-					description={ fieldSchema.description }
-					trueText={ fieldSchema.trueText }
-					falseText={ fieldSchema.falseText }
-					saveOnToggle={ Boolean( fieldSchema.saveOnToggle ) }
-					saveForm={ onToggle }
-					updateValue={ updateValue }
-				/>
-			);
-
-		case 'number':
-			return (
-				<NumberField
-					id={ id }
-					title={ fieldSchema.title }
-					description={ fieldSchema.description }
-					value={ fieldValue }
-					placeholder={ layout.placeholder }
-					updateValue={ updateValue }
-					error={ fieldError }
-				/>
-			);
-
-		default:
-			return (
-				<TextField
-					id={ id }
-					title={ fieldSchema.title }
-					description={ fieldSchema.description }
-					value={ fieldValue }
-					placeholder={ layout.placeholder }
-					updateValue={ updateValue }
-					error={ fieldError }
-				/>
-			);
+		return formValueActions.updateField( id, value );
 	}
-};
 
-SettingsItem.propTypes = {
-	layout: PropTypes.oneOfType( [
-		PropTypes.string.isRequired,
-		PropTypes.object.isRequired,
-	] ).isRequired,
-	schema: PropTypes.object.isRequired,
-	storeOptions: PropTypes.object.isRequired,
-	form: PropTypes.object.isRequired,
-	formValueActions: PropTypes.object.isRequired,
-	errors: PropTypes.object,
-	saveForm: PropTypes.func,
-};
+	updateSubValue = ( key, val ) => {
+		const {
+			layout,
+			formValueActions,
+		} = this.props;
+		const id = layout.key ? layout.key : layout;
+
+		return formValueActions.updateField( [ id ].concat( key ), val );
+	}
+
+	onToggle = () => this.props.saveForm( this.props.schema );
+
+	render() {
+		const {
+			form,
+			layout,
+			schema,
+			storeOptions,
+			errors,
+		} = this.props;
+		const id = layout.key ? layout.key : layout;
+		const fieldValue = form.values[ id ];
+		const fieldSchema = schema.properties[ id ];
+		const fieldType = layout.type || fieldSchema.type || '';
+		const fieldError = errors[ '' ] ? ( errors[ '' ].value || layout.validation_hint || '' ) : false;
+
+		switch ( fieldType ) {
+			case 'radios':
+				return (
+					<RadioButtons
+						valuesMap={ layout.titleMap }
+						title={ fieldSchema.title }
+						description={ fieldSchema.description }
+						value={ fieldValue }
+						setValue={ this.updateValue }
+						error={ fieldError }
+					/>
+				);
+
+			case 'dropdown':
+				return (
+					<Dropdown
+						id={ id }
+						valuesMap={ layout.titleMap }
+						title={ fieldSchema.title }
+						description={ fieldSchema.description }
+						value={ fieldValue }
+						updateValue={ this.updateValue }
+						error={ fieldError }
+					/>
+				);
+
+			case 'shipping_services':
+				return (
+					<ShippingServiceGroups
+						services={ schema.definitions.services }
+						title={ fieldSchema.title }
+						description={ fieldSchema.description }
+						settings={ fieldValue }
+						currencySymbol={ storeOptions.currency_symbol }
+						updateValue={ this.updateSubValue }
+						settingsKey={ id }
+						errors={ errors }
+						generalError={ fieldError }
+					/>
+				);
+
+			case 'packages':
+				return (
+					<div>
+						<FormLegend>{ __( 'Saved Packages' ) }</FormLegend>
+						{ __(
+							'Add and edit saved packages using the {{a}}Packaging Manager{{/a}}.',
+							{
+								components: {
+									a: <a href="admin.php?page=wc-settings&tab=shipping&section=package-settings" />,
+								},
+							}
+						) }
+					</div>
+				);
+
+			case 'indicators':
+				return (
+					<Indicators
+						title={ fieldSchema.title }
+						subtitle={ fieldSchema.subtitle }
+						indicators={ [ fieldValue ] }
+					/>
+				);
+
+			case 'text':
+				return (
+					<Text
+						id={ id }
+						title={ layout.title }
+						className={ layout.class }
+						value={ fieldValue || layout.description }
+					/>
+				);
+
+			case 'textarea':
+				return (
+					<TextArea
+						error={ fieldError }
+						id={ id }
+						readonly={ Boolean( layout.readonly ) }
+						placeholder={ layout.placeholder }
+						title={ fieldSchema.title }
+						description={ fieldSchema.description }
+						updateValue={ this.updateValue }
+						value={ fieldValue }
+					/>
+				);
+
+			case 'boolean':
+				return (
+					<Toggle
+						checked={ Boolean( fieldValue ) }
+						id={ id }
+						title={ fieldSchema.title }
+						description={ fieldSchema.description }
+						trueText={ fieldSchema.trueText }
+						falseText={ fieldSchema.falseText }
+						saveOnToggle={ Boolean( fieldSchema.saveOnToggle ) }
+						saveForm={ this.onToggle }
+						updateValue={ this.updateValue }
+					/>
+				);
+
+			case 'number':
+				return (
+					<NumberField
+						id={ id }
+						title={ fieldSchema.title }
+						description={ fieldSchema.description }
+						value={ fieldValue }
+						placeholder={ layout.placeholder }
+						updateValue={ this.updateValue }
+						error={ fieldError }
+					/>
+				);
+
+			default:
+				return (
+					<TextField
+						id={ id }
+						title={ fieldSchema.title }
+						description={ fieldSchema.description }
+						value={ fieldValue }
+						placeholder={ layout.placeholder }
+						updateValue={ this.updateValue }
+						error={ fieldError }
+					/>
+				);
+		}
+	}
+}
 
 export default SettingsItem;
