5aebd6c6cecf13ffa8b8fb2ba2ec37a794454404
Jeff Stieler <jeff.m.stieler@gmail.com>
2018-01-29 16:13:17 -0700

    Fix react/jsx-no-bind lint error in PrintTestLabelView.

--- client/apps/print-test-label/view.js
+++ client/apps/print-test-label/view.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { translate as __ } from 'i18n-calypso';
 import { bindActionCreators } from 'redux';
@@ -19,49 +19,53 @@ import FormSelect from 'components/forms/form-select';
 import FormLegend from 'components/forms/form-legend';
 import SettingsGroupCard from 'components/settings-group-card';
 
-const PrintTestLabelView = ( { paperSize, country, printingInProgress, error, updatePaperSize, print } ) => {
-	const paperSizes = getPaperSizes( country );
-	const onPaperSizeChange = ( event ) => updatePaperSize( event.target.value );
+class PrintTestLabelView extends Component {
+	static propTypes = {
+		paperSize: PropTypes.string.isRequired,
+		country: PropTypes.string.isRequired,
+		printingInProgress: PropTypes.bool,
+		error: PropTypes.string,
+		updatePaperSize: PropTypes.func.isRequired,
+		print: PropTypes.func.isRequired,
+	};
 
-	return (
-		<SettingsGroupCard heading={ __( 'Print' ) } >
-			{ error && <ErrorNotice>{ error }</ErrorNotice> }
-			<FormLegend>{ __( 'Having trouble configuring your printer?' ) }</FormLegend>
-			<p>{ __( 'You can run a test print for shipping labels by selecting the paper size, then print.' ) }</p>
-			<FormFieldset>
-				<FormLegend>{ __( 'Paper size' ) }</FormLegend>
-				<div className="print-test-label__form-container">
-					<FormSelect
-						className="print-test-label__paper-size"
-						value={ paperSize }
-						onChange={ onPaperSizeChange } >
-						{ Object.keys( paperSizes ).map( key =>
-							<option key={ key } value={ key }>
-								{ paperSizes[ key ] }
-							</option>
-						) }
-					</FormSelect>
-					<FormButton
-						type="button"
-						disabled={ Boolean( printingInProgress ) }
-						onClick={ print }
-						isPrimary={ false }>
-						{ __( 'Print' ) }
-					</FormButton>
-				</div>
-			</FormFieldset>
-		</SettingsGroupCard>
-	);
-};
+	onPaperSizeChange = ( event ) => this.props.updatePaperSize( event.target.value );
 
-PrintTestLabelView.propTypes = {
-	paperSize: PropTypes.string.isRequired,
-	country: PropTypes.string.isRequired,
-	printingInProgress: PropTypes.bool,
-	error: PropTypes.string,
-	updatePaperSize: PropTypes.func.isRequired,
-	print: PropTypes.func.isRequired,
-};
+	render() {
+		const { paperSize, country, printingInProgress, error, print } = this.props;
+		const paperSizes = getPaperSizes( country );
+
+		return (
+			<SettingsGroupCard heading={ __( 'Print' ) } >
+				{ error && <ErrorNotice>{ error }</ErrorNotice> }
+				<FormLegend>{ __( 'Having trouble configuring your printer?' ) }</FormLegend>
+				<p>{ __( 'You can run a test print for shipping labels by selecting the paper size, then print.' ) }</p>
+				<FormFieldset>
+					<FormLegend>{ __( 'Paper size' ) }</FormLegend>
+					<div className="print-test-label__form-container">
+						<FormSelect
+							className="print-test-label__paper-size"
+							value={ paperSize }
+							onChange={ this.onPaperSizeChange } >
+							{ Object.keys( paperSizes ).map( key =>
+								<option key={ key } value={ key }>
+									{ paperSizes[ key ] }
+								</option>
+							) }
+						</FormSelect>
+						<FormButton
+							type="button"
+							disabled={ Boolean( printingInProgress ) }
+							onClick={ print }
+							isPrimary={ false }>
+							{ __( 'Print' ) }
+						</FormButton>
+					</div>
+				</FormFieldset>
+			</SettingsGroupCard>
+		);
+	}
+}
 
 export default connect(
 	( state ) => state,
