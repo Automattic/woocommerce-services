f1a544b85617477c86b7f4b695b0df12090b2f21
Daniel Rey López <daniel.rey.lopez@a8c.com>
2018-10-29 12:14:31 +0000

    Tweak the ESLint config so it resolves dependencies from wp-calypso's node_modules folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,11 @@
-module.exports = require( 'wp-calypso/.eslintrc' );
+const path = require( 'path' );
+const conf = require( 'wp-calypso/.eslintrc' );
+
+delete conf.rules[ 'import/no-extraneous-dependencies' ];
+// TODO: uncomment this when https://github.com/benmosher/eslint-plugin-import/pull/1176 is released
+//conf.rules[ 'import/no-extraneous-dependencies' ][ 1 ].packageDir = [
+//	path.resolve( __dirname ),
+//	path.resolve( __dirname, 'node_modules', 'wp-calypso' ),
+//];
+
+module.exports = conf;
