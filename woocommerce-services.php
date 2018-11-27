7e54fa0628bf1a4254730638b145d6aa518591e5
Brando Meniconi <meniconi.brando@gmail.com>
2018-11-27 11:12:33 +0100

    Add priority to 'shipping_phone' checkout field
    add the priority key to the 'shipping_phone' field in 'add_shipping_phone_to_checkout' function

--- woocommerce-services.php
+++ woocommerce-services.php
@@ -1229,6 +1229,7 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 				'clear'        => true,
 				'validate'     => array( 'phone' ),
 				'autocomplete' => 'tel',
+				'priority'     => 80,
 			);
 			return $fields;
 		}
