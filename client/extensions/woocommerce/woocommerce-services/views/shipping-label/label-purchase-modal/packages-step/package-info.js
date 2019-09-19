b2ea4640ddc7671c010d7311a8c77c815117f5ef
Boro Sitnikovski <buritomath@gmail.com>
2019-09-19 10:57:44 +0200

    Make sure we use our own class instead of calypso's

--- client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/packages-step/package-info.js
+++ client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/packages-step/package-info.js
@@ -34,7 +34,7 @@ import {
 	getFormErrors,
 } from 'woocommerce/woocommerce-services/state/shipping-label/selectors';
 import { getPackageGroupsForLabelPurchase } from 'woocommerce/woocommerce-services/state/packages/selectors';
-import PackageDialog from 'woocommerce/woocommerce-services/views/packages/package-dialog';
+import PackageDialog from '../../../packages/package-dialog.js';
 import { getPackagesForm } from "../../../../state/packages/selectors";
 import * as PackagesActions from "../../../../state/packages/actions";
 const renderPackageDimensions = ( dimensions, dimensionUnit ) => {
