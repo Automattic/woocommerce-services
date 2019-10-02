9222d39478e53acb35449e476ec17470be92e5f6
Harris Wong <harris.wong@automattic.com>
2019-10-02 16:11:39 +0300

    Add e2e testing packages and files
    This includes puppeteer, jest, etc

---
+++ tests/e2e-tests/specs/admin/activate-extension.test.js
@@ -0,0 +1,36 @@
+/**
+ * WordPress dependencies
+ */
+import { activatePlugin } from "@wordpress/e2e-test-utils";
+
+/**
+ * activatePlugin( slug ) - Activates an installed plugin.
+ *
+ * @param {string} slug Plugin slug.
+ *
+ * Within the activatePlugin() function:
+ *
+ * 1) Switches the current user to the admin user (if the user
+ * running the test is not already the admin user).
+ *
+ * 2) Visits admin page; if user is not logged in then it logging in it first, then visits admin page.
+ *
+ * 3) Checks if plugin is activated. If not, activates the plugin.
+ *
+ * 4) Switches the current user to whichever user we should be
+ * running the tests as (if we're not already that user).
+ */
+
+describe( 'Store admin can login and make sure WooCommerce Service extension is activated', () => {
+
+	it( 'Can activate WooCommerce Service extension if it is deactivated' , async () => {
+        // await activatePlugin( 'woocommerce-services' );
+        
+        try {
+			await activatePlugin( 'woocommerce-services' );
+		} catch ( error ) {
+			await activatePlugin( 'woocommerce-com-woocommerce-services' );
+		}
+	});
+
+} );
