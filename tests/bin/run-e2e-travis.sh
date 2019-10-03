394f9410efc345e409884bfc147a7eea85d905cb
Harris Wong <harris.wong@automattic.com>
2019-10-03 17:53:37 +0300

    Clean up

--- tests/bin/run-e2e-travis.sh
+++ tests/bin/run-e2e-travis.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
 if [[ ${RUN_E2E} == 1 ]]; then
-
+	# Set env variables for jest
 	WP_SITE_URL="http://localhost:8080"
 	WP_CUSTOMER_USER_NAME=customer
 	WP_CUSTOMER_USER_PW=password
 	WP_ADMIN_USER_NAME=admin
 	WP_ADMIN_USER_PW=password
-
-	# Start xvfb to run the tests
-	export WP_BASE_URL="$WP_SITE_URL"
-	export WP_CUSTOMER_USER_NAME
-	export WP_CUSTOMER_USER_PW
-	export WP_ADMIN_USER_NAME
-	export WP_ADMIN_USER_PW
-	export DISPLAY=:99.0
-	# sh -e /etc/init.d/xvfb start
-	# sleep 3
-
+	
 	# Run the tests
-	# npm run test:e2e-travis
 	./node_modules/jest/bin/jest.js --config=./tests/e2e-tests/config/jest.config.js --rootDir=./ --verbose --runInBand;
 fi
