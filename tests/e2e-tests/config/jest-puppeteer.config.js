0c98b48e9d1fc5d410c30d84105fd7920c8b5bfd
Harris Wong <harris.wong@automattic.com>
2019-10-03 06:54:46 +0300

    Add travis.yml and configurations

--- tests/e2e-tests/config/jest-puppeteer.config.js
+++ tests/e2e-tests/config/jest-puppeteer.config.js
@@ -5,5 +5,11 @@ module.exports = {
         // Required for the logged out and logged in tests so they don't share app state/token.
         browserContext: 'incognito',
         ignoreHTTPSErrors: true,
+        // dumpio: true,
+        args: [
+            '--remote-debugging-port=9222',
+            // '--ignore-certificate-errors',
+            // '--ignore-certificate-errors-spki-list',
+        ]
     }
 };
