d716878c3089497b52a10ba19b0e6b0dd5001ad9
Harris Wong <harris.wong@automattic.com>
2019-10-03 16:26:12 +0300

    Wrap e2e test in a bash script to capture exit code

--- tests/bin/run-e2e-travis.sh
+++ tests/bin/run-e2e-travis.sh
@@ -2,13 +2,17 @@
 if [[ ${RUN_E2E} == 1 ]]; then
 
 	WP_SITE_URL="http://localhost:8080"
-	WP_USERNAME="customer"
-	WP_PASSWORD="customer_password"
+	WP_CUSTOMER_USER_NAME=customer
+	WP_CUSTOMER_USER_PW=password
+	WP_ADMIN_USER_NAME=admin
+	WP_ADMIN_USER_PW=password
 
 	# Start xvfb to run the tests
 	export WP_BASE_URL="$WP_SITE_URL"
-	export WP_USERNAME
-	export WP_PASSWORD
+	export WP_CUSTOMER_USER_NAME
+	export WP_CUSTOMER_USER_PW
+	export WP_ADMIN_USER_NAME
+	export WP_ADMIN_USER_PW
 	export DISPLAY=:99.0
 	sh -e /etc/init.d/xvfb start
 	sleep 3
