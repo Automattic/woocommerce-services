7aa901fffef00fc041909538558be3b4505f9ac1
Harris Wong <harris.wong@automattic.com>
2019-10-03 18:17:36 +0300

    Export env variables

--- tests/bin/run-e2e-travis.sh
+++ tests/bin/run-e2e-travis.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 if [[ ${RUN_E2E} == 1 ]]; then
 	# Set env variables for jest
-	WP_BASE_URL="http://localhost:8080"
+	WP_SITE_URL="http://localhost:8080"
 	WP_CUSTOMER_USER_NAME=customer
 	WP_CUSTOMER_USER_PW=password
 	WP_ADMIN_USER_NAME=admin
 	WP_ADMIN_USER_PW=password
-	
+
+	export WP_BASE_URL="$WP_SITE_URL"
+	export WP_CUSTOMER_USER_NAME
+	export WP_CUSTOMER_USER_PW
+	export WP_ADMIN_USER_NAME
+	export WP_ADMIN_USER_PW
+
 	# Run the tests
 	./node_modules/jest/bin/jest.js --config=./tests/e2e-tests/config/jest.config.js --rootDir=./ --verbose --runInBand;
 fi
