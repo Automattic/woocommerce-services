ac73c68b79c946c93f0a06f3c85b845364cb0559
budzanowski <bartosz.budzanowski@gmail.com>
2019-10-14 12:16:43 +0200

    Build in label refresh guard into label refresh action.

--- client/extensions/woocommerce/woocommerce-services/state/shipping-label/actions.js
+++ client/extensions/woocommerce/woocommerce-services/state/shipping-label/actions.js
@@ -13,6 +13,7 @@ import {
 	find,
 	first,
 	flatten,
+	get,
 	includes,
 	isBoolean,
 	isEqual,
@@ -72,6 +73,7 @@ import {
 	WOOCOMMERCE_SERVICES_SHIPPING_LABEL_OPEN_REFUND_DIALOG,
 	WOOCOMMERCE_SERVICES_SHIPPING_LABEL_CLOSE_REFUND_DIALOG,
 	WOOCOMMERCE_SERVICES_SHIPPING_LABEL_STATUS_RESPONSE,
+	WOOCOMMERCE_SERVICES_SHIPPING_LABEL_STATUS_RETRIEVAL_IN_PROGRESS,
 	WOOCOMMERCE_SERVICES_SHIPPING_LABEL_REFUND_REQUEST,
 	WOOCOMMERCE_SERVICES_SHIPPING_LABEL_REFUND_RESPONSE,
 	WOOCOMMERCE_SERVICES_SHIPPING_LABEL_OPEN_REPRINT_DIALOG,
@@ -1079,11 +1081,14 @@ export const openRefundDialog = ( orderId, siteId, labelId ) => {
 	};
 };
 
+const isRefreshingLabelStatus = ( label ) => {
+	return get( label, "statusRetrivalInProgress", false ) ;
+};
+
 export const fetchLabelsStatus = ( orderId, siteId ) => ( dispatch, getState ) => {
 	const shippingLabel = getShippingLabel( getState(), orderId, siteId );
-
 	const labelRequests = shippingLabel.labels.map( label => {
-		if ( label.statusUpdated ) {
+		if ( label.statusUpdated || isRefreshingLabelStatus( label ) ) {
 			return;
 		}
 		const labelId = label.label_id;
@@ -1093,7 +1098,12 @@ export const fetchLabelsStatus = ( orderId, siteId ) => ( dispatch, getState ) =
 		const setSuccess = json => {
 			response = json.label;
 		};
-
+		console.log( WOOCOMMERCE_SERVICES_SHIPPING_LABEL_STATUS_RETRIEVAL_IN_PROGRESS );
+		dispatch( {
+			type: WOOCOMMERCE_SERVICES_SHIPPING_LABEL_STATUS_RETRIEVAL_IN_PROGRESS,
+			orderId,
+			labelId,
+		} );
 		return api
 			.get( siteId, api.url.labelStatus( orderId, labelId ) )
 			.then( setSuccess )
