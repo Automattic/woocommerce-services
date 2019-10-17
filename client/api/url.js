ec265ffb89db30e4b8879f6d43b0936781698f9b
Fernando Espinosa <fer@ferdev.com>
2019-10-17 11:54:49 +0200

    Removes duplicated url constants
    Also fixes the `api.url.labelTestPrint` const name for the
    woocommerce-services api object.

--- client/api/url.js
+++ client/api/url.js
@@ -1,34 +1,10 @@
 /**
  * External dependencies
  */
-import _ from 'lodash';
-
 const namespace = 'wc/v1/connect/';
-
-export const accountSettings = () => namespace + 'account/settings';
-
-export const packages = () => namespace + 'packages';
-
-export const orderLabels = ( orderId ) => `${ namespace }label/${ orderId }`;
-
-export const getLabelRates = ( orderId ) => `${ namespace }label/${ orderId }/rates`;
-
-export const labelStatus = ( orderId, labelId ) => `${ namespace }label/${ orderId }/${ labelId }`;
-
-export const labelRefund = ( orderId, labelId ) => `${ namespace }label/${ orderId }/${ labelId }/refund`;
 
 export const labelsPrint = () => `${ namespace }label/print`;
 
 export const labelTestPrint = () => `${ namespace }label/preview`;
-
-export const addressNormalization = () => `${ namespace }normalize-address`;
-
-export const settingsForm = ( formType, methodId, instanceId ) => {
-	const path = _.filter( [ formType, methodId, instanceId ] ).join( '/' );
-
-	return namespace + path;
-};
-
-export const dismissShippingSettingsNuxNotice = () => `${ namespace }services/dismiss_notice`;
 
 export const selfHelp = () => `${ namespace }self-help`;
