6a9f20f84e2a5cb2daf13dda12c8be5514bb7d62
Boro Sitnikovski <buritomath@gmail.com>
2019-10-29 18:28:13 +0100

    Add initial implementation of ShippingSummary

---
+++ client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/shipping-summary.js
@@ -0,0 +1,108 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { localize } from 'i18n-calypso';
+import { isEqual } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import {
+	getShippingLabel,
+	isLoaded,
+	getFormErrors,
+} from 'woocommerce/woocommerce-services/state/shipping-label/selectors';
+import {
+	toggleStep,
+} from 'woocommerce/woocommerce-services/state/shipping-label/actions';
+import { hasNonEmptyLeaves } from 'woocommerce/woocommerce-services/lib/utils/tree';
+
+const ShippingSummary = props => {
+	const { translate, normalizationStatus, form, orderId, siteId } = props;
+	const { origin, rates } = form;
+	const labelCount = Object.keys( rates.values ).length;
+
+	return (
+		<div>
+			<div className="label-purchase-modal__shipping-summary-header">
+				{ translate( 'Shipping summary' ) }
+			</div>
+			<div className="label-purchase-modal__shipping-summary-info">
+				{ normalizationStatus.isSuccess ? (
+					<div className="label-purchase-modal__shipping-summary-street">
+						{ translate( 'Shipping from %(address)s', {
+							args: { address: origin.values.address },
+						} ) }
+						<a href="#" onClick={ () => props.toggleStep( orderId, siteId, 'origin', true ) }>Edit</a>
+					</div>
+				) : null }
+			</div>
+			<div className="label-purchase-modal__shipping-summarry-labels">
+				{ labelCount + ' ' + translate( 'shipping label ready', 'shipping labels ready', { count: labelCount } ) }
+			</div>
+		</div>
+	);
+}
+
+ShippingSummary.propTypes = {
+	siteId: PropTypes.number.isRequired,
+	orderId: PropTypes.number.isRequired,
+};
+
+const getNormalizationStatus = ( {
+	normalizationInProgress,
+	errors,
+	isNormalized,
+	values,
+	normalized,
+} ) => {
+	if ( normalizationInProgress ) {
+		return { isProgress: true };
+	}
+	if ( hasNonEmptyLeaves( errors ) || ( isNormalized && ! normalized ) || ! isNormalized ) {
+		return { isError: true };
+	}
+	if ( isNormalized ) {
+		return isEqual( values, normalized ) ? { isSuccess: true } : { isWarning: true };
+	}
+	return {};
+};
+
+const mapStateToProps = ( state, { orderId, siteId } ) => {
+	const loaded = isLoaded( state, orderId, siteId );
+	const shippingLabel = getShippingLabel( state, orderId, siteId );
+
+	const form = shippingLabel.form.origin;
+	const errors = loaded && getFormErrors( state, orderId, siteId ).origin;
+
+	return {
+		orderId,
+		siteId,
+		errors,
+		form: shippingLabel.form,
+		expanded: form.expanded,
+		normalizationStatus: getNormalizationStatus( { ...form, errors } ),
+	};
+};
+
+const mapDispatchToProps = dispatch => {
+	return bindActionCreators(
+		{
+			toggleStep,
+		},
+		dispatch
+	);
+};
+
+export default localize(
+	connect(
+		mapStateToProps,
+		mapDispatchToProps
+	)( ShippingSummary )
+);
