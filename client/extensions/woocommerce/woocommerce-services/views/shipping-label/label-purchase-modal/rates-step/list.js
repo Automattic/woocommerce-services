5750282c4dc5ad254afcc0b4a9dbc3508d4f1fa5
Harris Wong <harris.wong@automattic.com>
2019-11-01 17:09:19 -0600

    Separate multiple rendering to separate components

--- client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/rates-step/list.js
+++ client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/rates-step/list.js
@@ -30,14 +30,63 @@ const renderRateNotice = translate => {
 };
 
 class ShippingRates extends Component {
+	render() {
+		console.log(this.props.selectedPackages, this.props.availableRates);
+
+		const packageNames = getPackageDescriptions( this.props.selectedPackages, this.props.allPackages, true );
+
+		const shippingRates = Object.keys(this.props.selectedPackages).map((pckgId) => {
+			console.log("inside shipping rates loop", this.props.selectedPackages);
+			const hasSinglePackage = 1 === Object.keys( selectedPackages ).length;
+			const pckg = this.props.selectedPackages.pckgId
+
+
+			return <PackageShippingRates
+				{...this.props}
+				pckg={pckg}
+				pckgId={pckgId}
+				selectedRate={this.props.selectedRates[ pckgId ] || ''}  // Store owner selected rates, not customer
+				availableRates={this.props.availableRates[ pckgId ]}
+				packageErrors = {this.props.errors[ pckgId ] || []}
+				packageNames = {packageNames[ pckgId ]}
+			/>
+		});
+		return <div>
+			{ this.props.shouldShowRateNotice && renderRateNotice( this.props.translate ) }
+			{ shippingRates }
+		</div>
+	}
+}
+
+class PackageShippingRates extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			packageRates: [],
+			visiblePackageRates: []
+		}
+
+		this.handleShowMore = this.handleShowMore.bind(this);
+		this.renderSinglePackage = this.renderSinglePackage.bind(this);
+	}
+
+	handleShowMore() {
+		this.setState((state) => {
+			return {
+				visiblePackageRates: state.visiblePackageRates.concat(this.state.packageRates.splice(0, 1))
+			};
+		});
 	}
 
 	render() {
+		return this.renderSinglePackage(this.props.pckg, this.props.pckgId);
+	}
+
+	renderSinglePackage ( pckg, pckgId ) {
 		const {
 			id,
-			selectedRates, // Store owner selected rates, not customer
+			selectedRate,
 			availableRates,
 			selectedPackages,
 			allPackages,
@@ -46,57 +95,57 @@ class ShippingRates extends Component {
 			shouldShowRateNotice,
 			translate
 		} = this.props;
-		const packageNames = getPackageDescriptions( selectedPackages, allPackages, true );
+
 		const hasSinglePackage = 1 === Object.keys( selectedPackages ).length;
 
-		const renderSinglePackage = ( pckg, pckgId ) => {
-			if ( ! ( pckgId in availableRates ) ) {
-				return null;
-			}
-			const selectedRate = selectedRates[ pckgId ] || '';
-			const packageRates = availableRates[ pckgId ].default.rates;
-			let signatureRates = null;
-			if ( 'signature_required' in availableRates[ pckgId ] ) {
-				signatureRates = availableRates[ pckgId ].signature_required.rates || null;
-			}
-			const packageErrors = errors[ pckgId ] || [];
+		if ( ! availableRates ) {
+			return null;
+		}
 
-			const onRateUpdate = ( serviceId, signatureRequired ) => updateRate( pckgId, serviceId, signatureRequired );
+		// this.state.packageRates = availableRates[ pckgId ].default.rates;
+		const packageRates = availableRates.default.rates;
+		const packageNames = this.props.packageNames;
 
-			return (
-				<div key={ pckgId } className="rates-step__package-container">
-					{ ! hasSinglePackage ? (
-						<div className="rates-step__package-container-rates-header">
-							{ translate( 'Choose rate: %(pckg)s', { args: { pckg: packageNames[ pckgId ] } } ) }
-						</div>
-					) : null }
-					{ Object.values(
-						mapValues( packageRates, ( ( serviceRateObject ) => {
-							const { service_id } = serviceRateObject;
-							const rateObjectSignatureRequired = find( signatureRates, r => service_id === r.service_id );
-							return <ShippingRate
-								id={ id + '_' + pckgId }
-								key={ id + '_' + pckgId + '_' + service_id }
-								rateObject={ serviceRateObject }
-								rateObjectSignatureRequired={ rateObjectSignatureRequired }
-								updateValue={ onRateUpdate }
-								isSelected={ service_id === selectedRate.serviceId }
-							/>
-						} ) )
-					) }
-					{ packageErrors.slice( 1 ).map( ( error, index ) => {
-						// Print the rest of the errors (if any) below the dropdown
-						return <FieldError type="server-error" key={ index } text={ error } />;
-					} ) }
-				</div>
-			);
-		};
+		console.log(this.state.packageRates);
+		// this.state.visiblePackageRates = this.state.visiblePackageRates.concat(this.state.packageRates.splice(0, 1))
 
-		return <div>
-			{ shouldShowRateNotice && renderRateNotice( translate ) }
-			{ Object.values( mapValues( selectedPackages, renderSinglePackage ) ) }
-		</div>
-	}
+		let signatureRates = null;
+		if ( 'signature_required' in availableRates ) {
+			signatureRates = availableRates.signature_required.rates || null;
+		}
+		const packageErrors = this.props.packageErrors;
+
+		const onRateUpdate = ( serviceId, signatureRequired ) => updateRate( pckgId, serviceId, signatureRequired );
+
+		return (
+			<div key={ pckgId } className="rates-step__package-container">
+				{ ! hasSinglePackage ? (
+					<div className="rates-step__package-container-rates-header">
+						{ translate( 'Choose rate: %(pckg)s', { args: { pckg: packageNames } } ) }
+					</div>
+				) : null }
+				{ Object.values(
+					mapValues( packageRates, ( ( serviceRateObject ) => {
+						const { service_id } = serviceRateObject;
+						const rateObjectSignatureRequired = find( signatureRates, r => service_id === r.service_id );
+						return <ShippingRate
+							id={ id + '_' + pckgId }
+							key={ id + '_' + pckgId + '_' + service_id }
+							rateObject={ serviceRateObject }
+							rateObjectSignatureRequired={ rateObjectSignatureRequired }
+							updateValue={ onRateUpdate }
+							isSelected={ service_id === selectedRate.serviceId }
+						/>
+					} ) )
+				) }
+				<button onClick={ this.handleShowMore } >Show more</button>
+				{ packageErrors.slice( 1 ).map( ( error, index ) => {
+					// Print the rest of the errors (if any) below the dropdown
+					return <FieldError type="server-error" key={ index } text={ error } />;
+				} ) }
+			</div>
+		);
+	};
 }
 
 ShippingRates.propTypes = {
