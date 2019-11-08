967f61bf3165e581f25ddf4dfa92a8b137c66809
budzanowski <bartosz.budzanowski@gmail.com>
2019-11-08 16:59:12 +0100

    Display full info on price breakdown.

--- client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/price-summary.js
+++ client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/price-summary.js
@@ -86,14 +86,15 @@ class PriceSummary extends Component {
 		}
 
 		const { prices, discount, total } = priceBreakdown;
-
+		
 		return (
 			<div className="label-purchase-modal__shipping-summary-section">
 				<hr />
 				{ prices.map( ( service, index ) => {
 					return (
 						<Fragment key={ index }>
-							{ this.renderRow( service.title, service.retailRate, index ) }
+							{ ( service.retailRate !== service.rate ) ? this.renderRow( service.title, service.retailRate, index ) : null }
+							{ this.renderRow( translate( "Your price"), service.rate, 'rate-' + index ) }
 							{ service.addons.map( ( addon, addonIndex ) =>
 								<div key={ 'addons-' + index } className="label-purchase-modal__price-item-addons">
 									{ this.renderRow( addon.title, addon.rate, 'addon-' + addonIndex ) }
