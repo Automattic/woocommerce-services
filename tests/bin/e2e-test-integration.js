285099f6852ffe7059855ebfe9ce60517c4c9bd3
budzanowski <bartosz.budzanowski@gmail.com>
2019-11-20 10:40:32 +0100

    Add env variables check.

--- tests/bin/e2e-test-integration.js
+++ tests/bin/e2e-test-integration.js
@@ -3,6 +3,19 @@
 const { spawn } = require( 'child_process' );
 const program = require( 'commander' );
 
+const baseUrl = process.env.WP_BASE_URL;
+const adminUserName = process.env.WP_ADMIN_USER_NAME;
+const adminUserPassword = process.env.WP_ADMIN_USER_PW;
+
+const envVariablesSet = baseUrl && adminUserName && adminUserPassword;
+
+if( ! envVariablesSet ) {
+	! baseUrl && console.error( "\033[31m WP_BASE_URL \x1b[0m environment variable is missing!");
+	! adminUserName && console.error( "\033[31m WP_ADMIN_USER_NAME \x1b[0m environment variable is missing!");
+	! adminUserPassword && console.error( "\033[31m WP_ADMIN_USER_PW \x1b[0m environment variable is missing!");
+	throw new Error( "Test configutartion variables missing!" );
+}
+
 program
 	.usage( '<file ...> [options]' )
 	.option( '--dev', 'Development mode' )
