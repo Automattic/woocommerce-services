ed4b255c22658b48eb642189020d84484a0b5ef6
budzanowski <bartosz.budzanowski@gmail.com>
2019-11-22 14:25:06 +0100

    Run travis as every other e2e tests.

--- tests/bin/run-e2e-travis.sh
+++ tests/bin/run-e2e-travis.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-if [[ ${RUN_E2E} == 1 ]]; then
-	export WP_BASE_URL="http://localhost:8080"
-	export WP_CUSTOMER_USER_NAME=customer
-	export WP_CUSTOMER_USER_PW=password
-	export WP_ADMIN_USER_NAME=admin
-	export WP_ADMIN_USER_PW=password
-	export WOOCOMMERCE_SERVICES_CI_TEST_MODE=true
-
-	# Run the tests
-	./node_modules/jest/bin/jest.js --config=./tests/e2e-tests/config/jest.config.js --rootDir=./ --verbose --runInBand;
-fi
+export WP_BASE_URL="http://localhost:8080"
+export WP_CUSTOMER_USER_NAME=customer
+export WP_CUSTOMER_USER_PW=password
+export WP_ADMIN_USER_NAME=admin
+export WP_ADMIN_USER_PW=password
+export WOOCOMMERCE_SERVICES_CI_TEST_MODE=true
