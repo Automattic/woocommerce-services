89baeee824e3b5f362fe72200dc15cf03f66e33d
budzanowski <bartosz.budzanowski@gmail.com>
2019-11-25 18:36:21 +0100

    Check for e2e env.

--- tests/bin/run-e2e-travis.sh
+++ tests/bin/run-e2e-travis.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-export WP_BASE_URL="http://localhost:8080"
-export WP_CUSTOMER_USER_NAME=customer
-export WP_CUSTOMER_USER_PW=password
-export WP_ADMIN_USER_NAME=admin
-export WP_ADMIN_USER_PW=password
-export WOOCOMMERCE_SERVICES_CI_TEST_MODE=true
+if [[ ${RUN_E2E} == 1 ]]; then
+    export WP_BASE_URL="http://localhost:8080"
+    export WP_CUSTOMER_USER_NAME=customer
+    export WP_CUSTOMER_USER_PW=password
+    export WP_ADMIN_USER_NAME=admin
+    export WP_ADMIN_USER_PW=password
+    export WOOCOMMERCE_SERVICES_CI_TEST_MODE=true
+fi
