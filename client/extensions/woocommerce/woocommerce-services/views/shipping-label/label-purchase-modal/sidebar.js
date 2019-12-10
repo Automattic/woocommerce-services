873ecab8da42e539742183c0b6d3b21174f1bad6
roykho <roykho77@gmail.com>
2019-12-10 13:24:44 -0800

    Revert "Remove redundant Paper size dropdown"
    This reverts commit f0232dda9657429ffe39da9bdd8fd7299190cc12.

--- client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/sidebar.js
+++ client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/sidebar.js
@@ -12,6 +12,8 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import { getPaperSizes } from 'woocommerce/woocommerce-services/lib/pdf-label-utils';
+import Dropdown from 'woocommerce/woocommerce-services/components/dropdown';
 import FormCheckbox from 'components/forms/form-checkbox';
 import FormLabel from 'components/forms/form-label';
 import PriceSummary from './price-summary';
@@ -20,6 +22,7 @@ import PurchaseSection from './purchase-section';
 import {
 	setEmailDetailsOption,
 	setFulfillOrderOption,
+	updatePaperSize,
 } from 'woocommerce/woocommerce-services/state/shipping-label/actions';
 import {
 	getShippingLabel,
@@ -33,6 +36,9 @@ const Sidebar = props => {
 	const {
 		orderId,
 		siteId,
+		form,
+		errors,
+		paperSize,
 		translate,
 		fulfillOrder,
 		emailDetails,
@@ -40,11 +46,20 @@ const Sidebar = props => {
 
 	const onEmailDetailsChange = () => props.setEmailDetailsOption( orderId, siteId, ! emailDetails );
 	const onFulfillOrderChange = () => props.setFulfillOrderOption( orderId, siteId, ! fulfillOrder );
+	const onPaperSizeChange = value => props.updatePaperSize( orderId, siteId, value );
 
 	return (
 		<div className="label-purchase-modal__sidebar">
 			<ShippingSummary siteId={ siteId } orderId={ orderId } />
 			<PriceSummary siteId={ siteId } orderId={ orderId } />
+			<Dropdown
+				id={ 'paper_size' }
+				valuesMap={ getPaperSizes( form.origin.values.country ) }
+				title={ translate( 'Paper size' ) }
+				value={ paperSize }
+				updateValue={ onPaperSizeChange }
+				error={ errors.paperSize }
+			/>
 			<FormLabel className="label-purchase-modal__option-email-customer">
 				<FormCheckbox checked={ emailDetails } onChange={ onEmailDetailsChange } />
 				<span>{ translate( 'Email shipment details to the customer' ) }</span>
@@ -62,14 +77,17 @@ const Sidebar = props => {
 Sidebar.propTypes = {
 	siteId: PropTypes.number.isRequired,
 	orderId: PropTypes.number.isRequired,
+	paperSize: PropTypes.string.isRequired,
 	errors: PropTypes.object.isRequired,
 	form: PropTypes.object.isRequired,
+	updatePaperSize: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = ( state, { orderId, siteId } ) => {
 	const loaded = isLoaded( state, orderId, siteId );
 	const shippingLabel = getShippingLabel( state, orderId, siteId );
 	return {
+		paperSize: shippingLabel.paperSize,
 		form: shippingLabel.form,
 		errors: loaded && getFormErrors( state, orderId, siteId ).sidebar,
 		fulfillOrder: loaded && shouldFulfillOrder( state, orderId, siteId ),
@@ -82,6 +100,7 @@ const mapDispatchToProps = dispatch => {
 		{
 			setEmailDetailsOption,
 			setFulfillOrderOption,
+			updatePaperSize,
 		},
 		dispatch
 	);
