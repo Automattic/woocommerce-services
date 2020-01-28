8cd39834e71a2ffd9cee6216c0ff27653baa8fc2
David Stone <david@nnucomputerwhiz.com>
2020-01-28 14:14:23 -0700

    decode html entities to avoid &#039; from appearing on the label closes #1862

--- classes/class-wc-connect-service-settings-store.php
+++ classes/class-wc-connect-service-settings-store.php
@@ -112,7 +112,7 @@ if ( ! class_exists( 'WC_Connect_Service_Settings_Store' ) ) {
 
 		public function get_origin_address() {
 			$wc_address_fields = array();
-			$wc_address_fields['company'] = get_bloginfo( 'name' );
+			$wc_address_fields['company'] = html_entity_decode( get_bloginfo( 'name' ), ENT_QUOTES ); // HTML entities may be saved in the option.
 			$wc_address_fields['name'] = wp_get_current_user()->display_name;
 			$wc_address_fields['phone'] = '';
 
