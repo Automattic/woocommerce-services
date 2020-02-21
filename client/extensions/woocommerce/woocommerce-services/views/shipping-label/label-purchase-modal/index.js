5db2067a00b8ec09c50ea1c49e7cf35b1c1ce5ba
Harris Wong <harris.wong@automattic.com>
2020-02-21 16:22:47 -0700

    Simple layout for creating Jetpack account

--- client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/index.js
+++ client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/index.js
@@ -27,6 +27,7 @@ import {
 	isLoaded,
 	isCustomsFormRequired,
 } from 'woocommerce/woocommerce-services/state/shipping-label/selectors';
+import TextField from 'woocommerce/woocommerce-services/components/text-field';
 
 const LabelPurchaseModal = props => {
 	const { loaded, translate } = props;
@@ -37,6 +38,77 @@ const LabelPurchaseModal = props => {
 
 
 	const onClose = () => props.exitPrintingFlow( props.orderId, props.siteId, false );
+	const hasJetpackAccount = false;
+
+	const labelPurchaseBody = () => {
+		return (
+		<div className="label-purchase-modal__body">
+			<div className="label-purchase-modal__main-section">
+				<AddressStep
+					type="origin"
+					title={ translate( 'Origin address' ) }
+					siteId={ props.siteId }
+					orderId={ props.orderId }
+				/>
+				<AddressStep
+					type="destination"
+					title={ translate( 'Destination address' ) }
+					siteId={ props.siteId }
+					orderId={ props.orderId }
+				/>
+				<PackagesStep siteId={ props.siteId } orderId={ props.orderId } />
+				{ props.isCustomsFormRequired && (
+					<CustomsStep siteId={ props.siteId } orderId={ props.orderId } />
+				) }
+				<RatesStep siteId={ props.siteId } orderId={ props.orderId } />
+			</div>
+			<Sidebar siteId={ props.siteId } orderId={ props.orderId } />
+		</div>
+		);
+	}
+
+	const createJetpackAccountBody = () => {
+		return (
+			<>
+				<h3 class="form-section-heading">Create a Jetpack account</h3>
+				<p>Your Jetpack account will enable you to start using the benefits offered by Jetpack & WooCommerce Services.</p>
+				<TextField
+					id="email"
+					title="Email address"
+					value="Email address"
+				/>
+
+				<TextField
+					id="username"
+					title="Choose a username"
+					value="username"
+				/>
+
+				<TextField
+					id="password"
+					title="Choose a password"
+					value="password"
+				/>
+
+				<p>by creating an account via any of the options below, you agree to our Terms of Service</p>
+				<Button
+					className="asd"
+					primary
+				>
+					{ translate( 'Create your account and connect' ) }
+				</Button>
+
+				<p>Or connect your existing profiler to get started faster</p>
+				<Button
+					className="asd"
+				>
+					{ translate( 'Connect with Google' ) }
+				</Button>
+
+				<p>Log in with an existing Jetpack or Wordpress.com account</p>
+			</>
+		);
+	}
 
 	return (
 		<Dialog
@@ -53,28 +125,8 @@ const LabelPurchaseModal = props => {
 						<Gridicon icon="cross" />
 					</Button>
 				</div>
-				<div className="label-purchase-modal__body">
-					<div className="label-purchase-modal__main-section">
-						<AddressStep
-							type="origin"
-							title={ translate( 'Origin address' ) }
-							siteId={ props.siteId }
-							orderId={ props.orderId }
-						/>
-						<AddressStep
-							type="destination"
-							title={ translate( 'Destination address' ) }
-							siteId={ props.siteId }
-							orderId={ props.orderId }
-						/>
-						<PackagesStep siteId={ props.siteId } orderId={ props.orderId } />
-						{ props.isCustomsFormRequired && (
-							<CustomsStep siteId={ props.siteId } orderId={ props.orderId } />
-						) }
-						<RatesStep siteId={ props.siteId } orderId={ props.orderId } />
-					</div>
-					<Sidebar siteId={ props.siteId } orderId={ props.orderId } />
-				</div>
+
+				{ (hasJetpackAccount) ? labelPurchaseBody() : createJetpackAccountBody() }
 			</div>
 		</Dialog>
 	);
