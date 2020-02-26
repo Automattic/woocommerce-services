eeab8219726b0f889f60ea51dbe00be1dabb0079
c-shultz <chris.shultz@automattic.com>
2020-02-26 11:48:23 -0800

    Add general tests for shipping banner 'create' and 'tracking' buttons.

--- client/apps/shipping-label/test/view-wrapper-label.js
+++ client/apps/shipping-label/test/view-wrapper-label.js
@@ -13,6 +13,7 @@ import { moment, translate } from 'i18n-calypso';
  * Internal dependencies
  */
 import { ShippingLabelViewWrapper } from '../view-wrapper-label';
+import Button from 'components/button';
 
 configure( { adapter: new Adapter() } );
 
@@ -34,7 +35,7 @@ const labelEventsTwoPurchased = [
 		labelIndex: 2,
 		productNames: [ '' ],
 		createdDate: 1551113177000,
-	}
+	},
 ];
 
 function createViewWrapperLabelWrapper( {
@@ -48,7 +49,7 @@ function createViewWrapperLabelWrapper( {
 	const props = {
 		orderId: 1000,
 		siteId: 10,
-		loaded: loaded || true,
+		loaded: ( undefined !== loaded ) ? loaded : true,
 		labelsEnabled: labelsEnabled || true,
 		items: items || 1,
 		events: events || {},
@@ -61,6 +62,19 @@ function createViewWrapperLabelWrapper( {
 }
 
 describe( 'ShippingLabelViewWrapper', () => {
+	describe( 'for not yet loaded', () => {
+		const viewWrapperLabel = createViewWrapperLabelWrapper( { loaded: false } );
+		it( 'renders loading button', function() {
+			expect( viewWrapperLabel.find( '.shipping-label__button-loading' ) ).to.have.length( 1 );
+		} );
+	} );
+
+	describe( 'for loaded', () => {
+		const viewWrapperLabel = createViewWrapperLabelWrapper( {} );
+		it( 'renders create label button', function() {
+			expect( viewWrapperLabel.find( '.shipping-label__new-label-button' ) ).to.have.length( 1 );
+		} );
+	} );
 
 	describe( 'for 1 item unfulfilled', () => {
 		const viewWrapperLabel = createViewWrapperLabelWrapper( {} );
@@ -84,13 +98,45 @@ describe( 'ShippingLabelViewWrapper', () => {
 		it( 'renders singular fulfilled message', function() {
 			expect( viewWrapperLabel.find( '.shipping-label__banner-fulfilled-message' ) ).to.contain.text( '1 item was fulfilled on Feb 24, 2020' );
 		} );
+
+		it( 'renders redo label button', function() {
+			expect( viewWrapperLabel.find( '.shipping-label__redo-shipping-button' ) ).to.have.length( 1 );
+		} );
+
+		it( 'renders track label button', function() {
+			expect( viewWrapperLabel.find( 'Button' ).last().dive() ).to.contain.text( 'Track Packages' );
+		} );
 	} );
 
-	describe( 'for 2 items fulfilled', () => {
+	describe( 'for two items fulfilled by two labels', () => {
 		const viewWrapperLabel = createViewWrapperLabelWrapper( { items:2, events: labelEventsTwoPurchased } );
 
-		it( 'renders singular fulfilled message', function() {
+		it( 'renders plural fulfilled message', function() {
 			expect( viewWrapperLabel.find( '.shipping-label__banner-fulfilled-message' ) ).to.contain.text( '2 items were fulfilled on Feb 24, 2020' );
+		} );
+
+		it( 'renders redo label button', function() {
+			expect( viewWrapperLabel.find( '.shipping-label__redo-shipping-button' ) ).to.have.length( 1 );
+		} );
+
+		it( 'renders track label button', function() {
+			expect( viewWrapperLabel.find( 'Button' ).last().dive() ).to.contain.text( 'Track Packages' );
+		} );
+	} );
+
+	describe( 'for 1 item fulfilled by 2 labels', () => {
+		const viewWrapperLabel = createViewWrapperLabelWrapper( { items:1, events: labelEventsTwoPurchased } );
+
+		it( 'renders singular fulfilled message', function() {
+			expect( viewWrapperLabel.find( '.shipping-label__banner-fulfilled-message' ) ).to.contain.text( '1 item was fulfilled on Feb 24, 2020' );
+		} );
+
+		it( 'renders redo label button', function() {
+			expect( viewWrapperLabel.find( '.shipping-label__redo-shipping-button' ) ).to.have.length( 1 );
+		} );
+
+		it( 'renders track label button', function() {
+			expect( viewWrapperLabel.find( 'Button' ).last().dive() ).to.contain.text( 'Track Packages' );
 		} );
 	} );
 } );
