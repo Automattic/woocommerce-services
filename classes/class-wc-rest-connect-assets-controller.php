26696f8e2bf6c44c3e198f47824d7e874ee307eb
c-shultz <chris.shultz@automattic.com>
2020-03-16 11:24:08 -0700

    Add front end asset URLs endpoint.

---
+++ classes/class-wc-rest-connect-assets-controller.php
@@ -0,0 +1,25 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( class_exists( 'WC_REST_Connect_Assets_Controller' ) ) {
+	return;
+}
+
+class WC_REST_Connect_Assets_Controller extends WC_REST_Connect_Base_Controller {
+
+	protected $rest_base = 'connect/assets';
+
+	public function get() {
+
+		return new WP_REST_Response( array(
+			'success'  => true,
+			'assets' => array(
+				'wc_connect_admin_script' => WC_Connect_Loader::get_wcs_admin_script_url(),
+				'wc_connect_admin_style'  => WC_Connect_Loader::get_wcs_admin_style_url(),
+			),
+		), 200 );
+	}
+}
