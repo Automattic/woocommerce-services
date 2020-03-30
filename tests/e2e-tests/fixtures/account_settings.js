7bfcea6d62d030d3984cc23955119d97a77bc0f7
Harris Wong <harris.wong@automattic.com>
2020-03-30 17:22:37 -0600

    Add tests for payment method
    3 tests in total. 1 with no credit card. 1 with visa. The last one with both
    visa and mastercard.

---
+++ tests/e2e-tests/fixtures/account_settings.js
@@ -0,0 +1,64 @@
+const AccountWithNoCreditCard = {
+	"success": true,
+	"storeOptions": {
+		"currency_symbol": "$",
+		"dimension_unit": "cm",
+		"weight_unit": "kg",
+		"origin_country": "US"
+	},
+	"formData": {
+		"selected_payment_method_id": 0,
+		"enabled": true,
+		"email_receipts": true,
+		"paper_size": "legal"
+	},
+	"formMeta": {
+		"can_manage_payments": true,
+		"can_edit_settings": true,
+		"master_user_name": "johndoe",
+		"master_user_login": "johndoe",
+		"master_user_wpcom_login": "johndoe",
+		"master_user_email": "john.doe@automattic.com",
+		"payment_methods": [],
+		"warnings": {
+			"payment_methods": false
+		}
+	},
+	"userMeta": {
+		"last_box_id": "Box"
+	}
+}
+
+const VisaCard5959 = {
+	"payment_method_id": 9273191,
+	"name": "John Doe",
+	"card_type": "visa",
+	"card_digits": "5959",
+	"expiry": "2025-12-31"
+}
+
+const Mastercard2862 = {
+	"payment_method_id": 6717123,
+	"name": "John Doe",
+	"card_type": "mastercard",
+	"card_digits": "2862",
+	"expiry": "2025-12-31"
+}
+
+const AccountWithOneCreditCard = {
+	...AccountWithNoCreditCard
+};
+AccountWithOneCreditCard.formData.selected_payment_method_id = 9273191;
+AccountWithOneCreditCard.formMeta.payment_methods.push(VisaCard5959);
+
+const AccountWithTwoCreditCard = {
+	...AccountWithOneCreditCard
+};
+AccountWithOneCreditCard.formData.selected_payment_method_id = 6717123; // set default card
+AccountWithTwoCreditCard.formMeta.payment_methods.push(Mastercard2862);
+
+module.exports = {
+	AccountWithNoCreditCard,
+	AccountWithOneCreditCard,
+	AccountWithTwoCreditCard
+};
--- tests/e2e-tests/specs/admin/shipping-settings.test.js
+++ tests/e2e-tests/specs/admin/shipping-settings.test.js
@@ -3,25 +3,100 @@
  */
 import { clickReactButton } from '../../utils/index';
 import { StoreOwnerFlow } from "../../utils/flows";
+import { AccountWithNoCreditCard, AccountWithOneCreditCard, AccountWithTwoCreditCard } from "../../fixtures/account_settings";
 
-describe( 'Shipping label settings', () => {
+// describe( 'Saving shipping label settings', () => {
+// 	it( 'Can toggle shipping labels' , async () => {
+// 		let slug = 'woocommerce-services';
+// 		await StoreOwnerFlow.login();
+// 		await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
+//         await page.waitForSelector('.form-toggle__switch');
+//         await expect(page).toClick('.form-toggle__switch');
+//         await page.waitForSelector('.label-settings__cards-label form-label', {
+//             hidden: true
+//         });
+//         await expect( page ).toClick( '.button.is-primary', { text: 'Save changes' } );
+//         // Clicking save should persist the hidden state of the section.
+//         await page.waitForSelector('.label-settings__cards-label form-label', {
+//             hidden: true
+//         });
 
-	it( 'Can toggle to disable shipping labels' , async () => {
-		let slug = 'woocommerce-services';
-		await StoreOwnerFlow.login();
-		await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
-        await page.waitForSelector('.form-toggle__switch');
-        await expect(page).toClick('.form-toggle__switch');
-        await page.waitForSelector('.label-settings__cards-label form-label', {
-            hidden: true
+//         // Enable
+//         await expect(page).toClick('.form-toggle__switch');
+//         await expect( page ).toClick( '.button.is-primary', { text: 'Save changes' } );
+//     });
+// } );
+
+describe( 'Shipping label payment method', () => {
+    // it('should show "Choose a different card" button if Wordpress.com has a credit card', async () => {
+    //     await page.setRequestInterception(true);
+    //     page.on('request', request => {
+    //         if (request.url().match('wp-json/wc/v1/connect/account/settings')) {
+    //             request.respond({
+    //                 status: 200,
+    //                 contentType: 'application/json; charset=UTF-8',
+    //                 body: JSON.stringify(AccountWithOneCreditCard)
+    //             });
+    //         } else {
+    //             request.continue();
+    //         }
+    //     });
+
+    //     await StoreOwnerFlow.login();
+    //     await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
+    //     await page.waitForSelector('.button.is-borderless', { text: 'Choose a different card' } );
+    //     await expect( page ).toClick( '.button.is-borderless', { text: 'Choose a different card' } );
+    //     await expect(page).toMatchElement('.label-settings__card-number', {
+    //         text: 'VISA ****5959'
+    //     });
+    //     await expect(page).toMatchElement('.label-settings__card-name', {
+    //         text: 'John Doe'
+    //     });
+    // });
+
+    // it('should show "Add a credit card" button if Wordpress has no credit card', async () => {
+    //     await page.setRequestInterception(true);
+    //     page.on('request', request => {
+    //         if (request.url().match('wp-json/wc/v1/connect/account/settings')) {
+    //             request.respond({
+    //                 status: 200,
+    //                 contentType: 'application/json; charset=UTF-8',
+    //                 body: JSON.stringify(AccountWithNoCreditCard)
+    //             });
+    //         } else {
+    //             request.continue();
+    //         }
+    //     });
+
+    //     await StoreOwnerFlow.login();
+    //     await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
+    //     await page.waitForSelector('.button.is-compact', { text: 'Add a credit card' } );
+    // });
+
+    it('should show "Choose a different card" button if Wordpress.com has 2 credit cards', async () => {
+        console.log(AccountWithTwoCreditCard);
+        await page.setRequestInterception(true);
+        page.on('request', request => {
+            if (request.url().match('wp-json/wc/v1/connect/account/settings')) {
+                request.respond({
+                    status: 200,
+                    contentType: 'application/json; charset=UTF-8',
+                    body: JSON.stringify(AccountWithTwoCreditCard)
+                });
+            } else {
+                request.continue();
+            }
         });
-        await expect( page ).toClick( '.button.is-primary', { text: 'Save changes' } );
-        // Clicking save should persist the hidden state of the section.
-        await page.waitForSelector('.label-settings__cards-label form-label', {
-            hidden: true
+
+        await StoreOwnerFlow.login();
+        await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
+        await page.waitForSelector('.button.is-borderless', { text: 'Choose a different card' } );
+        await expect( page ).toClick( '.button.is-borderless', { text: 'Choose a different card' } );
+        await expect(page).toMatchElement('.label-settings__card-number', {
+            text: 'MasterCard ****2862'
+        });
+        await expect(page).toMatchElement('.label-settings__card-name', {
+            text: 'John Doe'
         });
     });
-
-
-
-} );
+});
