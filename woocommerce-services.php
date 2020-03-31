c9dbfb70a64a54c7d97fefeee7175b65fdfdd8f4
c-shultz <chris.shultz@automattic.com>
2020-03-31 13:02:25 -0700

    Version bump to prep for release.

--- woocommerce-services.php
+++ woocommerce-services.php
@@ -7,7 +7,7 @@
  * Author URI: https://woocommerce.com/
  * Text Domain: woocommerce-services
  * Domain Path: /i18n/languages/
- * Version: 1.22.5
+ * Version: 1.23.0
  * WC requires at least: 3.0.0
  * WC tested up to: 4.0
  *
