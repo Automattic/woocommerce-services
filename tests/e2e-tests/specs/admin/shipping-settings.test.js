2a4f8acc847383cd8102f6f70a6756387c5a8791
Harris Wong <harris.wong@automattic.com>
2020-04-15 15:19:38 -0600

    Remove single listener instead of all listener

--- tests/e2e-tests/specs/admin/shipping-settings.test.js
+++ tests/e2e-tests/specs/admin/shipping-settings.test.js
@@ -18,7 +18,7 @@ const saveAndWait = async () => {
  */
 const waitForSelectorAndText = async (selector, text) => {
     return await page.waitForFunction(
-        (cssSelector, innerTextContent) => Array.from(document.querySelectorAll(cssSelector)).find(el => el.textContent.trim() === innerTextContent.trim()),
+        (cssSelector, innerTextContent) => !!Array.from(document.querySelectorAll(cssSelector)).find(el => el.textContent.trim() === innerTextContent.trim()),
         {},
         selector, text
     );
@@ -65,141 +65,147 @@ describe( 'Saving shipping label settings', () => {
     });
 } );
 
-// describe( 'Shipping label payment method', () => {
-//     afterEach(() => {
-//         page.removeAllListeners('request');
-//     });
+describe( 'Shipping label payment method', () => {
+    let response;
 
-//     const mockAccountSettingAPI = async (response) => {
-//         await page.setRequestInterception(true);
-//         page.on('request', request => {
-//             if (request.url().match('wp-json/wc/v1/connect/account/settings')) {
-//                 request.respond({
-//                     status: 200,
-//                     contentType: 'application/json; charset=UTF-8',
-//                     body: JSON.stringify(response)
-//                 });
-//             } else {
-//                 request.continue();
-//             }
-//         });
-//     };
+    const accountSettingsRequestListener = (request) => {
+        if (request.url().match('wp-json/wc/v1/connect/account/settings')) {
+            request.respond({
+                status: 200,
+                contentType: 'application/json; charset=UTF-8',
+                body: JSON.stringify(response)
+            });
+        } else {
+            request.continue();
+        }
+    };
 
-//     it('should show "Add a credit card" button if Wordpress has no credit card', async () => {
-//         // Intercept API before making any HTTP request
-//         await mockAccountSettingAPI(AccountWithNoCreditCard);
+    const mockAccountSettingAPI = async (mockResponse) => {
+        response = mockResponse;
+        await page.setRequestInterception(true);
+        page.on('request', accountSettingsRequestListener);
+    };
 
-//         // Go to settings page after setting up interception.
-//         await StoreOwnerFlow.login();
-//         await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
+    afterEach(() => {
+        response = '';
+        page.removeListener('request', accountSettingsRequestListener);
+    });
 
-//         // Test
-//         await waitForSelectorAndText('.button.is-compact', 'Add a credit card');
-//     });
+    it('should show "Add a credit card" button if Wordpress has no credit card', async () => {
+        // Intercept API before making any HTTP request
+        await mockAccountSettingAPI(AccountWithNoCreditCard);
 
-//     it('should show "Choose a different card" button if Wordpress.com has a credit card', async () => {
-//         // Intercept API before making any HTTP request
-//         await mockAccountSettingAPI(AccountWithOneCreditCard);
+        // Go to settings page after setting up interception.
+        await StoreOwnerFlow.login();
+        await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
 
-//         // No need to login again, refresh page
-//         await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
+        // Test
+        await waitForSelectorAndText('.button.is-compact', 'Add a credit card');
+    });
 
-//         // Test
-//         await waitForSelectorAndText('.button.is-borderless', 'Choose a different card');
-//         await expect( page ).toClick( '.button.is-borderless', { text: 'Choose a different card' } );
-//         await expect(page).toMatchElement('.label-settings__card-number', {
-//             text: 'VISA ****5959'
-//         });
-//         await expect(page).toMatchElement('.label-settings__card-name', {
-//             text: 'John Doe'
-//         });
-//     });
+    it('should show "Choose a different card" button if Wordpress.com has a credit card', async () => {
+        // Intercept API before making any HTTP request
+        await mockAccountSettingAPI(AccountWithOneCreditCard);
 
-//     it('should show 2 credit cards with the right information if Wordpress.com has 2 credit cards', async () => {
-//         // Intercept API before making any HTTP request
-//         await mockAccountSettingAPI(AccountWithTwoCreditCard);
+        // No need to login again, refresh page
+        await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
 
-//         // No need to login again, refresh page
-//         await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
+        // Test
+        await waitForSelectorAndText('.button.is-borderless', 'Choose a different card');
+        await expect( page ).toClick( '.button.is-borderless', { text: 'Choose a different card' } );
+        await expect(page).toMatchElement('.label-settings__card-number', {
+            text: 'VISA ****5959'
+        });
+        await expect(page).toMatchElement('.label-settings__card-name', {
+            text: 'John Doe'
+        });
+    });
 
-//         // Test
-//         await waitForSelectorAndText('.button.is-borderless', 'Choose a different card' );
-//         await expect( page ).toClick( '.button.is-borderless', { text: 'Choose a different card' } );
+    it('should show 2 credit cards with the right information if Wordpress.com has 2 credit cards', async () => {
+        // Intercept API before making any HTTP request
+        await mockAccountSettingAPI(AccountWithTwoCreditCard);
 
-//         // Verify "Add another credit card" is present after clicking 'Choose a different card'
-//         await waitForSelectorAndText('.button.is-compact', 'Add another credit card' );
+        // No need to login again, refresh page
+        await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
 
-//         // The index is based on the order in the settings' API. Inside the payment_methods prop.
-//         // This API end point is mocked, check fixtures/account_settings.js for ordering.
-//         const VISA_CARD_INDEX = 0;
-//         const MASTERCARD_INDEX = 1;
+        // Test
+        await waitForSelectorAndText('.button.is-borderless', 'Choose a different card' );
+        await expect( page ).toClick( '.button.is-borderless', { text: 'Choose a different card' } );
 
-//         const cardNumbers = await page.$$('.label-settings__card-number');
-//         const firstCardNumber = await (await cardNumbers[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(firstCardNumber).toEqual('VISA ****5959');
-//         const secondCardNumber = await (await cardNumbers[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(secondCardNumber).toEqual('MasterCard ****2862');
+        // Verify "Add another credit card" is present after clicking 'Choose a different card'
+        await waitForSelectorAndText('.button.is-compact', 'Add another credit card' );
 
-//         const cardNames = await page.$$('.label-settings__card-name');
-//         const firstCardName = await (await cardNames[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(firstCardName).toEqual('John Doe');
-//         const secondCardName = await (await cardNames[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(secondCardName).toEqual('Jane Smith');
+        // The index is based on the order in the settings' API. Inside the payment_methods prop.
+        // This API end point is mocked, check fixtures/account_settings.js for ordering.
+        const VISA_CARD_INDEX = 0;
+        const MASTERCARD_INDEX = 1;
 
-//         const cardExpiryDates = await page.$$('.label-settings__card-date');
-//         const firstCardExpiryDate = await (await cardExpiryDates[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(firstCardExpiryDate).toEqual('Expires 2030-06-10');
-//         const secondCardExpiryDate = await (await cardExpiryDates[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(secondCardExpiryDate).toEqual('Expires 2025-12-31');
+        const cardNumbers = await page.$$('.label-settings__card-number');
+        const firstCardNumber = await (await cardNumbers[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(firstCardNumber).toEqual('VISA ****5959');
+        const secondCardNumber = await (await cardNumbers[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(secondCardNumber).toEqual('MasterCard ****2862');
 
-//         // Verify the default box is checked.
-//         const cardDefaultCheckbox = await page.$$('.label-settings__card-checkbox.form-checkbox');
-//         const firstCardDefaultCheckbox = await (await cardDefaultCheckbox[VISA_CARD_INDEX].getProperty('checked')).jsonValue();
-//         expect(firstCardDefaultCheckbox).toBeFalsy();
-//         const secondCardDefaultCheckbox = await (await cardDefaultCheckbox[MASTERCARD_INDEX].getProperty('checked')).jsonValue();
-//         expect(secondCardDefaultCheckbox).toBeTruthy();
-//     });
+        const cardNames = await page.$$('.label-settings__card-name');
+        const firstCardName = await (await cardNames[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(firstCardName).toEqual('John Doe');
+        const secondCardName = await (await cardNames[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(secondCardName).toEqual('Jane Smith');
 
-//     it('should have no default card checked if there are multiple cards in Wordpress.com', async () => {
-//         await mockAccountSettingAPI(AccountWithTwoCreditCardAndNoDefault);
+        const cardExpiryDates = await page.$$('.label-settings__card-date');
+        const firstCardExpiryDate = await (await cardExpiryDates[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(firstCardExpiryDate).toEqual('Expires 2030-06-10');
+        const secondCardExpiryDate = await (await cardExpiryDates[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(secondCardExpiryDate).toEqual('Expires 2025-12-31');
 
-//         // No need to login again, refresh page
-//         await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
+        // Verify the default box is checked.
+        const cardDefaultCheckbox = await page.$$('.label-settings__card-checkbox.form-checkbox');
+        const firstCardDefaultCheckbox = await (await cardDefaultCheckbox[VISA_CARD_INDEX].getProperty('checked')).jsonValue();
+        expect(firstCardDefaultCheckbox).toBeFalsy();
+        const secondCardDefaultCheckbox = await (await cardDefaultCheckbox[MASTERCARD_INDEX].getProperty('checked')).jsonValue();
+        expect(secondCardDefaultCheckbox).toBeTruthy();
+    });
 
-//         // Verify "Add another credit card" is present after clicking 'Choose a different card'
-//         await waitForSelectorAndText('.button.is-compact', 'Add another credit card' );
+    it('should have no default card checked if there are multiple cards in Wordpress.com', async () => {
+        await mockAccountSettingAPI(AccountWithTwoCreditCardAndNoDefault);
 
-//         // The index is based on the order in the settings' API. Inside the payment_methods prop.
-//         // This API end point is mocked, check fixtures/account_settings.js for ordering.
-//         const VISA_CARD_INDEX = 0;
-//         const MASTERCARD_INDEX = 1;
+        // No need to login again, refresh page
+        await StoreOwnerFlow.openSettings('shipping', 'woocommerce-services-settings');
 
-//         const cardNumbers = await page.$$('.label-settings__card-number');
-//         const firstCardNumber = await (await cardNumbers[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(firstCardNumber).toEqual('VISA ****5959');
-//         const secondCardNumber = await (await cardNumbers[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(secondCardNumber).toEqual('MasterCard ****2862');
+        // Verify "Add another credit card" is present after clicking 'Choose a different card'
+        await waitForSelectorAndText('.button.is-compact', 'Add another credit card' );
 
-//         const cardNames = await page.$$('.label-settings__card-name');
-//         const firstCardName = await (await cardNames[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(firstCardName).toEqual('John Doe');
-//         const secondCardName = await (await cardNames[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(secondCardName).toEqual('Jane Smith');
+        // The index is based on the order in the settings' API. Inside the payment_methods prop.
+        // This API end point is mocked, check fixtures/account_settings.js for ordering.
+        const VISA_CARD_INDEX = 0;
+        const MASTERCARD_INDEX = 1;
 
-//         const cardExpiryDates = await page.$$('.label-settings__card-date');
-//         const firstCardExpiryDate = await (await cardExpiryDates[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(firstCardExpiryDate).toEqual('Expires 2030-06-10');
-//         const secondCardExpiryDate = await (await cardExpiryDates[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
-//         expect(secondCardExpiryDate).toEqual('Expires 2025-12-31');
+        const cardNumbers = await page.$$('.label-settings__card-number');
+        const firstCardNumber = await (await cardNumbers[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(firstCardNumber).toEqual('VISA ****5959');
+        const secondCardNumber = await (await cardNumbers[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(secondCardNumber).toEqual('MasterCard ****2862');
 
-//         // Verify that no default box is checked.
-//         const cardDefaultCheckbox = await page.$$('.label-settings__card-checkbox.form-checkbox');
-//         const firstCardDefaultCheckbox = await (await cardDefaultCheckbox[VISA_CARD_INDEX].getProperty('checked')).jsonValue();
-//         expect(firstCardDefaultCheckbox).toBeFalsy();
-//         const secondCardDefaultCheckbox = await (await cardDefaultCheckbox[MASTERCARD_INDEX].getProperty('checked')).jsonValue();
-//         expect(secondCardDefaultCheckbox).toBeFalsy();
-//     });
-// });
+        const cardNames = await page.$$('.label-settings__card-name');
+        const firstCardName = await (await cardNames[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(firstCardName).toEqual('John Doe');
+        const secondCardName = await (await cardNames[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(secondCardName).toEqual('Jane Smith');
+
+        const cardExpiryDates = await page.$$('.label-settings__card-date');
+        const firstCardExpiryDate = await (await cardExpiryDates[VISA_CARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(firstCardExpiryDate).toEqual('Expires 2030-06-10');
+        const secondCardExpiryDate = await (await cardExpiryDates[MASTERCARD_INDEX].getProperty('innerText')).jsonValue();
+        expect(secondCardExpiryDate).toEqual('Expires 2025-12-31');
+
+        // Verify that no default box is checked.
+        const cardDefaultCheckbox = await page.$$('.label-settings__card-checkbox.form-checkbox');
+        const firstCardDefaultCheckbox = await (await cardDefaultCheckbox[VISA_CARD_INDEX].getProperty('checked')).jsonValue();
+        expect(firstCardDefaultCheckbox).toBeFalsy();
+        const secondCardDefaultCheckbox = await (await cardDefaultCheckbox[MASTERCARD_INDEX].getProperty('checked')).jsonValue();
+        expect(secondCardDefaultCheckbox).toBeFalsy();
+    });
+});
 
 describe( 'Packaging', () => {
     it( 'Can add package' , async () => {
