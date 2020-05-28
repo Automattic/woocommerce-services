0f86b339c198a63f97c53189e8d927fe7ae0f209
David Stone <david@nnucomputerwhiz.com>
2020-05-28 13:37:02 -0600

    Add logos to rate list
    closes #2029

--- client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/rates-step/carrier-logo.js
+++ client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/rates-step/carrier-logo.js
@@ -5,16 +5,25 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import fedex from './logos/fedex.svg';
+import usps from './logos/usps.png';
+import ups from './logos/ups.png';
 
 /**
  * Internal dependencies
  */
+import { translate } from 'i18n-calypso';
 
 function CarrierLogo( { carrier_id } ) {
 	switch ( carrier_id ) {
+		case 'fedex':
+			return <div><img alt={ translate( 'Fedex' ) } src={ fedex } /></div>;
+		case 'usps':
+			return <div><img alt={ translate( 'USPS' ) } src={ usps } /></div>;
+		case 'ups':
+			return <div><img alt={ translate( 'UPS' ) } src={ ups } /></div>;
 		default:
-			return <div/>;
+			return <div />;
 	}
 }
 
