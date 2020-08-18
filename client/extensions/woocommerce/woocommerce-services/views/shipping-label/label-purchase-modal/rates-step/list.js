0f0da0e24f7626e9a8daa96ab99823bef8650583
Brian Borman <brian.borman@automattic.com>
2020-08-18 14:50:31 -0400

    Filter out duplicate rate error messages on front-end

--- client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/rates-step/list.js
+++ client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-purchase-modal/rates-step/list.js
@@ -51,7 +51,8 @@ export const ShippingRates = ( {
 		}
 		const selectedRate = selectedRates[ pckgId ] || '';
 		const packageRates = availableRates[ pckgId ].default.rates;
-		const packageErrors = errors[ pckgId ] || [];
+		// filter out duplicate error messages
+		const packageErrors = errors[ pckgId ].filter( ( item, index ) => errors[ pckgId ].indexOf( item ) === index ) || [];
 
 		const onRateUpdate = ( serviceId, signatureRequired ) => updateRate( pckgId, serviceId, signatureRequired );
 		return (
