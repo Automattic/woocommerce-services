96d169103150e13360b5ac36e3fc8608f7437c99
Brian Borman <brian.borman@automattic.com>
2020-10-13 09:36:03 -0400

    Lookup carrier label for email tracking URL

--- woocommerce-services.php
+++ woocommerce-services.php
@@ -1040,7 +1040,8 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 			// Generate a table row for each label
 			foreach ( $labels as $label ) {
 				$carrier = $label['carrier_id'];
-				$carrier_label = strtoupper( $carrier );
+				$carrier_service = $this->get_service_schemas_store()->get_service_schema_by_id( $carrier );
+				$carrier_label = ( ! empty( $carrier_service->carrier_name ) ) ? $carrier_service->carrier_name : strtoupper( $carrier );
 				$tracking = $label['tracking'];
 				$error = array_key_exists( 'error', $label );
 				$refunded = array_key_exists( 'refund', $label );
