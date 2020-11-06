72cd7196c6b03efcade6de90984e41d043007436
Harris Wong <harris.wong@automattic.com>
2020-11-06 16:26:51 -0700

    Add test for DynamicCarrierAccountSettings

--- client/extensions/woocommerce/woocommerce-services/views/carrier-accounts/test/dynamic-settings.js
+++ client/extensions/woocommerce/woocommerce-services/views/carrier-accounts/test/dynamic-settings.js
@@ -5,8 +5,11 @@
  */
 import React from 'react';
 import { expect } from 'chai';
-import { configure, shallow } from 'enzyme';
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import * as api from 'woocommerce/woocommerce-services/api';
+import { act } from 'react-dom/test-utils';
+
 /**
  * Internal dependencies
  */
@@ -16,7 +19,7 @@ configure( { adapter: new Adapter() } );
 
 const mockCarrierRegistrationFieldsState = [{
     "type": "DhlExpressAccount",
-    "name": "DHL Express",
+    "name": "Test DHL Express",
     "fields": {
         "account_number": {
             "visibility": "visible",
@@ -33,16 +36,25 @@ const mockCarrierRegistrationFieldsState = [{
     }
 }];
 
-/**
- * useEffect not called when shallow mount. Refer to bug https://github.com/enzymejs/enzyme/issues/2086
- * Here, we try to start the component with a pre-defined state.
- */
-jest.mock('react', () => ({
-    ...jest.requireActual('react'),
-    useState: () => ([mockCarrierRegistrationFieldsState]),
-}));
+jest.mock('../dynamic-settings-form', () => {
+    return function DummyDynamicSettingsForm(props) {
+        return (
+        <div>
+            {JSON.stringify(props)}
+        </div>
+        );
+    }
+});
 
 describe( 'Dynamic carrier registration settings', () => {
+    beforeEach(() => {
+        jest.spyOn(api, "get").mockImplementation(() =>
+            Promise.resolve({
+                carriers: mockCarrierRegistrationFieldsState
+            })
+        );
+    });
+
     afterEach(() => {
         jest.clearAllMocks();
     });
@@ -52,9 +64,18 @@ describe( 'Dynamic carrier registration settings', () => {
             siteId: 1234,
             carrier: 'DhlExpressAccount'
         };
-		it( 'should render the Connect, Localized, and DynamicCarrierAccountSettingsForm sub components', function () {
-            const wrapper = shallow( <DynamicCarrierAccountSettings { ...props } /> );
-			expect(wrapper.text()).to.match(/Connect\(Localized\(DynamicCarrierAccountSettingsForm/);
+
+
+		it( 'should render the Connect, Localized, and DynamicCarrierAccountSettingsForm sub components', async () => {
+            let wrapper;
+            await act(async () => {
+                wrapper = mount(<DynamicCarrierAccountSettings { ...props } />);
+            });
+
+            const actualDynamicCarrierAccountSettingsFormProps = JSON.parse(wrapper.text());
+            expect(actualDynamicCarrierAccountSettingsFormProps.carrierName).to.equal(mockCarrierRegistrationFieldsState[0].name);
+            expect(actualDynamicCarrierAccountSettingsFormProps.carrierType).to.equal(mockCarrierRegistrationFieldsState[0].type);
+            expect(actualDynamicCarrierAccountSettingsFormProps.registrationFields).to.deep.equal(mockCarrierRegistrationFieldsState[0].fields);
         } );
     } );
 
@@ -63,9 +84,44 @@ describe( 'Dynamic carrier registration settings', () => {
             siteId: 1234,
             carrier: 'ASDASDASD'
         };
-		it( 'should render the Connect, Localized, and DynamicCarrierAccountSettingsForm sub components', function () {
-            const wrapper = shallow( <DynamicCarrierAccountSettings { ...props } /> );
+
+
+		it( 'should display not supported message', async () => {
+            let wrapper;
+            await act(async () => {
+                wrapper = mount(<DynamicCarrierAccountSettings { ...props } />);
+            });
             expect(wrapper.text()).to.equal('ASDASDASD not supported.');
         } );
+    } );
+} );
+
+describe( 'Dynamic carrier registration settings with pending promises', () => {
+    /**
+     * Moved this into its own describe block to test pending promise
+     * so that it doesn't change all the async-await promises in the above
+     * test cases.
+     */
+    beforeEach(() => {
+        jest.spyOn(api, "get").mockImplementation(() =>
+            Promise.resolve({})
+        );
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    const props = {
+        siteId: 1234,
+        carrier: 'DhlExpressAccount'
+    };
+
+    it( 'should display a loading message', async () => {
+        let wrapper;
+        await act(async () => {
+            wrapper = mount(<DynamicCarrierAccountSettings { ...props } />);
+        });
+        expect(wrapper.text()).to.equal('Loading...');
     } );
 } );
