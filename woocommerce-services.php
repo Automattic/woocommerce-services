a27ef299c56d03783bf8b5a9654abd4cfde934cf
frosso <francesco.rosso@automattic.com>
2020-11-20 18:52:06 -0600

    WIP

--- woocommerce-services.php
+++ woocommerce-services.php
@@ -48,7 +48,7 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 	define( 'WOOCOMMERCE_CONNECT_MAX_JSON_DECODE_DEPTH', 32 );
 
 	if ( ! defined( 'WOOCOMMERCE_CONNECT_SERVER_API_VERSION ' ) ) {
-		define( 'WOOCOMMERCE_CONNECT_SERVER_API_VERSION', '4');
+		define( 'WOOCOMMERCE_CONNECT_SERVER_API_VERSION', '5');
 	}
 
 	// Check for CI environment variable to trigger test mode.
