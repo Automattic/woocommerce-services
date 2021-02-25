f8440510416f36a329fdadeae639c69d26e72786
Harris Wong <harris.wong@automattic.com>
2021-02-24 17:49:16 -0700

    Unzip into current directory since it is already in a folder

--- tests/bin/install-wc-tests.sh
+++ tests/bin/install-wc-tests.sh
@@ -15,7 +15,7 @@ WC_VERSION=${6-"5.0.0"}
 install_wc() {
 	# TODO: fetch the latest tag
 	curl -Lo /tmp/woocommerce.zip https://github.com/woocommerce/woocommerce/releases/download/5.0.0/woocommerce.zip
-	unzip /tmp/woocommerce.zip -d /tmp/woocommerce
+	unzip /tmp/woocommerce.zip
 }
 
 install_wp() {
