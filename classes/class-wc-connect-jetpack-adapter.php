f352148a1f3253a1a70800f00b05b51a1c0b1edb
frosso <francesco.rosso@automattic.com>
2021-03-02 13:03:27 -0600

    better check with deprecation notice

--- classes/class-wc-connect-jetpack-adapter.php
+++ classes/class-wc-connect-jetpack-adapter.php
@@ -5,14 +5,14 @@ use Automattic\Jetpack\Connection\Manager;
 
 class WC_Connect_Jetpack_Adapter {
 	public static function get_master_user_access_token( $user_id ) {
-		if ( class_exists( '\Automattic\Jetpack\Connection\Manager' ) ) {
-			$connection = new Manager();
+		if ( class_exists( '\Automattic\Jetpack\Connection\Tokens' ) && method_exists( '\Automattic\Jetpack\Connection\Tokens', 'get_access_token' ) ) {
+			$connection = new Tokens();
 
 			return $connection->get_access_token( $user_id );
 		}
 
-		if ( class_exists( '\Automattic\Jetpack\Connection\Tokens' ) ) {
-			$connection = new Tokens();
+		if ( class_exists( '\Automattic\Jetpack\Connection\Manager' ) && method_exists( '\Automattic\Jetpack\Connection\Manager', 'get_access_token' ) ) {
+			$connection = new Manager();
 
 			return $connection->get_access_token( $user_id );
 		}
