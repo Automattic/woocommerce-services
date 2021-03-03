e38dd7435e1a382f5c59c25b1b127932eeedb772
Chris Shultz <chris.shultz@automattic.com>
2021-03-03 12:44:27 -0800

    Make sure Babel config works for both production and test environments.

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,12 @@
 const config = {
-	presets: [
-		[
-			'@babel/preset-env',
-			{ targets: { node: 'current' } },
-			'@automattic/calypso-build/babel/default',
-		],
-	],
+	env: {
+		test: {
+			presets: [ [ '@babel/preset-env', { targets: { node: 'current' } } ] ],
+		},
+		production: {
+			presets: [ '@automattic/calypso-build/babel/default' ],
+		},
+	  }
 };
 
 module.exports = config;
