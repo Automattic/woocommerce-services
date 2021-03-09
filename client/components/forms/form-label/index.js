8d3e78b860bb965a345626825df643378e9ffc40
Brian Borman <brian.borman@automattic.com>
2021-03-09 09:53:27 -0500

    Cleanup internal FormLabel component

--- client/components/forms/form-label/index.js
+++ client/components/forms/form-label/index.js
@@ -8,41 +8,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { localize } from 'i18n-calypso';
-import { isObject, omit } from 'lodash';
 
-const renderRequiredBadge = translate => (
-	<small className="form-label__required">{ translate( 'Required' ) }</small>
-);
-
-const renderOptionalBadge = translate => (
-	<small className="form-label__optional">{ translate( 'Optional' ) }</small>
-);
-
-const addKeys = elements =>
-	elements.map( ( elem, idx ) => ( isObject( elem ) ? { ...elem, key: idx } : elem ) );
-
-const FormLabel = ( { children, required, optional, translate, className, ...extraProps } ) => {
-	children = React.Children.toArray( children ) || [];
-	if ( required ) {
-		children.push( renderRequiredBadge( translate ) );
-	}
-
-	if ( optional ) {
-		children.push( renderOptionalBadge( translate ) );
-	}
-
+const FormLabel = ( { children, required, optional, translate, className, moment, numberFormat, ...extraProps } ) => {
 	return (
 		<label
-			{ ...omit( extraProps, 'moment', 'numberFormat' ) }
+			{ ...extraProps }
 			className={ classnames( className, 'form-label' ) }
 		>
-			{ children.length ? addKeys( children ) : null }
+			{ children }
+			{required && ( <small className="form-label__required">{ translate( 'Required' ) }</small> ) }
+			{optional && ( <small className="form-label__optional">{ translate( 'Optional' ) }</small> )}
 		</label>
 	);
 };
 
 FormLabel.propTypes = {
 	required: PropTypes.bool,
+	optional: PropTypes.bool,
+	children: PropTypes.node,
 };
 
 export default localize( FormLabel );
