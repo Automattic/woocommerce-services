efecf2405a9e8e3feda0f6b57d1bc510e151962a
frosso <francesco.rosso@automattic.com>
2021-03-10 11:50:14 -0600

    fix: broken status page

--- client/components/forms/form-label/index.js
+++ client/components/forms/form-label/index.js
@@ -9,15 +9,25 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { localize } from 'i18n-calypso';
 
-const FormLabel = ( { children, required, optional, translate, className, moment, numberFormat, ...extraProps } ) => {
+const FormLabel = ( { children: childrenProp, required, optional, translate, className, moment, numberFormat, dangerouslySetInnerHTML, ...extraProps } ) => {
+	const children = dangerouslySetInnerHTML ? null : (
+		<>
+			{ childrenProp }
+			{/* eslint-disable-next-line wpcalypso/jsx-classname-namespace */}
+			{ required && ( <small className="form-label__required">{ translate( 'Required' ) }</small> ) }
+			{/* eslint-disable-next-line wpcalypso/jsx-classname-namespace */}
+			{ optional && ( <small className="form-label__optional">{ translate( 'Optional' ) }</small> ) }
+		</>
+	);
+
 	return (
 		<label
 			{ ...extraProps }
+			/* eslint-disable-next-line react/no-danger */
+			dangerouslySetInnerHTML={ dangerouslySetInnerHTML }
 			className={ classnames( className, 'form-label' ) }
 		>
 			{ children }
-			{required && ( <small className="form-label__required">{ translate( 'Required' ) }</small> ) }
-			{optional && ( <small className="form-label__optional">{ translate( 'Optional' ) }</small> )}
 		</label>
 	);
 };
