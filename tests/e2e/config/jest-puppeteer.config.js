0f40bd823c2fc4b947b3adcebf26084cfc961d91
Samir Merchant <samir.merchant@automattic.com>
2021-03-11 12:57:52 -0500

    add puppeteer config for more consistent testing results

--- tests/e2e/config/jest-puppeteer.config.js
+++ tests/e2e/config/jest-puppeteer.config.js
@@ -2,8 +2,6 @@ const { useE2EJestPuppeteerConfig } = require( '@woocommerce/e2e-environment' );
 
 const puppeteerConfig = useE2EJestPuppeteerConfig( {
 	launch: {
-		headless: false,
-		devtools: true,
 		slowMo: 100,
 		args: [ '--window-size=1920,1080', '--user-agent=chrome --runInBand --maxWorkers=4' ],
 	}
