22932de8c383a8b14befdbe1b6ea324edd62051f
frosso <francesco.rosso@automattic.com>
2021-03-12 13:54:57 -0600

    IDK

--- client/extensions/woocommerce/woocommerce-services/views/live-rates-carriers-list/carriers-list.js
+++ client/extensions/woocommerce/woocommerce-services/views/live-rates-carriers-list/carriers-list.js
@@ -12,28 +12,28 @@ import Card from 'components/card'
 import CarrierIcon from '../../components/carrier-icon'
 import Gridicon from 'gridicons'
 
-const Actions = localize(({ translate }) => {
+const Actions = localize( ( { translate } ) => {
 	return (
 		<>
 			{ /* eslint-disable-next-line wpcalypso/jsx-classname-namespace */}
-			<a className="button is-compact" href="admin.php?page=wc-settings&tab=shipping&section">{translate('Add to shipping zones')}</a>
+			<a className="button is-compact" href="admin.php?page=wc-settings&tab=shipping&section">{ translate( 'Add to shipping zones' ) }</a>
 			<Tooltip
 				position="top left"
-				text={translate('To be displayed at checkout, this carrier must be added as a shipping method to selected shipping zones')}
+				text={ translate( 'To be displayed at checkout, this carrier must be added as a shipping method to selected shipping zones' ) }
 			>
 				<div>
-					<Gridicon icon="help-outline" size={18}/>
+					<Gridicon icon="help-outline" size={ 18 }/>
 				</div>
 			</Tooltip>
 		</>
 	)
 })
 
-const CarrierDiscount = localize(({
+const CarrierDiscount = localize( ( {
 	translate,
 	name,
 	amount
-}) => translate('Up to {{strong}}%(discountAmount)s{{/strong}} off on %(carrierName)s shipping labels', {
+} ) => translate( 'Up to {{strong}}%(discountAmount)s{{/strong}} off on %(carrierName)s shipping labels', {
 	args: {
 		carrierName: name,
 		discountAmount: amount,
@@ -44,51 +44,51 @@ const CarrierDiscount = localize(({
 }))
 
 const carrierItemMap = {
-	'wc_services_usps': ({ translate }) => (
+	'wc_services_usps': ( { translate } ) => (
 		<div className="live-rates-carriers-list__element element-usps">
 			<div className="live-rates-carriers-list__icon">
-				<CarrierIcon carrier="usps" size={18}/>
+				<CarrierIcon carrier="usps" size={ 18 } />
 			</div>
-			<div className="live-rates-carriers-list__carrier">{translate('USPS')}</div>
+			<div className="live-rates-carriers-list__carrier">{ translate( 'USPS' ) }</div>
 			<div className="live-rates-carriers-list__features">
 				<ul>
-					<li>{translate('Ship with the largest delivery network in the United States')}</li>
+					<li>{ translate( 'Ship with the largest delivery network in the United States' ) }</li>
 					<li>
-						<CarrierDiscount name={translate('USPS')} amount="81%"/>
+						<CarrierDiscount name={ translate( 'USPS' ) } amount="81%" />
 					</li>
 					<li>
-						{translate('Live rates for %(carrierName)s at checkout', {
+						{ translate( 'Live rates for %(carrierName)s at checkout', {
 							args: {
-								carrierName: translate('USPS'),
+								carrierName: translate( 'USPS' ),
 							},
 						})}
 					</li>
 				</ul>
 			</div>
-			<div className="live-rates-carriers-list__actions"><Actions/></div>
+			<div className="live-rates-carriers-list__actions"><Actions /></div>
 		</div>
 
 	),
 	'wc_services_dhlexpress': ({ translate }) => (
 		<div className="live-rates-carriers-list__element element-dhlexpress">
 			<div className="live-rates-carriers-list__icon">
-				<CarrierIcon carrier="dhlexpress" size={18}/>
+				<CarrierIcon carrier="dhlexpress" size={ 18 } />
 			</div>
-			<div className="live-rates-carriers-list__carrier">{translate('DHL Express')}</div>
+			<div className="live-rates-carriers-list__carrier">{ translate( 'DHL Express' ) }</div>
 			<div className="live-rates-carriers-list__features">
 				<ul>
-					<li>{translate('Express delivery from the experts in international shipping')}</li>
-					<li><CarrierDiscount name={translate('DHL Express')} amount="74%"/></li>
+					<li>{ translate( 'Express delivery from the experts in international shipping' ) }</li>
+					<li><CarrierDiscount name={ translate( 'DHL Express' ) } amount="74%" /></li>
 					<li>
-						{translate('Live rates for %(carrierName)s at checkout', {
+						{ translate( 'Live rates for %(carrierName)s at checkout', {
 							args: {
-								carrierName: translate('DHL Express'),
+								carrierName: translate( 'DHL Express' ),
 							},
 						})}
 					</li>
 				</ul>
 			</div>
-			<div className="live-rates-carriers-list__actions"><Actions/></div>
+			<div className="live-rates-carriers-list__actions"><Actions /></div>
 		</div>
 	),
 }
@@ -98,22 +98,22 @@ const CarriersList = ({ translate, carrierIds }) => {
 		<Card className="live-rates-carriers-list__wrapper">
 			<div className="live-rates-carriers-list__heading">
 				<div className="live-rates-carriers-list__icon"/>
-				<div className="live-rates-carriers-list__carrier">{translate('Carrier')}</div>
-				<div className="live-rates-carriers-list__features">{translate('Features')}</div>
+				<div className="live-rates-carriers-list__carrier">{ translate( 'Carrier' ) }</div>
+				<div className="live-rates-carriers-list__features">{ translate( 'Features' ) }</div>
 				<div className="live-rates-carriers-list__actions"/>
 			</div>
-			{carrierIds.map(carrierId => {
-				const CarrierView = carrierItemMap[carrierId]
-				if (!CarrierView) {
+			{carrierIds.map( ( carrierId ) => {
+				const CarrierView = carrierItemMap[ carrierId ]
+				if ( ! CarrierView ) {
 					return null
 				}
 
 				return (
-					<CarrierView key={carrierId} translate={translate}/>
+					<CarrierView key={ carrierId } translate={ translate } />
 				)
 			})}
 		</Card>
 	)
 }
 
-export default localize(CarriersList)
+export default localize( CarriersList )
--- client/extensions/woocommerce/woocommerce-services/views/live-rates-carriers-list/index.js
+++ client/extensions/woocommerce/woocommerce-services/views/live-rates-carriers-list/index.js
@@ -10,22 +10,22 @@ import { localize } from 'i18n-calypso'
 import ExtendedHeader from 'woocommerce/components/extended-header'
 import CarriersList from './carriers-list'
 
-const supportedCarrierIds = ['wc_services_usps', 'wc_services_dhlexpress'];
+const supportedCarrierIds = [ 'wc_services_usps', 'wc_services_dhlexpress' ];
 
-const LiveRatesCarriersList = ({ translate, carrierIds }) => {
-	if (carrierIds.some(carrierId => supportedCarrierIds.includes(carrierId)) === false) {
+const LiveRatesCarriersList = ( { translate, carrierIds } ) => {
+	if ( carrierIds.some( ( carrierId ) => supportedCarrierIds.includes( carrierId ) ) === false ) {
 		return null
 	}
 
 	return (
 		<div>
 			<ExtendedHeader
-				label={translate('Live rates at checkout')}
-				description={translate('Show live rates directly on your store - never under or overcharge for shipping again')}
+				label={ translate( 'Live rates at checkout' ) }
+				description={ translate( 'Show live rates directly on your store - never under or overcharge for shipping again' ) }
 			/>
-			<CarriersList carrierIds={carrierIds} />
+			<CarriersList carrierIds={ carrierIds } />
 		</div>
 	)
 }
 
-export default localize(LiveRatesCarriersList)
+export default localize( LiveRatesCarriersList )
