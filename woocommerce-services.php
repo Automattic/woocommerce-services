4f77813e15aaa8e5ee1e77e9637cb2d425b41030
Waclaw Jacek <waclaw.jacek@automattic.com>
2021-03-30 10:35:14 +0200

    Replace try-catch blocks from DHL note with check for data store.
    Replace the previously implemented try-catch blocks in the
    WC_Connect_Note_DHL_Live_Rates_Available class with one try-catch
    block in woocommerce-services.php, doing a check similar to what is
    implemented in WC Admin 2.1.2:
    https://github.com/woocommerce/woocommerce-admin/pull/6532

--- woocommerce-services.php
+++ woocommerce-services.php
@@ -216,7 +216,7 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 		public static function delete_notices() {
 			if ( self::can_add_wc_admin_notice() ) {
 				require_once __DIR__ . '/classes/class-wc-connect-note-dhl-live-rates-available.php';
-				WC_Connect_Note_DHL_Live_Rates_Available::delete_note();
+				WC_Connect_Note_DHL_Live_Rates_Available::possibly_delete_note();
 			}
 		}
 		/**
@@ -225,6 +225,12 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 		 * @return bool true|false.
 		 */
 		public static function can_add_wc_admin_notice() {
+			try {
+				WC_Data_Store::load( 'admin-note' );
+			} catch ( Exception $e ) {
+				return false;
+			}
+
 			return trait_exists( 'Automattic\WooCommerce\Admin\Notes\NoteTraits' ) && class_exists( 'Automattic\WooCommerce\Admin\Notes\Note' );
 		}
 
