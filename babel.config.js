2190b9885dbb9568264a9c878913975f1e8a9528
frosso <francesco.rosso@automattic.com>
2021-08-02 16:09:30 -0500

    testing things

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,12 @@
-const { useE2EBabelConfig } = require( '@woocommerce/e2e-environment' );
-
-module.exports = function( api ) {
-	api.cache( true );
-
-	return useE2EBabelConfig( {
-		presets: [
-			'@wordpress/babel-preset-default',
+module.exports = {
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				targets: {
+					node: 'current',
+				},
+			},
 		],
-	} );
+	],
 };
