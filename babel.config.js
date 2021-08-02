6430aa2e6c4464eb7cc47123ce6f1d274920ace3
frosso <francesco.rosso@automattic.com>
2021-08-02 16:04:41 -0500

    testing things

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,11 @@
-module.exports = {
-	presets: [
-		[
-			'@babel/preset-env',
-			{
-				targets: {
-					node: 'current',
-				},
-			},
+const { useE2EBabelConfig } = require( '@woocommerce/e2e-environment' );
+
+module.exports = function( api ) {
+	api.cache( true );
+
+	return useE2EBabelConfig( {
+		presets: [
+			'@wordpress/babel-preset-default',
 		],
-	],
+	} );
 };
--- tests/e2e/config/jest-puppeteer.config.js
+++ tests/e2e/config/jest-puppeteer.config.js
@@ -1,10 +1,8 @@
 const { useE2EJestPuppeteerConfig } = require( '@woocommerce/e2e-environment' );
 
-const puppeteerConfig = useE2EJestPuppeteerConfig( {
+module.exports = useE2EJestPuppeteerConfig( {
 	launch: {
 		slowMo: 100,
 		args: [ '--window-size=1920,1080', '--user-agent=chrome --runInBand --maxWorkers=4' ],
 	}
 } );
-
-module.exports = puppeteerConfig;
