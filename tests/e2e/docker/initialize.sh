bc9faf032f5325a86a0acc7d8b321766d26a8278
frosso <francesco.rosso@automattic.com>
2021-08-03 13:17:57 -0500

    testing things

--- tests/e2e/docker/initialize.sh
+++ tests/e2e/docker/initialize.sh
@@ -23,7 +23,7 @@ wp plugin install jetpack --activate
 #wp config set JETPACK_DEV_DEBUG true --raw
 
 # create credentials for REST API
-#wp eval-file "$WCS_DIR/tests/bin/wc_rest_api_credentials.php"
+wp eval-file "$WCS_DIR/tests/bin/wc_rest_api_credentials.php"
 
 # mock for Jetpack
 cp "$WCS_DIR/tests/e2e/config/travis/wc-services-testing-helper.php" "$WP_CORE_DIR/wp-content/plugins/"
@@ -36,7 +36,7 @@ wp plugin activate woocommerce-services
 wp plugin activate wc-services-testing-helper
 wp option update jetpack_tos_agreed 1
 wp option update wc_connect_options '{"tos_accepted": true }' --format=json
-#wp transient set wcc_is_new_label_user false
+wp transient set wcc_is_new_label_user false
 
 # set permalink settings to post name to enable REST API
 wp rewrite structure '/%postname%/'
