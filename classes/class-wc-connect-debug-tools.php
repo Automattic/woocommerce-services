7d157abf5e1b93d64563513c4a2456f9ac8d5625
Dustin Parker <11618203+dustinparker@users.noreply.github.com>
2022-02-08 14:41:56 +0100

    Create a backup of all tax rates before deleting the "CA" tax rates.

--- classes/class-wc-connect-debug-tools.php
+++ classes/class-wc-connect-debug-tools.php
@@ -30,7 +30,7 @@ if ( ! class_exists( 'WC_Connect_Debug_Tools' ) ) {
 				$tools['delete_ca_taxes'] = array(
 					'name'     => __( 'Delete California tax rates', 'woocommerce-services' ),
 					'button'   => __( 'Delete CA tax rates', 'woocommerce-services' ),
-					'desc'     => sprintf( '<strong class="red">%1$s</strong> %2$s <a href="https://woocommerce.com/document/woocommerce-shipping-and-tax/woocommerce-tax/#jan-2022-ca-notice" target="_blank">%3$s</a>', __( 'Note:', 'woocommerce-services' ), __( 'This option will delete ALL of your "CA" tax rates where the tax name ends with " Tax" (case insensitive), use with caution. This action cannot be reversed.', 'woocommerce-services' ), __( 'Additional information.', 'woocommerce-services' ) ),
+					'desc'     => sprintf( '<strong class="red">%1$s</strong> %2$s %3$s %4$s <a href="https://woocommerce.com/document/woocommerce-shipping-and-tax/woocommerce-tax/#jan-2022-ca-notice" target="_blank">%5$s</a>', __( 'Note:', 'woocommerce-services' ), __( 'This option will delete ALL of your "CA" tax rates where the tax name ends with " Tax" (case insensitive).', 'woocommerce-services' ), '<br>', __( 'A backup CSV of all existing tax rates will be made before the deletion process runs.', 'woocommerce-services' ), __( 'Additional information.', 'woocommerce-services' ) ),
 					'callback' => array( $this, 'delete_california_tax_rates' ),
 				);
 			}
@@ -48,8 +48,9 @@ if ( ! class_exists( 'WC_Connect_Debug_Tools' ) ) {
 		}
 
 		/**
-		 * Loop through the tax rates in the database and delete
-		 * rates where:
+		 * Back up all existing tax rates from the database in a CSV file.         *
+		 * Then, if successfully backed up, loop through the tax rates
+		 * in the database and delete rates where:
 		 * tax_rate_country = 'US' and
 		 * tax_rate_state = 'CA' and
 		 * tax_rate_name LIKE '% Tax'
@@ -57,6 +58,16 @@ if ( ! class_exists( 'WC_Connect_Debug_Tools' ) ) {
 		 * @return void
 		 */
 		function delete_california_tax_rates() {
+			$backed_up = WC_Connect_Functions::backup_existing_tax_rates();
+
+			if ( ! $backed_up ) {
+				echo '<div class="error inline"><p>';
+				echo __( 'ERROR: The "CA" tax rate deletion process was cancelled because the existing tax rates could not be backed up.', 'woocommerce-services' );
+				echo '</p></div>';
+
+				return;
+			}
+
 			global $wpdb;
 
 			$found_ca_rates = $wpdb->get_results(
@@ -95,6 +106,5 @@ if ( ! class_exists( 'WC_Connect_Debug_Tools' ) ) {
 			echo sprintf( __( 'Successfully deleted %1$d rows from the database.', 'woocommerce-services' ), $deleted_count );
 			echo '</p></div>';
 		}
-
 	}
 }
