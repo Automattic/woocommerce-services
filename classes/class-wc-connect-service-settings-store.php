04ba16bed6dd794d085dcdae6965a1163b99da03
Dustin Parker <11618203+dustinparker@users.noreply.github.com>
2022-11-17 10:40:53 +0100

    Add some type checks to avoid bugs down the road.

--- classes/class-wc-connect-service-settings-store.php
+++ classes/class-wc-connect-service-settings-store.php
@@ -116,8 +116,17 @@ if ( ! class_exists( 'WC_Connect_Service_Settings_Store' ) ) {
 		 * @return array
 		 */
 		public function get_endshipper_address() {
-			$master_user    = WC_Connect_Jetpack::get_master_user();
-			$connected_data = WC_Connect_Jetpack::get_connected_user_data( $master_user->ID );
+			$email       = '';
+			$master_user = WC_Connect_Jetpack::get_master_user();
+
+			if ( is_a( $master_user, 'WP_User' ) ) {
+
+				$connected_data = WC_Connect_Jetpack::get_connected_user_data( $master_user->ID );
+
+				if ( is_array( $connected_data ) && ! empty( $connected_data['email'] ) ) {
+					$email = $connected_data['email'];
+				}
+			}
 
 			$store_settings = array(
 				'company'   => html_entity_decode( get_bloginfo( 'name' ), ENT_QUOTES ), // HTML entities may be saved in the option.
@@ -127,7 +136,7 @@ if ( ! class_exists( 'WC_Connect_Service_Settings_Store' ) ) {
 				'state'     => WC()->countries->get_base_state(),
 				'country'   => WC()->countries->get_base_country(),
 				'postcode'  => WC()->countries->get_base_postcode(),
-				'email'     => $connected_data['email'],
+				'email'     => $email,
 				'phone'     => '',
 				'name'      => '',
 			);
