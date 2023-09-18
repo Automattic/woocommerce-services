6894c72eff87e14ac576f89e7107807ab6399ca8
Iyut <iyut85@yahoo.com>
2023-09-18 17:56:16 +0700

    change debug text

--- classes/class-wc-connect-tracks.php
+++ classes/class-wc-connect-tracks.php
@@ -110,7 +110,7 @@ if ( ! class_exists( 'WC_Connect_Tracks' ) ) {
 
 			$event_type = self::$product_name . '_' . $event_type;
 
-			$this->debug( 'Tracked the following eventssssss: ' . $event_type );
+			$this->debug( 'Tracked the following event: ' . $event_type );
 			WC_Connect_Jetpack::tracks_record_event( $user, $event_type, $data );
 		}
 
