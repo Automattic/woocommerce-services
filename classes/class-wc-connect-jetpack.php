1723baf99a4a82e01091010d0b757deacb703aff
Wacław Jacek <waclaw.jacek@automattic.com>
2023-10-18 13:01:28 +0200

    Refactor method name

--- classes/class-wc-connect-jetpack.php
+++ classes/class-wc-connect-jetpack.php
@@ -71,7 +71,7 @@ if ( ! class_exists( 'WC_Connect_Jetpack' ) ) {
 			return false;
 		}
 
-		public static function get_connected_user_data() {
+		public static function get_connection_owner_wpcom_data() {
 			$connection_owner_user_id = self::get_connection_manager()->get_connection_owner()->ID;
 
 			if ( ! $connection_owner_user_id ) {
