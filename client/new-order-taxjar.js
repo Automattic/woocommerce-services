e84f96a9a87b6be92064654438cec127cd5ebd46
Luthfi Bintoro <iyut85@yahoo.com>
2023-11-30 21:19:17 +0700

    Fix/issue 2703 - Add condition to make sure the js is used only when recalculating tax. (#2704)
    * check recalculate action before parsing
    
    * add changelog
    
    * change selector on e2e test

--- client/new-order-taxjar.js
+++ client/new-order-taxjar.js
@@ -10,7 +10,8 @@ jQuery( document ).ready( function() {
 	*/
 	const TaxJarOrder = function( $ ){ // eslint-disable-line no-unused-vars
 		$( document ).ajaxSend( function( event, request, settings ) {
-			if ( settings.data ) {
+			// Making sure it's a recalculate event by checking `woocommerce_calc_line_taxes` before parsing to JSON.
+			if ( settings.data && settings.data.indexOf( 'woocommerce_calc_line_taxes' ) > 0 ) {
 				const data = JSON.parse( '{"' + decodeURIComponent( settings.data.replace( /&/g, '","' ).replace( /=/g, '":"' ) ) + '"}' );
  				if ( 'woocommerce_calc_line_taxes' === data.action ) {
 					let street = '';
