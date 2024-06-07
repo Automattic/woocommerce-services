dbee1b19764eae5aba89ca77dbded7870c4fd8c7
Sam Najian <sam.najian@automattic.com>
2024-06-07 17:23:00 +0200

    Disable the modal for the time being

--- classes/class-wc-connect-service-settings-store.php
+++ classes/class-wc-connect-service-settings-store.php
@@ -616,9 +616,9 @@ if ( ! class_exists( 'WC_Connect_Service_Settings_Store' ) ) {
 
 			return $lookup;
 		}
-
+		
 		public function is_eligible_for_migration() {
-			return true;
+			return false;
 		}
 
 		private function translate_unit( $value ) {
