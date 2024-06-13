e7ec3ad0c53189fd835728090556f18abc2f65db
Harris Wong <harris.wong@automattic.com>
2024-06-13 16:52:42 -0600

    Set a flag "wcshipping_migrated" to indicate it is migrated

--- classes/class-wc-rest-connect-migration-flag-controller.php
+++ classes/class-wc-rest-connect-migration-flag-controller.php
@@ -11,9 +11,8 @@ if ( class_exists( 'WC_REST_Connect_Migration_Flag_Controller' ) ) {
 class WC_REST_Connect_Migration_Flag_Controller extends WC_REST_Connect_Base_Controller {
 	protected $rest_base = 'connect/migration-flag';
 
-	public function post( $request ) {
-		$flag = $request->get_json_params();
-
+	public function post() {
+		WC_Connect_Options::update_option( 'wcshipping_migrated', true );
 
 		return new WP_REST_Response( array( 'success' => true ), 200 );
 	}
