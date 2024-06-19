53175cda4f41684c95523da23e7d741a22b1288c
Harris Wong <harris.wong@automattic.com>
2024-06-19 12:01:03 -0600

    Set a flag to indicate migration has started or has completed (#2747)
    * Add connect/migration-flag endpoint
    
    * Set a flag "wcshipping_migrated" to indicate it is migrated
    
    * is_eligible_for_migration() now checks for the config as well
    
    * Add migration done API call to the update button
    
    * Update migration flag to migration state
    
    * Update space to tabs
    
    * Call API to update migration status to "started"
    
    * Update migration state to completed in plugin deactivation hook
    
    * Default is_eligible_for_migration to return false
    
    * Update API constants to something like an enum instead
    
    * PHP cs fixes
    
    * Return 200 if option updated, 304 otherwise
    
    * Remove redundancy in condition
    
    * Inline code comment can not be a DocBlock
    
    * Remove redundant class_exists() check for the enum class
    
    * Return 500 if update_option failed

---
+++ classes/class-wc-connect-wcst-to-wcshipping-migration-state-enum.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Enum for WCS&T to WCShipping migration states.
+ */
+class WC_Connect_WCST_To_WCShipping_Migration_State_Enum {
+	// These are used for WCS&T to WCShipping migration.
+	public const NOT_STARTED = 1;
+	public const STARTED     = 2;
+	public const COMPLETED   = 3;
+	public const FAILED      = 4;
+
+	public static function is_valid_value( $state ) {
+		$valid_states = array(
+			self::NOT_STARTED,
+			self::STARTED,
+			self::COMPLETED,
+			self::FAILED,
+		);
+		return in_array( $state, $valid_states, true );
+	}
+}
