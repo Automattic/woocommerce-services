aeec3dcce6e7b9f0f02dc62381bda594a80b1251
Harris Wong <harris.wong@automattic.com>
2024-06-25 15:57:03 -0600

    Rename state to better describe its name

--- client/components/migration/feature-announcement.jsx
+++ client/components/migration/feature-announcement.jsx
@@ -38,7 +38,7 @@ const FeatureAnnouncement = ({ translate, isEligable }) => {
 	   };
 
 		const installPluginAPICall = () =>
-			fetch( getBaseURL() + 'wc-admin/plugins/install', {
+			fetch( getBaseURL() + 'wc-admin/plugins/install2', {
 				method: 'POST',
 				headers,
 				body: JSON.stringify({
@@ -63,71 +63,88 @@ const FeatureAnnouncement = ({ translate, isEligable }) => {
 					status: 'inactive'
 				})
 			} );
-		const markMigrationStartedAPICall = () =>
+		const markMigrationStartedAPICall = (migrationState) => () =>
 			fetch( getBaseURL() + 'wc/v1/connect/migration-flag', {
 				method: 'POST',
 				headers,
 				body: JSON.stringify({
-					migration_state: 2 // Check WC_Connect_WCST_To_WCShipping_Migration_State_Enum::STARTED
+					migration_state: migrationState // Check WC_Connect_WCST_To_WCShipping_Migration_State_Enum::STARTED
 				})
 			} );
 
-		const stateErrorHandling = () => {
+		const stateErrorHandling = (failedMigrationState) => () => {
+			// TODO: Print the error somewhere in an inbox box?
+			console.log(failedMigrationState);
+
+			return fetch( getBaseURL() + 'wc/v1/connect/migration-flag', {
+				method: 'POST',
+				headers,
+				body: JSON.stringify({
+					migration_state: failedMigrationState // Check WC_Connect_WCST_To_WCShipping_Migration_State_Enum::STARTED
+				})
+			} );
 		};
 
 		const installAndActivatePlugins = async() => {
 			const migrationStateTransitions = {
-				s0: {
-					success: 's1',
-					fail: 's0',
-					callback: markMigrationStartedAPICall,
+				stateInit: {
+					success: 'stateInstalling',
+					fail: 'stateInit',
+					callback: markMigrationStartedAPICall(2),
 				},
-				s1: {
-					success: 's3',
-					fail: 's2',
+				stateErrorInit: {
+					success: 'stateInit',
+					fail: 'stateErrorInit',
+					callback: stateErrorHandling('stateInit'),
+				},
+				stateInstalling: {
+					success: 'stateActivating',
+					fail: 'stateErrorInstalling',
 					callback: installPluginAPICall,
 				},
-				s2: {
-					success: 's1',
-					fail: 's2',
-					callback: stateErrorHandling,
+				stateErrorInstalling: {
+					success: 'stateInstalling',
+					fail: 'stateErrorInstalling',
+					callback: stateErrorHandling('stateInstalling'),
 				},
-				s3: {
-					success: 's7', // TODO: This should be s5 to migrate DB.
-					fail: 's4',
+				stateActivating: {
+					success: 'stateDeactivating', // TODO: This should be stateDBMigrating to migrate DB.
+					fail: 'stateErrorActivating',
 					callback: activatePluginAPICall,
 				},
-				s4: {
-					success: 's3',
-					fail: 's4',
-					callback: stateErrorHandling,
+				stateErrorActivating: {
+					success: 'stateActivating',
+					fail: 'stateErrorActivating',
+					callback: stateErrorHandling('stateActivating'),
 				},
-				s5: {
+				stateDB: {
 					success: 's7',
-					fail: 's6',
+					fail: 'stateErrorDB',
 					callback: new Promise((resolve) => resolve({status: 200})), // TODO: DB migration
 				},
-				s6: {
-					success: 's5',
-					fail: 's6',
-					callback: stateErrorHandling,
+				stateErrorDB: {
+					success: 'stateDBMigrating',
+					fail: 'stateErrorDB',
+					callback: stateErrorHandling('stateDBMigrating'),
 				},
-				s7: {
-					success: 's9',
-					fail: 's8',
+				stateDeactivating: {
+					success: 'stateDone',
+					fail: 'stateErrorDeactivating',
 					callback: deactivateWCSTPluginAPICall,
 				},
-				s8: {
-					success: 's7',
-					fail: 's8',
-					callback: stateErrorHandling,
+				stateErrorDeactivating: {
+					success: 'stateDeactivating',
+					fail: 'stateErrorDeactivating',
+					callback: stateErrorHandling('stateDeactivating'),
 				},
-				s9: { // Done state.
+				stateDone: { // Done state.
 					success: null,
 					fail: null,
 					callback: null,
 				}
 			}
+
+			const errorStates = ['stateErrorInstalling', 'stateErrorActivating', 'stateErrorDB', 'stateErrorDeactivating'];
 
 			const runNext = async (migrationState) => {
 				const currentMigrationState = migrationStateTransitions[migrationState];
@@ -150,9 +167,9 @@ const FeatureAnnouncement = ({ translate, isEligable }) => {
 			};
 
 			// Run the migration chain
-			let nextMigrationStateToRun = 's0';
+			let nextMigrationStateToRun = 'stateInit';
 			let runAttemps = 0;
-			while ( runAttemps++ < 15 && nextMigrationStateToRun !== 's9' ) {
+			while ( runAttemps++ < 15 && nextMigrationStateToRun !== 'stateDone' && ! errorStates.includes(nextMigrationStateToRun)) {
 				nextMigrationStateToRun = await runNext(nextMigrationStateToRun);
 				console.log(runAttemps);
 			}
