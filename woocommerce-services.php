da3580f20540c3961bfcd2849f7d564f26ca3e74
Sam Najian <dev.samnajian@gmail.com>
2024-06-26 19:02:16 +0200

    Change dismiss icon and behaviour [shipping-140] (#2758)
    * Add is-dismissable and button label from server
    
    * Register separate stylesheet and add stylings for the banner and buttons
    
    * Add functionality to dismiss and remember dismissal

--- woocommerce-services.php
+++ woocommerce-services.php
@@ -254,6 +254,8 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 
 		protected static $wcs_version;
 
+		const MIGRATION_DISMISSAL_COOKIE_KEY = 'wcst-wcshipping-migration-dismissed';
+
 		public static function plugin_deactivation() {
 			wp_clear_scheduled_hook( 'wc_connect_fetch_service_schemas' );
 
@@ -1523,6 +1525,8 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 			// Add the WCS&T to WCShipping migratio notice, creating a button to update.
 			$settings_store = $this->get_service_settings_store();
 			if ( $settings_store->is_eligible_for_migration() ) {
+				wp_enqueue_script( 'wc_connect_admin_notices' );
+				wp_enqueue_style( 'wc_connect_admin_notices' );
 				add_action( 'admin_notices', array( $this, 'display_wcst_to_wcshipping_migration_notice' ) );
 			}
 		}
@@ -1543,9 +1547,12 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 					"var link = document.createElement('link');link.rel = 'stylesheet';link.type = 'text/css';link.href = '" . esc_js( $stylesheet_url ) . "';document.getElementsByTagName('HEAD')[0].appendChild(link);"
 				);
 			}
+
 			wp_register_script( 'wc_services_admin_pointers', $this->wc_connect_base_url . 'woocommerce-services-admin-pointers-' . $plugin_version . '.js', array( 'wp-pointer', 'jquery' ), null );
 			wp_register_style( 'wc_connect_banner', $this->wc_connect_base_url . 'woocommerce-services-banner-' . $plugin_version . '.css', array(), null ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
 			wp_register_script( 'wc_connect_banner', $this->wc_connect_base_url . 'woocommerce-services-banner-' . $plugin_version . '.js', array(), null );
+			wp_register_script( 'wc_connect_admin_notices', $this->wc_connect_base_url . 'woocommerce-services-admin-notices-' . $plugin_version . '.js', array( 'wp-util', 'jquery' ), null, array( 'in_footer' => true ) );
+			wp_register_style( 'wc_connect_admin_notices', $this->wc_connect_base_url . 'woocommerce-services-admin-notices-' . $plugin_version . '.css', array(), null );
 
 			$i18n_json = $this->get_i18n_json();
 			/** @var array $i18nStrings defined in i18n/strings.php */
@@ -1563,6 +1570,13 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 				array(
 					'assetPath'       => self::get_wc_connect_base_url(),
 					'adminPluginPath' => admin_url( 'plugins.php' ),
+				)
+			);
+			wp_localize_script(
+				'wc_connect_admin_notices',
+				'wc_connect_admin_notices',
+				array(
+					'dismissalCookieKey' => self::MIGRATION_DISMISSAL_COOKIE_KEY,
 				)
 			);
 		}
@@ -1895,6 +1909,9 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 		 * @return bool
 		 */
 		public function display_wcst_to_wcshipping_migration_notice() {
+			if ( isset( $_COOKIE[ self::MIGRATION_DISMISSAL_COOKIE_KEY ] ) && (int) $_COOKIE[ self::MIGRATION_DISMISSAL_COOKIE_KEY ] === 1 ) {
+				return;
+			}
 			$schema = $this->get_service_schemas_store();
 			$banner = $schema->get_wcship_wctax_upgrade_banner();
 			if ( empty( $banner ) ) {
@@ -1902,8 +1919,9 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 			}
 			echo wp_kses_post(
 				sprintf(
-					'<div class="notice notice-%s is-dismissible wcst-wcshipping-migration-notice"><p style="margin-bottom:0px">',
-					$banner->type
+					'<div class="notice notice-%s %s wcst-wcshipping-migration-notice"><div class="notice-content"><p>',
+					$banner->type,
+					$banner->dismissible ? 'is-dismissible' : ''
 				) .
 				sprintf(
 					/* translators: %s: documentation URL */
@@ -1911,7 +1929,7 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 					'https://woocommerce.com/document/woocommerce-shipping-and-tax/woocommerce-shipping/#how-do-i-migrate-from-wcst'
 				) .
 				sprintf(
-					'</p><button style="color:#3858E9;margin: 12px 0;border: 1px solid #3858E9;padding: 11.5px 12px 11.5px 12px;border-radius: 2px;background-color:#fff;">Confirm update</button></div>',
+					'</p><div><button class="action-button">%s</button></div></div></div>',
 					$banner->action
 				)
 			);
