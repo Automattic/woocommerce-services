019d6fd10b6833f1bbd853cda4b765c6ac4b7718
Sam Najian <sam.najian@automattic.com>
2024-08-12 11:08:32 +0200

    Try to get logs from the e2e container

--- tests/bin/wait-for-e2e-build.sh
+++ tests/bin/wait-for-e2e-build.sh
@@ -11,9 +11,31 @@ count=0
 WP_BASE_URL=$(node ./tests/bin/get-base-url.js)
 printf "Testing URL: $WP_BASE_URL\n\n"
 ready_page=$(curl -s ${WP_BASE_URL}/?pagename=ready)
+echo WP_BASE_URL
+docker ps
+# Find the container running on port 8084
+get_docker_logs_on_port() {
+
+    local port=$1
+
+    # Find the container running on the specified port
+    local container_id=$(docker ps | grep "$port" | awk '{print $1}')
+
+    # Check if a container was found
+    if [ -n "$container_id" ]; then
+        echo "Container found with ID: $container_id"
+        echo "Fetching logs..."
+        docker logs $container_id
+    else
+        echo "No container found running on port $port."
+    fi
+}
+
+get_docker_logs_on_port 8084
 
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' ${WP_BASE_URL}/?pagename=ready)" != "200" || $ready_page != *"E2E-tests"* ]]
 do
+  get_docker_logs_on_port 8084
   echo "$(date) - Docker container is still being built"
   sleep ${DELAY_SEC}
 
