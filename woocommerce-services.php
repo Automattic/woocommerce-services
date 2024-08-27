ca2aad73250050823514d969a64a71a3791f9d72
Harris Wong <harris.wong@automattic.com>
2024-08-27 10:57:12 -0600

    Move require_once out from the is_wc_shipping_activated() condition

--- woocommerce-services.php
+++ woocommerce-services.php
@@ -1102,12 +1102,12 @@ if ( ! class_exists( 'WC_Connect_Loader' ) ) {
 				$rest_carrier_types_controller = new WC_REST_Connect_Shipping_Carrier_Types_Controller( $this->api_client, $settings_store, $logger );
 				$this->set_carrier_types_controller( $rest_carrier_types_controller );
 				$rest_carrier_types_controller->register_routes();
-
-				require_once __DIR__ . '/classes/class-wc-rest-connect-migration-flag-controller.php';
-				$rest_migration_flag_controller = new WC_REST_Connect_Migration_Flag_Controller( $this->api_client, $settings_store, $logger, $this->tracks );
-				$this->set_rest_migration_flag_controller( $rest_migration_flag_controller );
-				$rest_migration_flag_controller->register_routes();
 			}
+			require_once __DIR__ . '/classes/class-wc-rest-connect-migration-flag-controller.php';
+			$rest_migration_flag_controller = new WC_REST_Connect_Migration_Flag_Controller( $this->api_client, $settings_store, $logger, $this->tracks );
+			$this->set_rest_migration_flag_controller( $rest_migration_flag_controller );
+			$rest_migration_flag_controller->register_routes();
+
 			require_once __DIR__ . '/classes/class-wc-rest-connect-shipping-carriers-controller.php';
 			$rest_carriers_controller = new WC_REST_Connect_Shipping_Carriers_Controller( $this->api_client, $settings_store, $logger );
 			$this->set_rest_carriers_controller( $rest_carriers_controller );
