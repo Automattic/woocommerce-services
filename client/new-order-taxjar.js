4abf4226e2529fdea55e5e637826c92406683192
Iyut <iyut85@yahoo.com>
2024-12-10 17:37:27 +0700

    making sure the data is a string

--- client/new-order-taxjar.js
+++ client/new-order-taxjar.js
@@ -11,7 +11,7 @@ jQuery( document ).ready( function() {
 	const TaxJarOrder = function( $ ){ // eslint-disable-line no-unused-vars
 		$( document ).ajaxSend( function( event, request, settings ) {
 			// Making sure it's a recalculate event by checking `woocommerce_calc_line_taxes` before parsing to JSON.
-			if ( settings.data && settings.data.indexOf( 'woocommerce_calc_line_taxes' ) > 0 ) {
+			if ( settings.data && typeof settings.data === 'string' && settings.data.indexOf( 'woocommerce_calc_line_taxes' ) > 0 ) {
 				const data = JSON.parse( '{"' + decodeURIComponent( settings.data.replace( /&/g, '","' ).replace( /=/g, '":"' ) ) + '"}' );
  				if ( 'woocommerce_calc_line_taxes' === data.action ) {
 					let street = '';
