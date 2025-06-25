ab0e4497404232c8660692d1d4ca960118a38f7e
PKMN-World <github+pkmn@alexhaas.me>
2025-06-25 03:55:37 +0000

    Adding setting to disable legacy shipping features.

--- classes/class-wc-connect-shipping-label.php
+++ classes/class-wc-connect-shipping-label.php
@@ -495,6 +495,11 @@ if ( ! class_exists( 'WC_Connect_Shipping_Label' ) ) {
 				return false;
 			}
 
+			$disable_shipping = get_option( 'wc_connect_disable_shipping', 'no' );
+			if ( $disable_shipping === 'yes' ) {
+				return false;
+			}
+
 			// If the shipping label is disabled, will remove the meta box.
 			if ( ! $this->is_shipping_label_enabled() ) {
 				return false;
@@ -532,6 +537,10 @@ if ( ! class_exists( 'WC_Connect_Shipping_Label' ) ) {
 		 * @return bool True if shipping label is enabled from the settings.
 		 */
 		public function is_shipping_label_enabled() {
+			if ( get_option( 'wc_connect_disable_shipping' ) === 'yes' ) {
+				return false;
+			}
+			
 			$account_settings = $this->account_settings->get();
 
 			if ( isset( $account_settings['formData']['enabled'] ) && is_bool( $account_settings['formData']['enabled'] ) ) {
