a0b0521ac76592c534025c5b50e4a0e86b29a567
Iyut <iyut85@yahoo.com>
2025-07-02 14:07:55 +0700

    specify html_entity_decode 2nd parameter

--- classes/class-wc-connect-shipping-method.php
+++ classes/class-wc-connect-shipping-method.php
@@ -611,7 +611,7 @@ if ( ! class_exists( 'WC_Connect_Shipping_Method' ) ) {
 
 		public static function format_rate_title( $rate_title ) {
 			$formatted_title = wp_kses(
-				html_entity_decode( $rate_title ),
+				html_entity_decode( $rate_title, ENT_COMPAT ),
 				array(
 					'sup'    => array(),
 					'del'    => array(),
