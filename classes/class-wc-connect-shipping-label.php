ede8a89127f1a4d7f42c727cc022a55de6d01674
PKMN-World <195479173+PKMN-World@users.noreply.github.com>
2025-07-24 00:11:27 -0700

    Disable shipping features unless WooCommerce Shipping is active (#2856)
    * Add option to disable legacy shipping features.
    
    * Remove new setting and check if WooCommerce Shipping plugin is enabled.
    
    ---------
    
    Co-authored-by: Fernando Espinosa <Ferdev@users.noreply.github.com>

--- classes/class-wc-connect-shipping-label.php
+++ classes/class-wc-connect-shipping-label.php
@@ -532,6 +532,10 @@ if ( ! class_exists( 'WC_Connect_Shipping_Label' ) ) {
 		 * @return bool True if shipping label is enabled from the settings.
 		 */
 		public function is_shipping_label_enabled() {
+			if ( ! WC_Connect_Loader::is_wc_shipping_activated() ) {
+				return false;
+			}
+
 			$account_settings = $this->account_settings->get();
 
 			if ( isset( $account_settings['formData']['enabled'] ) && is_bool( $account_settings['formData']['enabled'] ) ) {
