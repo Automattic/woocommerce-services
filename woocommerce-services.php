667808f2f4319f31f33d00390df3a1f63e74253b
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2025-08-13 12:52:54 +0300

    Revert "woorelease: Product version bump update"
    This reverts commit 82c9e5842a6997528106661d90a28b554e7efa68.

--- woocommerce-services.php
+++ woocommerce-services.php
@@ -8,7 +8,7 @@
  * Author URI: https://woocommerce.com/
  * Text Domain: woocommerce-services
  * Domain Path: /i18n/languages/
- * Version: 3.0.8
+ * Version: 3.0.7
  * Requires Plugins: woocommerce
  * Requires PHP: 7.4
  * Requires at least: 6.7
