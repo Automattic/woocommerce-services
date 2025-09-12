5c149a32fdd52c0079db7992cbf3354752c37a81
Iyut <iyut85@yahoo.com>
2025-09-12 17:28:53 +0700

    bump tested up to version

--- woocommerce-services.php
+++ woocommerce-services.php
@@ -13,8 +13,8 @@
  * Requires PHP: 7.4
  * Requires at least: 6.7
  * Tested up to: 6.8
- * WC requires at least: 9.9
- * WC tested up to: 10.1
+ * WC requires at least: 10.0
+ * WC tested up to: 10.2
  *
  * Copyright (c) 2017-2023 Automattic
  *
