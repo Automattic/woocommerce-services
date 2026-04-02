1c056c0dfb95ab0eae7367d07e6f7265e1955d5e
bartech <bartech@bartech.dev>
2026-04-02 15:00:08 +0200

    Fix pre-commit PHPCBF running on entire directory instead of staged files
    `composer run phpcbf` hardcodes `.` as the scan path, so the file
    paths passed by lint-staged were ignored. This caused JS files to be
    reformatted with PHP coding standards on every PHP commit.
    
    Call `./vendor/bin/phpcbf` directly with only the staged file paths.

--- bin/wc-phpcbf.sh
+++ bin/wc-phpcbf.sh
@@ -3,7 +3,10 @@
 # phpcbf returns exit-code as 1 even if all errors were fixed.
 # https://github.com/squizlabs/PHP_CodeSniffer/issues/3057#issuecomment-919794895
 
-composer run phpcbf $@
+# Run phpcbf directly on the passed files instead of using `composer run phpcbf`
+# which hardcodes `.` as the scan path and would format the entire directory
+# (including JS files) regardless of the file list lint-staged provides.
+./vendor/bin/phpcbf --standard=WooCommerce-Core,WordPress-Core,WordPress-Extra "$@"
 
 status=$?
 
